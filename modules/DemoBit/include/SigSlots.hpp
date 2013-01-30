08cb265a116d41fc9d9dab484431bbea3eb82dfc
Hugh Dickinson <hugh.dickinson@fysik.su.se>
2013-01-30 10:45:20 +0100

    Update of DemoBit and initial work on boost spirit IniParser to instantiate required classes at runtime

---
+++ modules/DemoBit/include/SigSlots.hpp
@@ -0,0 +1,14 @@
+//
+//  SigSlots.hpp
+//  HEPForgeGambit
+//
+//  Created by Hugh Dickinson on 24/01/2013.
+//  Copyright (c) 2013 Hugh Dickinson. All rights reserved.
+//
+
+#ifndef HEPForgeGambit_SigSlots_hpp
+#define HEPForgeGambit_SigSlots_hpp
+
+
+
+#endif
