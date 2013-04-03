a99c28245f1b50ad668b201f5af7adb4cee6d2d4
Pat Scott <patscott@physics.mcgill.ca>
2013-04-03 13:31:05 -0400

    Re-added half-finished files to backend_devb branch.

--- modules/ExampleBit_A/include/ExampleBit_A_rollcall.hpp
+++ modules/ExampleBit_A/include/ExampleBit_A_rollcall.hpp
@@ -33,30 +33,39 @@ START_MODULE
 
   #define CAPABILITY nevents                // A physical observable or likelihood that this module can calculate.  There may be one or more 
   START_CAPABILITY                          //  functions in this module that can calculate this particular thing in different ways.
+
     #define FUNCTION nevents_dbl            // Name of an observable function: floating-point number of events in some hypothetical process
     START_FUNCTION(double)                  // Declare that this function calculates the nevents observable as a double precision variable
     DEPENDENCY(xsection, double)            // Dependencies: Number of events depends on cross-section
     DEPENDENCY(charge, double)              // and charge. 
     #undef FUNCTION
+
     #define FUNCTION nevents_int            // Name of an observable function: integral number of events in some hypothetical process
     START_FUNCTION(int)                     // Declare that this function calculates the nevents observable as an integer variable
     DEPENDENCY(nevents, double)             // Dependencies: Integral number of events depends on floating-point nevents
     #undef FUNCTION
+
   #undef CAPABILITY
+
 
   #define CAPABILITY nevents_like           // A physical observable or likelihood that this module can calculate
   START_CAPABILITY
+
     #define FUNCTION nevents_like           // Likelihood: Likelihood of seeing number of events 
     START_FUNCTION(double)                  // Function calculates the nevents_like likelihood as a double precision variable                  
     DEPENDENCY(nevents, double)             // Dependency: Likelihood calculation requires number of events       
     #undef FUNCTION
+
   #undef CAPABILITY
+
 
   #define CAPABILITY dog                    // A physical observable or likelihood that this module can calculate
   START_CAPABILITY
+
     #define FUNCTION authors_dogs_name      // Observable: name of the author of ExampleBitA's dog    
     START_FUNCTION(std::string)             // Function calculates the dog observable as a string 
     #undef FUNCTION
+
   #undef CAPABILITY
 
 
