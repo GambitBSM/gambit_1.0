6963e6650f9fc249534e00e74310482269bc5060
Andy Buckley <andy.buckley@cern.ch>
2013-04-25 09:16:47 +0200

    More clean-ups

--- modules/HEColliderBit/src/compile.sh
+++ modules/HEColliderBit/src/compile.sh
@@ -1,6 +1,6 @@
-# need to write a proper makefile
+# TODO: write a proper makefile
 # most probably you will need to adapt the location of the HEColliderBit directory
 
 g++ -c DetectorResponse.cpp   `root-config --cflags`
-g++ -c fastsim.cpp  `root-config --cflags` -l m
-g++ example.cpp -o example fastsim.o DetectorResponse.o  `root-config --cflags` -l m `root-config --libs`
+g++ -c FastSim.cpp  `root-config --cflags` -lm
+g++ example-fastsim.cpp -o example-fastsim fastsim.o DetectorResponse.o  `root-config --cflags` -lm `root-config --libs`
