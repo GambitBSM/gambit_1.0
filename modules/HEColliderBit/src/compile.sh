de63a6647ea05a4f87dda8e7b41e6f430752c280
Andy Buckley <andy.buckley@cern.ch>
2013-04-25 12:20:58 +0200

    Removing defunct compile.sh script

--- modules/HEColliderBit/src/compile.sh
+++
@@ -1,6 +0,0 @@
-# TODO: write a proper makefile
-# most probably you will need to adapt the location of the HEColliderBit directory
-
-g++ -c DetectorResponse.cpp   `root-config --cflags`
-g++ -c FastSim.cpp  `root-config --cflags` -lm
-g++ example-fastsim.cpp -o example-fastsim fastsim.o DetectorResponse.o  `root-config --cflags` -lm `root-config --libs`
