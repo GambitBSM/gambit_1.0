4a9205a68abfad255014ab3986403bf8a392d423
Abram Krislock <abram.krislock@fysik.su.se>
2013-04-26 15:38:47 +0200

    Changed Delphes3Backend and Pythia8Backend to be classes instead of namespaces. This works better with openMP, however I may still have problems running Delphes in parallel threads using openMP.
    Will test that after this commit.

--- modules/HEColliderBit/src/Delphes3Backend.hpp
+++ modules/HEColliderBit/src/Delphes3Backend.hpp
@@ -42,38 +42,41 @@
 #include "classes/DelphesFactory.h"
 #include "ExRootAnalysis/ExRootConfReader.h"
 
-#define DELPHES3BACKEND_PRIVATE GAMBIT::HEColliderBit::Delphes3Backend::confReader,GAMBIT::HEColliderBit::Delphes3Backend::modularDelphes,GAMBIT::HEColliderBit::Delphes3Backend::factory,GAMBIT::HEColliderBit::Delphes3Backend::allParticleOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::stableParticleOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::partonOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::pdg,GAMBIT::HEColliderBit::Delphes3Backend::candidate,GAMBIT::HEColliderBit::Delphes3Backend::pdgParticle,GAMBIT::HEColliderBit::Delphes3Backend::pdgCode
+// #define DELPHES3BACKEND_PRIVATE GAMBIT::HEColliderBit::Delphes3Backend::confReader,GAMBIT::HEColliderBit::Delphes3Backend::modularDelphes,GAMBIT::HEColliderBit::Delphes3Backend::factory,GAMBIT::HEColliderBit::Delphes3Backend::allParticleOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::stableParticleOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::partonOutputArray,GAMBIT::HEColliderBit::Delphes3Backend::pdg,GAMBIT::HEColliderBit::Delphes3Backend::candidate,GAMBIT::HEColliderBit::Delphes3Backend::pdgParticle,GAMBIT::HEColliderBit::Delphes3Backend::pdgCode
 
 namespace GAMBIT
 {
   namespace HEColliderBit
   {
-    namespace Delphes3Backend
+    class Delphes3Backend
     {
-      // To read Delphes Config File
-      extern ExRootConfReader *confReader;
-      // Modularity of Delphes is set by said Config File.
-      extern Delphes *modularDelphes;
-      // Factory production of particle "candidates"
-      extern DelphesFactory *factory;
-      // Delphes particle arrays: Pre-Detector-Sim
-      extern TObjArray *allParticleOutputArray;
-      extern TObjArray *stableParticleOutputArray;
-      extern TObjArray *partonOutputArray;
-      // Database of PDG codes and particle info
-      extern TDatabasePDG *pdg;
-      // Used within the event Converters below
-      extern Candidate *candidate;
-      extern TParticlePDG *pdgParticle;
-      extern Int_t pdgCode;
-
-      void initialize(string configFileName);
-      void finalize();
+    public:
+      Delphes3Backend(string configFileName);
+      ~Delphes3Backend();
 
       void processEvent(Pythia8::Event &eventIn, Event &eventOut);
       void convertInput(Pythia8::Event &event);
       void convertOutput(Event &event);
-      // TODO: rollcall!!
-    }
+
+    private:
+      // To read Delphes Config File
+      ExRootConfReader *confReader;
+      // Modularity of Delphes is set by said Config File.
+      Delphes *modularDelphes;
+      // Factory production of particle "candidates"
+      DelphesFactory *factory;
+      // Delphes particle arrays: Pre-Detector-Sim
+      TObjArray *allParticleOutputArray;
+      TObjArray *stableParticleOutputArray;
+      TObjArray *partonOutputArray;
+      // Database of PDG codes and particle info
+      TDatabasePDG *pdg;
+      // Used within the event Converters below
+      Candidate *candidate;
+      TParticlePDG *pdgParticle;
+      Int_t pdgCode;
+
+      // TODO: rollcall?
+    };
   }
 }
