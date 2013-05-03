eed6a2fbfafc6ffe1342fd5ad2d12ebb2dcefc38
Benjamin Farmer <ben.farmer@gmail.com>
2013-05-03 14:47:10 +1000

    Fixed up preliminary code in ModelBit so that it compiles (run the makefile in ModelBit dir AFTER compiling main gambit code). Main files of interest are (in ModelBit dir) 'minitest' (executable), 'minitest.cpp', 'models/MSSM.hpp', and 'include/modelmacros.hpp'

--- modules/ModelBit/minitest.cpp
+++ modules/ModelBit/minitest.cpp
@@ -20,24 +20,31 @@ inline std::ostream& operator << (std::ostream& os, const std::vector<Type>& v)
 
 int main( int argc, const char* argv[] )
 {
-  // Can access names without creating objects
-  std::cout<<models::MSSM::name()<<std::endl;
-  std::cout<<models::CMSSM_base::name()<<std::endl;
-  std::cout<<models::CMSSM::P1::name()<<std::endl;
-
-  // But cannot access lineage. Need to make objects for that
-  models::CMSSM_base M1;
-  models::CMSSM::P1 M2;
+  // Need to make objects in order to access names and lineage
+  gambit::models::CMSSM_base M1;
+  gambit::models::CMSSM::P1 M2;
   
+  // We can also refer to models by pointers of the base class type, which means
+  // the rest of the code need not know exactly which class a given model is in
+  // order to fiddle with the model objects.
+  gambit::models::model_base* p1=&M1;
+  gambit::models::model_base* p2=&M2;
+  
+  // Can then refer to functions static binding if we know the class:
   std::cout<<M1.name()<<", "<<M1.lineage()<<std::endl;
   std::cout<<M2.name()<<", "<<M2.lineage()<<std::endl;
   
-  M2.setValue("M0",1234);
-  M2.setValue("M12",4321);
-  M2.setValue("A0",100);
+  // or by dynamic binding if we don't (apologies if these are the wrong C++ 
+  // words...)
+  std::cout<<p1->name()<<", "<<p1->lineage()<<std::endl;
+  std::cout<<p2->name()<<", "<<p2->lineage()<<std::endl;
   
-  std::cout<<"M0 = "<<M2.getValue("M0")<<std::endl;
-  std::cout<<"M12 = "<<M2.getValue("M12")<<std::endl;
+  M2.setValue("M0",1234);
+  p2->setValue("M12",4321);
+  p2->setValue("A0",100);
+  
+  std::cout<<"M0 = "<<p2->getValue("M0")<<std::endl;
+  std::cout<<"M12 = "<<p2->getValue("M12")<<std::endl;
   std::cout<<"A0 = "<<M2.getValue("A0")<<std::endl;
 
 }
