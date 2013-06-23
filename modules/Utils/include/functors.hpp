43898dee7d7d01f68d1a84138fab1c74b283034f
Pat Scott <patscott@physics.mcgill.ca>
2013-06-23 14:51:03 -0400

    Added new safe pointer class, and arranged dependency access in modules to be done using these pointers.  Older GET_DEP macros still work fine, but the safe pointer access method is simpler and more efficient.  See ExampleBit_B for usage.
    Started to prepare rollcall system for integration with models.
    
    	modified:   ExampleBit_B/include/ExampleBit_B_rollcall.hpp
    	modified:   ExampleBit_B/src/ExampleBit_B.cpp
    	modified:   Utils/include/functors.hpp
    	modified:   Utils/include/module_macros.hpp
    	modified:   Utils/include/util_classes.hpp

--- modules/Utils/include/functors.hpp
+++ modules/Utils/include/functors.hpp
@@ -50,23 +50,23 @@ namespace GAMBIT
       // from within module functions
 
       /// Getter for the wrapped function's name
-      str name()        { return myName;       }
+      str name()        { if (this == NULL) failBigTime(); return myName;       }
       /// Getter for the wrapped function's reported capability
-      str capability()  { return myCapability; }
+      str capability()  { if (this == NULL) failBigTime(); return myCapability; }
       /// Getter for the wrapped function's reported return type
-      str type()        { return myType;       }
+      str type()        { if (this == NULL) failBigTime(); return myType;       }
       /// Getter for the wrapped function's origin (module or backend name)
-      str origin()      { return myOrigin;     }
+      str origin()      { if (this == NULL) failBigTime(); return myOrigin;     }
       /// Getter for the  version of the wrapped function's origin (module or backend)
-      str version()     { return myVersion;    }
+      str version()     { if (this == NULL) failBigTime(); return myVersion;    }
       /// Getter for the wrapped function current status (0 = disabled, 1 = available (default), 2 = active)
-      int status()      { return myStatus;     }
+      int status()      { if (this == NULL) failBigTime(); return myStatus;     }
       /// Getter for the  overall quantity provided by the wrapped function (capability-type pair)
-      sspair quantity() { return std::make_pair(myCapability, myType); }
+      sspair quantity() { if (this == NULL) failBigTime(); return std::make_pair(myCapability, myType); }
       /// Getter for obsType (relevant for output nodes, aka helper structures for the dep. resolution)
-      str obsType()     { return myObsType;    }
+      str obsType()     { if (this == NULL) failBigTime(); return myObsType;    }
       /// Setter for obsType (relevant only for next-to-output nodes)
-      void setObsType(str obsType) { this->myObsType = obsType; }
+      void setObsType(str obsType) { if (this == NULL) failBigTime(); this->myObsType = obsType; }
 
       /// Set method for version
       void setVersion(str ver) { myVersion = ver; }
@@ -249,6 +249,17 @@ namespace GAMBIT
       /// Map from (backend requirement-type pairs) to (vector of permitted {backend-version} pairs)
       std::map< sspair, std::vector<sspair> > permitted_map;
 
+      /// Attempt to retrieve a dependency or model parameter that has not been resolved
+      static void failBigTime()
+      {
+          cout << endl << "Error in module function!  Attempted to use a conditional " << endl;
+          cout << "dependency that has not been activated, or a model parameter " << endl;
+          cout << "that is not defined in the model for which this function has " << endl;
+          cout << "been invoked.  Please check your module function source code. " << endl;
+          exit(1);
+          /// FIXME \todo throw real error here                             
+      }
+
   };
 
 
@@ -301,14 +312,26 @@ namespace GAMBIT
         if(needs_recalculating)
         {
           if(usePointer)
-            myValue = * myPointer;
+            myValue = *myPointer;
           else
             this->myFunction(myValue);
         }
       }
 
       /// Operation (return value)
-      TYPE operator()() { return myValue; }
+      TYPE operator()() 
+      { 
+        if (this == NULL) functor::failBigTime();
+        return myValue;
+      }
+
+      /// Alternative to operation (returns a safe pointer to value)
+      safe_ptr<TYPE> valuePtr()
+      {
+        if (this == NULL) functor::failBigTime();
+        //return &myValue;
+        return safe_ptr<TYPE>(&myValue);
+      }
 
       /// Add a dependency (a beer for anyone who can explain why this-> is required here)
       void setDependency(str dep, str type, void(*resolver)(functor*), str obsType = "")
@@ -515,9 +538,19 @@ namespace GAMBIT
       /// 2) Operation (execute function and return value) 
       TYPE operator()(ARGS... args) 
       { 
+        if (this == NULL) functor::failBigTime();
         if(this->needs_recalculating) { myValue = this->myFunction(args...); }
         return myValue;
       }
+
+      /// 2) Alternative to operation (execute function return a pointer to value)
+      TYPE* valuePtr(ARGS... args)
+      {
+        if (this == NULL) functor::functor::failBigTime();
+        if(this->needs_recalculating) { myValue = this->myFunction(args...); }
+        return &myValue;
+      }
+
 
     protected:
 
@@ -555,6 +588,7 @@ namespace GAMBIT
       /// 2) Operation (execute function and return value) 
       void operator()(ARGS... args) 
       { 
+        if (this == NULL) functor::functor::failBigTime();
         if(this->needs_recalculating) { this->myFunction(args...); }
       }
 
--- modules/Utils/include/util_classes.hpp
+++ modules/Utils/include/util_classes.hpp
@@ -44,7 +44,54 @@ namespace GAMBIT
     ini_code(void (*unroll)()) { (*unroll)(); }
   };
   
-  
+  /// A safe pointer that throws an informative error if you try to dereference
+  /// it when nullified, and cannot be used to overwrite the thing it points to.
+  template <typename TYPE> 
+  class safe_ptr
+  {
+
+    public:
+
+      /// Construct-o-safe_ptr
+      safe_ptr(TYPE* in_ptr = NULL) { ptr = in_ptr; }
+
+      /// Set pointer
+      void set(TYPE* in_ptr) { ptr = in_ptr; }
+
+      /// Dereference pointer
+      const TYPE& operator*() const
+      { 
+        if (ptr == NULL) dieGracefully();
+        return *ptr;
+      }        
+
+      /// Access is allowed to const member functions only
+      const TYPE* operator->() const
+      { 
+        if (ptr == NULL) dieGracefully();
+        return ptr;
+      }        
+          
+    protected:
+
+      /// The actual underlying pointer, interpreted as a pointer to constant value
+      const TYPE* ptr;
+
+      /// Failure message invoked when the user tries to dereference a null safe_ptr
+      static void dieGracefully()
+      {
+        cout << endl << "You just tried to dereference a GAMBIT safe pointer that has value" << endl;
+        cout << "NULL.  Bad idea.  Probably you tried to retrieve a conditional" << endl;
+        cout << "dependency that has not been activated because the necessary condition" << endl;
+        cout << "has not been met, or you tried to access a model parameter for a model" << endl;
+        cout << "that you are not actually scanning.  This means there is a bug in one" << endl;
+        cout << "of your module functions." << endl;
+      }
+
+  };
+
+  //PS: Ben, the following classes belong somewhere else, as they are not general utility classes...
+
   /// A placeholder "MSSM" low-energy spectrum object. Defined at scale Q.
   struct MSSMspecQ
   {
