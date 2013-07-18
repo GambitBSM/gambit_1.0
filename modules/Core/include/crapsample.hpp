ed2e5eb698570813acb08cc33bb922631a544490
gmartine <gmartine@spacesheep.local>
2013-07-18 13:20:40 +0200

    update

--- modules/Core/include/crapsample.hpp
+++ modules/Core/include/crapsample.hpp
@@ -1,25 +1,94 @@
 #ifndef CRAPSAMPLE_HPP
 #define CRAPSAMPLE_HPP
 
-#include <gambit_scan.hpp>
+#include <master_like.hpp>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <ofstream>
 
-class CrappySampler
+class Ran
+{
+	private:
+		unsigned long long int u, v, w;
+		
+	public:
+		Ran(unsigned long long int j) : v(4101842887655102017LL), w(1)
+		{
+			u = j ^ v; int64();
+			v = u; int64(); int64();
+			w = v; int64(); int64();
+		}
+		inline unsigned long long int int64()
+		{
+			u = u * 2862933555777941757LL + 7046029254386353087LL;
+			v ^= v >> 17; v ^= v << 31; v ^= v >> 8;
+			w = 4294957665U*(w & 0xffffffff) + (w >> 32);
+			unsigned long long int x = u ^ (u << 21); x ^= x >> 35; x ^= x << 4;
+			return (x + v) ^ w;
+		}
+		inline double Doub(){return 5.42101086242752217E-20 * int64();}
+		inline unsigned int int32(){return (unsigned int)int64();}
+};
+	
+class CrapSample
 {
 private:
-	Gambit_Functor *like1;
-	Gambit_Functor *dlike1;
+	
 	
 public:
-	CrappySampler(Gambit_Functor *likein, Gambit_Functor *dlikein) : like1(likein), dlike1(dlikein) {}
-	
-	void mcmc()
+	void MetHas(MasterLike <double, vector <double>> &LogLike, char *name, int N)
 	{
-		double *a;
-		int i;
-		double result;
-		result = (*like1)(a);
-		result += (*dlike1)(a, i);
-		cout << "mcmc doing stuff" << endl;
+		ofstream out(name);
+		double chisq;
+		std::vector<double> a(ma);
+		std::vector<double> aNext(ma);
+		double ans, chisqnext;
+		int mult = 1;
+		int count = 0, total = 0;
+		
+		for (int i = 0; i < ma; i++)
+			a[i] = gDev.Doub();
+		
+		chisq = LogLike(a);
+		Ran gDev(0);
+		
+		cout << "Metropolis Hastings Algorthm Started\n" << "\tpoints = " << "\n\taccept ratio = " << endl;
+		
+		do
+		{
+
+			for (int i = 0; i < ma; i++)
+			{
+				aNext[i] = gDev.Doub();
+			}
+
+
+			chisqnext = LogLike(aNext);
+
+			ans = chisqnext - chisq;
+			if ((ans <= 0.0)||(-std::log(gDev.Doub()) >= ans))
+			{
+				out << mult << "   ";
+				for (int k = 0; k < ma; k++)
+				{
+					out << a[k] << "   ";
+					a[k] = aNext[k];
+				}
+				out << "   " << 2.0*chisq << endl;
+				
+				chisq = chisqnext;
+				mult = 1;
+				count++;
+				cout << "\033[2A\tpoints = " << count << "\n\taccept ratio = " << "               \033[15D" << (double)count/(double)total << endl;
+			}
+			else
+			{
+				mult++;
+			}
+		}
+		while(count < N);
 	}
 };
 
--- modules/Core/include/gambit_scan.hpp
+++ modules/Core/include/gambit_scan.hpp
@@ -1,47 +1,137 @@
-#ifndef GAMBITSCAN_HPP
-#define GAMBITSCAN_HPP
-#include <string>
+//  GAMBIT: Global and Modular BSM Inference Tool
+//  *********************************************
+/// \file
+///  Master Likelihood container
+///
+///  *********************************************
+///
+///  Authors
+///  =======
+///
+///  (add name and date if you modify)
+///
+///  \author Christoph Weniger (c.weniger@uva.nl)
+///  \date May 20 2013
+///  \date June 03 2013
+///
+///
+///  *********************************************
+
+#ifndef __gambit_scan_hpp__
+#define __gambit_scan_hpp__
+
 #include <vector>
-#include <cstring>
-#include <master_like.hpp>
+#include <functors.hpp>
+#include <graphs.hpp>
+#include <functors.hpp>
 
-//using namespace std;
-using namespace GAMBIT;
-
-struct Gambit_Functor
-{ 
-	MasterLike *a; 
-	std::string *params; 
-	std::string output; 
-	int defint;
-	int N;
-
-	Gambit_Functor (MasterLike *a, char *name, char **names, int N, int defint = 0) : a(a), output(name), N(N), defint(defint)
+namespace Gambit
+{
+	namespace Scanner
 	{
-		params = new std::string[N];
-		for (int i = 0; i < N; i++)
+		class Gambit_Scanner
 		{
-			params[i] = names[i];
+		protected:
+			vector <double> upper_limit;
+			vector <double> lower_limit;
+			vector <std::string> param_names;
+			Graphs::DependencyResolver *dependencyResolver;
+			vector <primary_model_functor *> functors;
+			
+			Gambit_Scanner (Graphs::DependencyResolver &a, IniParser::IniFile &inifile, std::string name) : dependencyResolver(&a)
+			{
+				param_names = iniFile.getParameterList();
+				for (vector<std::string>::iterator it = param_names.begin(); it != param_names.end(); ++it)
+				{
+					std::pair<double, double> range = iniFile.getParameterEntry< std::pair<double, double> >(*it, "range");
+					if (range.first > range.second)
+					{
+						double temp = range.first;
+						range.first = range.second;
+						range.second = temp;
+					}
+					lower_limit.push_back(range.first);
+					upper_limit.push_back(range.second);
+				}
+			}
+			
+			virtual int Run() = 0;
 		}
-	}
-	double operator () (double *in)
-	{
-		for (int i = 0; i < N; i++)
-			(*a)[params[i].c_str()] = in[i];
-		a->calculate();
-		return (*a)(output.c_str())[defint];
-	}
-	double operator () (double *in, int intin)
-	{
-		for (int i = 0; i < N; i++)
-			(*a)[params[i].c_str()] = in[i];
-		a->calculate();
-		return (*a)(output.c_str())[intin];
-	}
-	~Gambit_Functor ()
-	{
-		delete[] params;
-	}
+		
+		class Scanner_Function_Base
+		{
+		protected:
+			// Storage
+			std::vector<Graphs::VertexID> vertices;
+			Gambit::Scanner::Gambit_Scanner *parent
+			
+			
+		public:
+			// Construct MasterLike from Graphs Output and inifile
+			Scanner_Function_Base(Gambit::Scanner::Gambit_Scanner *a, int funcNum) : parent(a)
+			{
+				vertices = dependencyResolver.getObsLikeOrder();
+				int size = 0;
+				for (std::vector<Graphs::VertexID>::iterator it = vertices.begin(), it2 = vertices.begin; it != vertices.end(); ++it)
+				{
+					if (dependencyResolver->getIniEntry(*it)).obsType == key)
+					{
+						*(++it2) = *it;
+						size++;
+					}
+				}
+				vertices.resize(size);
+			}
+			
+			void Reset() {dependencyResolver.resetAll();}
+			
+			std::vector <std:string> GetParameterValues(){}
+		};
+		
+		template <class output, class input>
+		class Scanner_Function : public Scanner_Function_Base
+		{
+		public:
+			Scanner_Function (Graphs::DependencyResolver &a, std::string key) : Scanner_Function_Base (a, key) {}
+			
+			virtual output & operator () (input in)
+			{
+				//Ben stuff
+				//std::vector<Graphs::VertexID> OL = dependencyResolver.getObsLikeOrder();
+				output ret = 0;
+				for (std::vector<Graphs::VertexID>::iterator it = vertices.begin(); it != vertices.end(); ++it)
+				{
+					dependencyResolver.calcObsLike(*it);
+					//dependencyResolver.notifyOfInvalidation(*it);
+					output ret += dependencyResolver.getObsLike(*it);
+				}
+				dependencyResolver.resetAll();
+			}
+		};
+		
+		template <>
+		class Scanner_Function <double *, double *> : public Scanner_Function_Base
+		{
+		public:
+			Scanner_Function (Graphs::DependencyResolver &a, std::string key) : Scanner_Function_Base (a, key) {}
+			
+			virtual output & operator () (input in)
+			{
+				//Ben stuff
+				//std::vector<Graphs::VertexID> OL = dependencyResolver.getObsLikeOrder();
+				output ret = 0;
+				for (std::vector<Graphs::VertexID>::iterator it = vertices.begin(); it != vertices.end(); ++it)
+				{
+					dependencyResolver.calcObsLike(*it);
+					//dependencyResolver.notifyOfInvalidation(*it);
+					output ret += dependencyResolver.getObsLike(*it);
+				}
+				dependencyResolver.resetAll();
+			}
+		};
+		
+
+	};
 };
 
 #endif
