89c31b42804ce21229ad625335a78bb6643c536e
Pat Scott <patscott@physics.mcgill.ca>
2013-10-29 14:42:46 -0400

    Also commented out DarkSUSY backend in backend_rollcall.hpp to allow users without DarkSUSY to run the examples.  Just comment this back in in your local version if you want to use/test DarkSUSY. No, this isn't a good general system for dealing with absent libs; a system for automatically detecting present/absent shared backend libs and adjusting accordingly is in the pipeline.
    	modified:   Core/include/backend_rollcall.hpp

--- modules/Core/include/backend_rollcall.hpp
+++ modules/Core/include/backend_rollcall.hpp
@@ -39,7 +39,7 @@
 #include "backend_libfirst.hpp"
 #include "backend_libfortran.hpp"
 #include "backend_fakeSoftSUSY.hpp"
-#include "backend_DarkSUSY.hpp"
+//#include "backend_DarkSUSY.hpp"
 //#include "backend_SuperIso.hpp"
 
 // Get a bit fancy and include duplicate backends 
