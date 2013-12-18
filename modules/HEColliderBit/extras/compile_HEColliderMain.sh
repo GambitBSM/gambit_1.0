c330c0845a5fefc01d4c5263785dca6a8635c3a9
Andy Buckley <andy.buckley@cern.ch>
2013-12-18 18:13:34 +0100

    Mass migration/deletion of files from extras into new working dirs

--- modules/HEColliderBit/extras/compile_HEColliderMain.sh
+++
@@ -1,6 +0,0 @@
-g++ HEColliderMain.cpp Delphes3Backend.cpp Pythia8Backend.cpp -O3 \
-  -pedantic -Wall -Wshadow -Wno-long-long -fopenmp \
-  -I$BOOST -I$DELPHES3 -I$DELPHES3/external -I$PYTHIA8/include \
-  `root-config --cflags --libs --ldflags` -lEG \
-  -L$DELPHES3 -lDelphes -L$PYTHIA8/lib/archive -lpythia8 -llhapdfdummy \
-  -o HEColliderMain
