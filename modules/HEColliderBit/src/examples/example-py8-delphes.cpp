d8d14ae52b52f569b6843784ae480aae8606f8e4
Andy Buckley <andy.buckley@cern.ch>
2013-12-19 16:09:30 +0100

    Restructured threading to parallelize each sp block independently (sp blocks are run in series, so we get n_sp init times :-/)

--- modules/HEColliderBit/src/examples/example-py8-delphes.cpp
+++ modules/HEColliderBit/src/examples/example-py8-delphes.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <memory>
 #include <numeric>
+#include <algorithm>
 
 #ifdef ARCHIVE
 #include "boost/archive/text_oarchive.hpp"
@@ -23,21 +24,20 @@ using namespace std;
 namespace Gambit {
   namespace HEColliderBit {
 
-    struct SubprocessGroup {
-      SubprocessGroup()
-        : xsec(-1), nevts(-1) { }
-      SubprocessGroup(double xs, const vector<int>& parts1, const vector<int>& parts2)
-        : xsec(xs), nevts(-1), particlesInProcess1(parts1), particlesInProcess2(parts2) { }
+    struct Subprocess {
+      Subprocess()
+        : xsec(-1) { }
+      Subprocess(double xs, const vector<int>& parts1, const vector<int>& parts2)
+        : particlesInProcess1(parts1), particlesInProcess2(parts2), xsec(xs) { }
       void add_analysis(Analysis* a) { analyses.push_back(shared_ptr<Analysis>(a)); }
-      double xsec;
       // string name; // or int id = 1000*sp_type + sp_instance
-      int nevts;
       /// @todo Add some metric of CPU cost per event for this process type?
       /// The processes are selected by the IDs of the particles which
       /// must be in the process.
       vector<int> particlesInProcess1;
       vector<int> particlesInProcess2;
       vector<shared_ptr<Analysis>> analyses;
+      double xsec;
     };
 
 
@@ -82,50 +82,47 @@ int main() {
   /// @todo We'll eventually need more than just ATLAS, so Delphes/FastSim handling will need to be bound to analyses (and cached)
   /// @note That means that the class loaders had better be working by then...
   const string delphesConfigFile = "../data/delphes_card_ATLAS.tcl";
-  const int NEVENTS = 2000;
+  const int NUM_EVENTS = 2000;
 
-  cout << endl << "Running parallelized HECollider simulation" << endl;
+  cout << endl << "Running parallelized HECollider simulation for " << NUM_EVENTS << " events" << endl;
 
   // Global instance of Delphes, since it's not thread-safe
-  unique_ptr<Delphes3Backend> myDelphes = nullptr; //AB new Delphes3Backend(delphesConfigFile);
+  shared_ptr<Delphes3Backend> myDelphes;//(new Delphes3Backend(delphesConfigFile));
 
   // Subprocess group setup
-  // Decide how many events of each subprocess group to use, split according to number of threads
-  /// @todo Use fast lookup of interpolated NLO subprocess cross-sections to do this
+  /// Decide how many events of each subprocess group to use, from interpolated NLO subprocess cross-sections
   /// @note Hard-coded for now, so I can do *something*
-  map<string, SubprocessGroup> sp_groups;
-
+  map<string, double> sp_xsecs = {{"g~", 0.5}, {"q~", 0.2}, {"X~", 0.02}, {"t~", 0.1}};
+  double total_xsec = 0; for (const auto& ispx : sp_xsecs) total_xsec += ispx.second;
+  map<string, Subprocess> sps;
   // Gluino processes
-  sp_groups["g~"] = SubprocessGroup(0.4, //< xsec estimate
-                    {{1000021}},
-                    {{1000021, 1000001, 1000002, 1000003, 1000004,
-                               2000001, 2000002, 2000003, 2000004}});
-
+  sps["g~"] = Subprocess(sp_xsecs["g~"],
+                         {{1000021}},
+                         {{1000021, 1000001, 1000002, 1000003, 1000004,
+                                    2000001, 2000002, 2000003, 2000004}});
   // Squark processes
-  sp_groups["q~"] = SubprocessGroup(0.2, //< xsec estimate
-                    {{1000001, 1000002, 1000003, 1000004,
-                      2000001, 2000002, 2000003, 2000004}},
-                    {{1000001, 1000002, 1000003, 1000004,
-                      2000001, 2000002, 2000003, 2000004}});
-
+  sps["q~"] = Subprocess(sp_xsecs["q~"],
+                         {{1000001, 1000002, 1000003, 1000004,
+                           2000001, 2000002, 2000003, 2000004}},
+                         {{1000001, 1000002, 1000003, 1000004,
+                           2000001, 2000002, 2000003, 2000004}});
   // Gaugino processes
-  sp_groups["X~"] = SubprocessGroup(0.02, //< xsec estimate
-                    {{1000021, 1000001, 1000002, 1000003, 1000004,
-                               2000001, 2000002, 2000003, 2000004}},
-                    {{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}});
-
-  // // Stop processes
-  // sp_groups["t~"] = SubprocessGroup(1, {{1000006}},{{ 1000006}});
+  sps["X~"] = Subprocess(sp_xsecs["X~"],
+                         {{1000021, 1000001, 1000002, 1000003, 1000004,
+                                    2000001, 2000002, 2000003, 2000004}},
+                         {{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}});
+  // Stop processes
+  sps["t~"] = Subprocess(sp_xsecs["t~"], {{1000006}}, {{1000006}});
 
   // Bind subprocesses to analysis pointers
-  for (auto& sp_group : sp_groups) {
-    sp_group.second.add_analysis( mkAnalysis("ATLAS_0LEP") );
-    sp_group.second.add_analysis( mkAnalysis("ATLAS_0LEPStop_20invfb") );
-    // sp_group.second.add_analysis( mkAnalysis("ATLAS_1LEPStop_20invfb") ); //< buggy: segfaults in finalize
-    sp_group.second.add_analysis( mkAnalysis("ATLAS_2LEPStop_20invfb") );
-    sp_group.second.add_analysis( mkAnalysis("ATLAS_2bStop_20invfb") );
+  for (auto& isp : sps) {
+    auto& sp = isp.second;
+    sp.add_analysis( mkAnalysis("ATLAS_0LEP") );
+    sp.add_analysis( mkAnalysis("ATLAS_0LEPStop_20invfb") );
+    // sp.add_analysis( mkAnalysis("ATLAS_1LEPStop_20invfb") ); //< buggy: segfaults in finalize
+    sp.add_analysis( mkAnalysis("ATLAS_2LEPStop_20invfb") );
+    sp.add_analysis( mkAnalysis("ATLAS_2bStop_20invfb") );
   }
-
 
   /// @note Needs more complexity when we want to run different analyses with
   /// high acceptances for low-xsec subprocesses.
@@ -138,96 +135,85 @@ int main() {
   //    for low-xsecs. For instance:
   //      if (xsec * ideal_acceptance)[worst subprocess] < 0.01 (xsec * crappy_acceptance)[next-to-worst subprocess]
   // 3) The question then becomes: can we somehow estimate a priori ideal_acceptance and crappy_acceptance?
-  //
+
+  // Split up parallelized subprocess generation runs
   const int NUM_CORES = omp_get_max_threads();
-  const int num_events_per_thread = (int) ceil(NEVENTS / (double) NUM_CORES);
-  vector<SubprocessGroup> thread_cfgs;
-  thread_cfgs.reserve(NUM_CORES);
-  double total_xsec = 0;
-  for (const auto& sp_group : sp_groups) total_xsec += sp_group.second.xsec;
-  for (auto& sp_group : sp_groups) {
-    int sp_group_num_threads = (int) floor(NUM_CORES * sp_group.second.xsec / total_xsec);
-    if (sp_group_num_threads == 0) sp_group_num_threads = 1;
-    /// @todo Need to be able to discard negligible processes... defined how? (see above)
-    cout << sp_group.first << " processes => " << sp_group_num_threads << " threads" << endl;
-    for (int i = 0; i < sp_group_num_threads; ++i) {
-      thread_cfgs.push_back(sp_group.second);
-      thread_cfgs.back().nevts = num_events_per_thread;
-    }
-  }
-  const int NUM_THREADS = thread_cfgs.size();
-  cout << "Total #cores = " << NUM_CORES << ", #threads = " << NUM_THREADS << endl;
-  if (NUM_THREADS > NUM_CORES)
-    cerr << "WARNING: more Pythia instances are needed than there are CPUs. HEColliderBit will run SLOW." << endl;
-  omp_set_num_threads(NUM_THREADS);
-  cout << "Total target number of events = " << NEVENTS << endl;
+  vector<Subprocess> thread_cfgs(NUM_CORES);
+  for (auto& isp : sps) {
+    auto& sp = isp.second;
+    const int sp_num_events = (int) ceil(NUM_EVENTS * sp.xsec / total_xsec);
+    if (sp_num_events < NUM_CORES) continue; //< @note Skip subprocesses with fewer events than cores
+    const int sp_num_events_per_thread = (int) ceil(sp_num_events / (double) NUM_CORES);
+    for (int i = 0; i < NUM_CORES; ++i) thread_cfgs[i] = sp;
 
-  #pragma omp parallel shared(MAIN_SHARED) num_threads(NUM_THREADS)
-  {
-    // Py8 backend process configuration
-    const int NTHREAD = omp_get_thread_num();
-    unique_ptr<Pythia8Backend> myPythia( new Pythia8Backend(NTHREAD) );
-    myPythia->set("SLHA:file", slhaFileName);
-    myPythia->set("SUSY:idVecA", thread_cfgs[NTHREAD].particlesInProcess1);
-    myPythia->set("SUSY:idVecB", thread_cfgs[NTHREAD].particlesInProcess2);
+    // Run this subprocess' parallelized generation loop
+    #pragma omp parallel shared(MAIN_SHARED)
+    {
+      // Py8 backend process configuration
+      const int NTHREAD = omp_get_thread_num();
+      unique_ptr<Pythia8Backend> myPythia( new Pythia8Backend(NTHREAD) );
+      myPythia->set("SLHA:file", slhaFileName);
+      myPythia->set("SUSY:idVecA", thread_cfgs[NTHREAD].particlesInProcess1);
+      myPythia->set("SUSY:idVecB", thread_cfgs[NTHREAD].particlesInProcess2);
 
-    // Configure the persistency system
-    #ifdef ARCHIVE
-    ofstream outFile("tester_thread" + boost::lexical_cast<string>(NTHREAD) + ".dat");
-    boost::archive::text_oarchive outArchive(outFile);
-    #endif
-
-    // Print out some run details
-    /// @note We might want more CPU-heavy processes to run fewer events.
-    cout << " Target number of events for thread #" << NTHREAD << " = " << thread_cfgs[NTHREAD].nevts << endl;
-
-    // Run the event loop
-    int counter = 0;
-    GHEC::Event recoEvent;
-    assert(thread_cfgs[NTHREAD].nevts > 0);
-    for (counter = 0; counter < thread_cfgs[NTHREAD].nevts; counter++) {
-      // // Run Pythia8 and run Delphes (not thread safe)
-      // Pythia8::Event genEvent;
-      // myPythia->nextEvent(genEvent);
-      // #pragma omp critical
-      // {
-      //   myDelphes->processEvent(genEvent, recoEvent);
-      // }
-      // or just run Pythia...
-      myPythia->nextEvent(recoEvent);
-
-      // Run all analyses attached to the thread
-      for (shared_ptr<Analysis> ana : thread_cfgs[NTHREAD].analyses)
-        ana->analyze(recoEvent);
-
-      // Archive the recoEvent to file if persistency is enabled
+      // Configure the persistency system
       #ifdef ARCHIVE
-      outArchive << recoEvent;
+      ofstream outFile("tester_thread" + boost::lexical_cast<string>(NTHREAD) + ".dat");
+      boost::archive::text_oarchive outArchive(outFile);
       #endif
-    } // end event loop
 
-    // Record the (LO) cross-section on the analysis
-    /// @todo Combine same-process xsecs on the SP group, then set on the analyses before adding SPs?
-    thread_cfgs[NTHREAD].xsec = myPythia->xsec();
-    cout << "XSEC = " << myPythia->xsec() << endl;
-    for (shared_ptr<Analysis> ana : thread_cfgs[NTHREAD].analyses) {
-      cout << "Py8 xsec = " << myPythia->xsec() << " +- " << myPythia->xsecErr() << endl;
-      ana->improve_xsec(myPythia->xsec(), myPythia->xsecErr());
-      /// @todo Getting stupid values from Py8...
-    }
+      // Print out some run details
+      /// @note We might want more CPU-heavy processes to run fewer events.
+      // cout << " Target number of events for thread #" << NTHREAD << " = " << thread_cfgs[NTHREAD].nevts << endl;
+      // assert(thread_cfgs[NTHREAD].nevts > 0);
 
-    // Print out final run details
-    cout << "Thread #" << NTHREAD << " has run " << counter << " events" << endl;
+      // Run the event loop
+      GHEC::Event recoEvent;
+      for (int counter = 0; counter < sp_num_events_per_thread; counter++) {
+        // // Run Pythia8 and run Delphes (not thread safe)
+        // Pythia8::Event genEvent;
+        // myPythia->nextEvent(genEvent);
+        // #pragma omp critical
+        // {
+        //   myDelphes->processEvent(genEvent, recoEvent);
+        // }
+        // or just run Pythia...
+        cout << "FOO" << endl;
+        myPythia->nextEvent(recoEvent);
+        cout << "BAR" << endl;
 
-    #ifdef ARCHIVE
-    outFile.close();
-    #endif
-  } // end omp parallel block
+        // Run all analyses attached to the thread
+        for (shared_ptr<Analysis> ana : thread_cfgs[NTHREAD].analyses)
+          ana->analyze(recoEvent);
 
+        // Archive the recoEvent to file if persistency is enabled
+        #ifdef ARCHIVE
+        outArchive << recoEvent;
+        #endif
+      } // end event loop
+
+      // Record the (LO) cross-section on the analysis
+      /// @todo Combine same-process xsecs on the SP group, then set on the analyses before adding SPs?
+      thread_cfgs[NTHREAD].xsec = myPythia->xsec();
+      cout << "XSEC = " << myPythia->xsec() << endl;
+      for (shared_ptr<Analysis> ana : thread_cfgs[NTHREAD].analyses) {
+        cout << "Py8 xsec = " << myPythia->xsec() << " +- " << myPythia->xsecErr() << endl;
+        ana->improve_xsec(myPythia->xsec(), myPythia->xsecErr());
+        /// @todo Getting stupid values from Py8...
+      }
+
+      // Print out final run details
+      // cout << "Thread #" << NTHREAD << " has run " << counter << " events" << endl;
+
+      #ifdef ARCHIVE
+      outFile.close();
+      #endif
+    } // end omp parallel block
+  }
 
   /// Combine analysis acceptances from each subprocess
   map<string, shared_ptr<Analysis>> anas;
-  for (auto& spg : sp_groups) {
+  for (auto& spg : sps) {
     for (auto& a : spg.second.analyses) {
       const string aname = (!a->name.empty()) ? a->name : typeid(*a).name();
       if (anas.find(aname) == anas.end()) {
