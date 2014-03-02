3104ac9cb50a6a9eab8fc06a2da4580f2f30276f
Aldo F. Saavedra <a.saavedra@physics.usyd.edu.au>
2014-03-02 12:48:03 +0000

    Moved the Event,Jet,MCUtils,PIDCodes,PIDUtils,Particle,Vectors,FastJetUtils to contrib/hep_simple_lib They now belong to within the namespace HEP_Simple_Lib:: (Until Adelaide at least) Moved FastSim to contrib/fast_sim/ Added FastSim to the Backends

---
+++ modules/Backends/include/FastSim_types.hpp
@@ -0,0 +1,62 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Definitions of container classes
+///  for the FastSim backend.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Aldo Saavedra
+///          (a.saavedra@physics.usyd.edu.au)
+//  \date 2014 Feb
+///
+///  *********************************************
+
+
+#ifndef __FastSim_types_hpp__
+#define __FastSim_types_hpp__
+
+#include "FastSim.hpp"
+/*
+#include "Jet.hpp"
+
+namespace Gambit
+{
+
+    enum DetectorType { NOMINAL,ACERDET,FASTATLAS, FASTCMS, ATLAS2011, CMS2011};
+    // NOMINAL the energy response is smeared and the calorimeter parameters are the same as the ACERDET
+    // ACERDET the energy of the different particles is smeared by the respective functions as used in the ACERDET paper and the calorimeter parameters are
+    //         the same as the ACERDET paper
+    // the values below are not implemented yet
+    // FASTATLAS  will be defined with a simple smear functions but the calorimeter parameters will reflect the ATLAS detector
+    // FASTCMS    will be defined with a simple smear functions but the calorimeter parameters will reflect the CMS detector
+    // ATLAS2011  will try to reproduce the performance of the ATLAS detector
+    // CMS2011    will try to reproduce the performance of the CMS detector
+
+    using namespace std;
+
+    struct  FastSimEvent {
+
+      vector<HEColliderBit::Jet*> _jets;
+      vector<HEColliderBit::Jet*> _bjets;
+
+      vector<HEColliderBit::Particle*> _iso_electrons;
+      vector<HEColliderBit::Particle*> _iso_muons;
+      vector<HEColliderBit::Particle*> _iso_photons;
+      vector<HEColliderBit::Particle*> _noniso_electrons;
+      vector<HEColliderBit::Particle*> _noniso_muons;
+      vector<HEColliderBit::Particle*> _noniso_photons;
+
+      double _MET;
+      double _METPhi;
+
+    };
+
+
+}
+*/
+
+#endif /* defined __FastSim_types_hpp__ */
---
+++ modules/Backends/include/backend_FastSim.hpp
@@ -0,0 +1,117 @@
+/* 
+ * Example of how to use the macros in 'backend_macros.hpp' 
+ * to set up a backend for a specific library.
+ * 
+ * \author Aldo Saavedra
+ * \date 2014-02-27  
+ * 
+ */
+
+/* Specify the path to the shared library along with a backend name. */
+
+#define LIBPATH      "Backends/lib/libfastsim.so"
+#ifdef BACKENDRENAME
+  #define BACKENDNAME BACKENDRENAME
+#else
+  #define BACKENDNAME LibFastSim
+#endif
+#define VERSION 1.0
+
+
+/* The following macro loads the library (using dlopen) in LIBPATH 
+ * when this header file is included somewhere. */
+
+LOAD_LIBRARY
+
+/* Next we use macros BE_VARIABLE and BE_FUNCTION to load pointers 
+ * (using dlsym) to the variables and functions within the library.
+ *  
+ * The macros also create functors that wrap the library variables and functions.
+ * These are used by the Core for dependency resolution and to set up a suitable 
+ * interface to the library functions/variables at module level. */
+
+/* Syntax for BE_FUNCTION:
+ * BE_FUNCTION([choose function name], [type], [arguement types], "[exact symbol name]", "[choose capability name]")
+ * 
+ * The last argument (capability name) is optional. 
+ * If left out (as done in some of the examples below) it will default to "[backend name]_[function name]_capability"
+ * (e.g. "LibFirst_initialize_capability") */
+
+BE_FUNCTION(initialize, void, (fast_sim::DetectorType), "_ZN8fast_sim7FastSim4initENS_12DetectorTypeE")
+//BE_FUNCTION(someFunction, void, (), "_Z12someFunctionv", "someFunction")
+//BE_FUNCTION(returnResult, double, (), "_Z12returnResultv")
+//BE_FUNCTION(byRefExample, double, (double&), "_Z12byRefExampleRd", "refex")
+//BE_FUNCTION(byRefExample2, void, (double&, double), "_Z13byRefExample2Rdd", "refex2")
+
+/* We have now created the following:
+ *
+ * - Function pointers
+ * Gambit::Backends::LibFirst::initialize       type: void (*)(int)
+ * Gambit::Backends::LibFirst::someFunction     type: void (*)()
+ * Gambit::Backends::LibFirst::returnResult     type: double (*)()
+ *
+ * - Functors
+ * Gambit::Backends::LibFirst::Functown::initialize       type: Gambit::backend_functor<void,int>
+ * Gambit::Backends::LibFirst::Functown::someFunction     type: Gambit::backend_functor<void>
+ * Gambit::Backends::LibFirst::Functown::returnResult     type: Gambit::backend_functor<double>  */
+
+
+/* Syntax for BE_VARIABLE:
+ * BE_VARIABLE([choose variable name], [type], "[exact symbol name]", "[choose capability name]")  */
+
+//BE_VARIABLE(SomeInt, int, "someInt", "SomeInt")
+//BE_VARIABLE(SomeDouble, double, "someDouble", "SomeDouble")
+
+/* We have now created the following:
+ *
+ * - Pointers
+ * Gambit::Backends::LibFirst::SomeInt      type: int*
+ * Gambit::Backends::LibFirst::SomeDouble   type: double*
+ *
+ * - Functors
+ * Gambit::Backends::LibFirst::Functown::SomeInt      type: Gambit::backend_functor<int>
+ * Gambit::Backends::LibFirst::Functown::SomeDouble   type: Gambit::backend_functor<double> */
+
+
+/* At this point we have a minimal interface to the loaded library.
+ * Any additional convenince functions could be constructed below 
+ * using the available pointers. All convenience functions must be
+ * registred/wrapped via the macro BE_CONV_FUNCTION (see below). */
+
+
+//namespace Gambit
+//{
+//  namespace Backends
+//  {
+//    namespace BACKENDNAME
+//    {
+
+//      /* Convenience functions go here */
+
+//      double awesomenessByAnders(int a)
+//      {
+//        initialize(a);
+//        someFunction();
+//        return returnResult();
+//      }
+
+//    } /* end namespace BACKENDNAME */                                          
+//  } /* end namespace Backends */                                                
+//} /* end namespace Gambit */                                                   
+
+
+/* Now register any convenience functions and wrap them in functors. 
+ *
+ * Syntax for BE_CONV_FUNCTION:
+ * BE_CONV_FUNCTION([function name], type, "[choose capability name]")
+ * 
+ * As with BE_FUNCTION, the last argument is optional. */
+
+//BE_CONV_FUNCTION(awesomenessByAnders, double, "awesomeness")
+
+
+// Undefine macros to avoid conflict with other backends
+#undef LIBPATH 
+#undef BACKENDNAME
+#undef VERSION
+
