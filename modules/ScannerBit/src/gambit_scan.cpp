535ed7cf57a140f5438b8959f2f2cc3d5b019a44
gmartine <gregory.david.martinez@gmail.com>
2014-03-11 04:09:59 +0100

    minor updates to the plugin interface class

--- modules/ScannerBit/src/gambit_scan.cpp
+++ modules/ScannerBit/src/gambit_scan.cpp
@@ -36,7 +36,6 @@ namespace Gambit
                                 scanLog::err << "Did not specify module library path." << scanLog::endl;
                         }
                         
-                        std::string version = "";
                         unsigned int dim = factory.getDim();
                         auto keys = factory.getKeys();
                         std::vector<void *> input(4);
@@ -45,12 +44,11 @@ namespace Gambit
                         input[2] = (void *)&factory;
                         input[3] = (void *)&interface;
                         
-                        Plugin::Plugin_Interface<int ()> plugin_interface(interface.fileName(), interface.pluginName(), version, &input);
                         scanLog::err.check();
-
                         outputHandler::out.redir("scanner");
                         try
                         {
+                                Plugin::Plugin_Interface<int ()> plugin_interface(interface.fileName(), interface.pluginName(), &input);
                                 plugin_interface();
                         }
                         catch (Plugin::PluginException exception)
@@ -60,31 +58,6 @@ namespace Gambit
                         outputHandler::out.defout();
                         scanLog::err.check();
                         
-                                //could do this ...
-                                
-                                //let's use the "LogLikelihood" function from crapsample! ...
-                                //std::vector<double> some_vector;
-                                //typedef double (*funcType)(std::vector<double> &);
-                                //funcType func = (funcType) interface.getMember("func");
-                                //double some_value = func(some_vector); 
-                                
-                                //Or let's use the ran_export class from crapsample! ...
-                                
-                                //typedef class {public: virtual double Num()=0;} randomClass;
-                                //randomClass *ptr = (randomClass *)interface.getMember("random");
-                                //double some_value2 = ptr->Num();
-                                //interface.deleteMember((void *)ptr, "random"); 
-                                
-                                //std::cout << "real value = " << ptr->Num() << "   " << ptr->Num() << std::endl;
-                                
-                                //or this get the interger ...
-                        
-                                //int *ptrt = (int *)interface.getMember("temp");
-                                
-                                //and output stuff
-                        
-                                //std::cout << "temp = " << some_value2 << "   " << (*ptrt) << "   " << some_value << std::endl;
-                
                         return 0;
                 }
                 
