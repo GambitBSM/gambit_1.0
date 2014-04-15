8223beb87151cdf1155abaaccac0be74431bfbab
Andy Buckley <andy.buckley@cern.ch>
2014-04-15 12:22:05 +0200

    Comparison script rewritten to make comparisons to xsecs for more process groups

--- modules/HEColliderBit/extras/xsec/eval_xsec.cpp
+++ modules/HEColliderBit/extras/xsec/eval_xsec.cpp
@@ -4,24 +4,29 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  assert(argc = 2 && "Usage: eval_xsec <slhafile>");
+  if (argc != 2) {
+    cout << "Usage: eval_xsec <slhafile>" << endl;
+    exit(1);
+  }
+
+  // Make the xsec predictor
   xsec::Evaluator ev;
 
-  // Gluino-gluino
+  // Gluino-gluino (ATLAS finalState = 2)
   const double xs_gg = ev.xsec({{1000021}}, {{1000021}}, argv[1]);
   // Neutralino/chargino + gluino
   const double xs_xg = ev.xsec({{1000021}}, {{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}}, argv[1]);
   // Neutralino/chargino pairs
   const double xs_xx = ev.xsec({{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}},
                                {{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}}, argv[1]);
-  // Squark-gluino (TODO: what about bg and tg?)
+  // Squark-gluino (TODO: what about bg and tg?) (ATLAS finalState = 1)
   const double xs_qg = ev.xsec({{1000021}}, {{1000001, 1000002, 1000003, 1000004,
                                               2000001, 2000002, 2000003, 2000004}}, argv[1]);
   // Neutralino/chargino + squark
   const double xs_xq = ev.xsec({{1000022, 1000023, 1000024, 1000025, 1000035, 1000037}},
                                {{1000001, 1000002, 1000003, 1000004,
                                  2000001, 2000002, 2000003, 2000004}}, argv[1]);
-  // Light squark-antisquark and squark-squark pairs
+  // Light squark-squark and squark-antisquark pairs (ATLAS finalState = 3,4)
   const double xs_qq = ev.xsec({{1000001, 1000002, 1000003, 1000004,
                                  2000001, 2000002, 2000003, 2000004}},
                                {{1000001, 1000002, 1000003, 1000004,
@@ -34,8 +39,8 @@ int main(int argc, char* argv[]) {
   const double xs_ll = ev.xsec({{1000011, 1000012, 1000013, 1000014, 1000015, 1000016}},
                                {{1000011, 1000012, 1000013, 1000014, 1000015, 1000016}}, argv[1]);
 
-  cout << xs_gg << endl;
-  // cout << xs_gg << " " << xs_xg << " " << xs_xx << " " << xs_qg << " "
-  //      << xs_xq << " " << xs_qq << " " << xs_bb << " " << xs_tt << " " << xs_ll << endl;
+  // cout << xs_gg << endl;
+  cout << xs_gg << " " << xs_xg << " " << xs_xx << " " << xs_qg << " "
+       << xs_xq << " " << xs_qq << " " << xs_bb << " " << xs_tt << " " << xs_ll << endl;
   return 0;
 }
