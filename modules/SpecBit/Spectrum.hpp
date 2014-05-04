12bd86232bd5b36734ce779f8a3e067ce54020bc
Peter Athron <peter.athron@adelaide.edu.au>
2014-05-05 00:14:48 +0930

    added MSSM implentation of abstract class methods for getting DRbar mass parameters. Created thre verions to take 0,1 or two indices.

--- modules/SpecBit/Spectrum.hpp
+++ modules/SpecBit/Spectrum.hpp
@@ -40,7 +40,11 @@ public:
    virtual double get_Mixing_angle(std::string) const = 0;
    virtual double get_Mixing_element(std::string, int, int) const = 0;
    virtual double get_mass2_parameter(std::string) const = 0;
+   virtual double get_mass2_parameter(std::string, int) const = 0;
+   virtual double get_mass2_parameter(std::string, int, int) const = 0;
    virtual double get_mass_parameter(std::string) const = 0;
+   virtual double get_mass_parameter(std::string, int) const = 0;
+   virtual double get_mass_parameter(std::string, int, int) const = 0;
    virtual double get_dimensionless_parameter(std::string) const = 0;
    virtual double get_dimensionless_parameter(std::string, int) const = 0;
    virtual double get_dimensionless_parameter(std::string, int, int) const = 0;
