50039d921a8487c83552750a2352fd3a56bda327
Pat Scott <patscott@physics.mcgill.ca>
2014-05-28 19:06:44 +1000

    Small tweak to make ExampleBit_A compile with icc again.
    	modified:   ExampleBit_A/src/ExampleBit_A.cpp

--- modules/ExampleBit_A/src/ExampleBit_A.cpp
+++ modules/ExampleBit_A/src/ExampleBit_A.cpp
@@ -331,7 +331,7 @@ namespace Gambit
       
       // Will not compile without the byVal convertor.
       cout << endl << "Calling fptrRoutine with commonblock elements a and c and function doubleFuncArray1 as arguments in a way that fails without byVal or dereferencing of the function pointer..." << endl;
-      BEreq::libFarrayTest_fptrRoutine(commonBlock.a,*commonBlock.c,*function_pointer);      
+      BEreq::libFarrayTest_fptrRoutine(commonBlock.a,*commonBlock.c,byVal(function_pointer));      
  
       result = 1.0;     
  
