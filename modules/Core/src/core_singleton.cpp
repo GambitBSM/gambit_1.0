e80ee2e0ba2a44314db5f13f3794403cc75df985
Pat Scott <patscott@physics.mcgill.ca>
2014-08-11 16:58:52 +0100

    Added new files from last commit.
    	new file:   Core/include/core_singleton.hpp
    	new file:   Core/src/core_singleton.cpp
    	new file:   Models/include/claw_singleton.hpp
    	new file:   Models/src/claw_singleton.cpp
    	new file:   Utils/include/backend_singleton.hpp
    	new file:   Utils/src/backend_singleton.cpp

---
+++ modules/Core/src/core_singleton.cpp
@@ -0,0 +1,30 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  GAMBIT Core object accessor function.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Pat Scott
+///  \date 2014 Aug
+///
+///  *********************************************
+
+#include "core_singleton.hpp"
+#include "claw_singleton.hpp"
+#include "backend_singleton.hpp"
+
+namespace Gambit
+{
+
+  /// Core accessor function
+  gambit_core& Core()
+  {
+    static gambit_core local(Models::modelClaw(), Backends::backendInfo());
+    return local;
+  }
+
+}
