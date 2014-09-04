3e939f26de24ce641a12276171dbf34a2066c4d1
Benjamin Farmer <farmer@m1-login1.(none)>
2014-09-04 15:55:50 +1000

    Started 'gambitizing' of SpecBit

---
+++ modules/SpecBit/src/SpecBit.cpp
@@ -0,0 +1,33 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Functions of module SpecBit
+///
+///  These functions link ModelParameters to 
+///  Spectrum objects in various ways.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///
+///  \author Ben Farmer
+///          (ben.farmer@gmail.com)
+///    \date 2014 Sep
+///  
+///  *********************************************
+
+#include "gambit_module_headers.hpp"
+#include "SpecBit_rollcall.hpp"
+
+namespace Gambit
+{
+
+  namespace SpecBit
+  {
+    using namespace LogTags;
+
+
+  } // end namespace SpecBit
+} // end namespace SpecBit
+
