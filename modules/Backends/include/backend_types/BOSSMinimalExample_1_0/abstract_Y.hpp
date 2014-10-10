f2ab0ee337c4a38b6a0a3bc4d56e8062bdda2e79
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2014-10-10 09:59:52 +0200

    Updated the backend examples that test classloading with BOSS. They now correspond to the latest BOSS output

--- modules/Backends/include/backend_types/BOSSMinimalExample_1_0/abstract_Y.hpp
+++ modules/Backends/include/backend_types/BOSSMinimalExample_1_0/abstract_Y.hpp
@@ -1,44 +1,68 @@
 #ifndef __ABSTRACT_Y_BOSSMinimalExample_1_0_HPP__
 #define __ABSTRACT_Y_BOSSMinimalExample_1_0_HPP__
 
+#include "abstractbase.hpp"
 #include "forward_decls_abstract_classes.hpp"
+#include "forward_decls_wrapper_classes.hpp"
 #include "abstract_X.hpp"
+
 #include "identification.hpp"
 
 namespace CAT_3(BACKENDNAME,_,SAFE_VERSION)
 {
-
-  namespace nspace3
-  {
-
-    class Abstract_Y
+    
+    
+    // Forward declaration needed by the destructor pattern.
+    void wrapper_deleter(nspace3::Y*);
+    
+    namespace nspace3
     {
-      public:
-
-        virtual nspace1::nspace2::Abstract_X& x_ref_GAMBIT() =0;
-
-        virtual nspace1::nspace2::Abstract_X* get_x_GAMBIT() =0;
-        nspace1::nspace2::Abstract_X* get_x()
+        class Abstract_Y : virtual public AbstractBase
         {
-            return get_x_GAMBIT();
-        }
-
-        virtual void set_x_GAMBIT(nspace1::nspace2::Abstract_X&) =0;
-        void set_x(nspace1::nspace2::Abstract_X& x_in)
-        {
-            set_x_GAMBIT(x_in);
-        }
-
-      public:
-        virtual void pointerAssign_GAMBIT(Abstract_Y*) =0;
-        virtual Abstract_Y* pointerCopy_GAMBIT() =0;
-        virtual ~Abstract_Y() {};
-    };
-
-  }
-
+            public:
+    
+                virtual nspace1::nspace2::Abstract_X& x_ref_GAMBIT() =0;
+    
+                virtual nspace1::nspace2::Abstract_X* get_x_GAMBIT() =0;
+    
+                virtual void set_x_GAMBIT(nspace1::nspace2::Abstract_X&) =0;
+    
+                virtual void set_x_ptr_GAMBIT(nspace1::nspace2::Abstract_X*) =0;
+    
+            public:
+                virtual void pointerAssign_GAMBIT(Abstract_Y*) =0;
+                virtual Abstract_Y* pointerCopy_GAMBIT() =0;
+    
+            private:
+                Y* wptr;
+    
+            public:
+                void wrapper_GAMBIT(Y* wptr_in)
+                {
+                    wptr = wptr_in;
+                    is_wrapped(true);
+                }
+    
+                Y* wrapper_GAMBIT()
+                {
+                    return wptr;
+                }
+    
+                virtual ~Abstract_Y()
+                {
+                    if (can_delete_wrapper())
+                    {
+                        can_delete_me(false);
+                        wrapper_deleter(wptr);
+                    }
+                }
+        };
+    }
+    
 }
+
 
 #include "backend_undefs.hpp"
 
-#endif /* ABSTRACT_Y_BOSSMinimalExample_1_0_HPP__ */
+
+#endif /* __ABSTRACT_Y_BOSSMinimalExample_1_0_HPP__ */
---
+++ modules/Backends/include/backend_types/BOSSMinimalExample_1_2/forward_decls_wrapper_classes.hpp
@@ -0,0 +1,19 @@
+#ifndef __FORWARD_DECLS_WRAPPER_CLASSES_BOSSMinimalExample_1_2_HPP__
+#define __FORWARD_DECLS_WRAPPER_CLASSES_BOSSMinimalExample_1_2_HPP__
+
+
+#include "identification.hpp"
+
+namespace CAT_3(BACKENDNAME,_,SAFE_VERSION)
+{
+    
+    
+    class X;
+    class Y;
+    
+}
+
+
+#include "backend_undefs.hpp"
+
+#endif /* __FORWARD_DECLS_WRAPPER_CLASSES_BOSSMinimalExample_1_2_HPP__ */
