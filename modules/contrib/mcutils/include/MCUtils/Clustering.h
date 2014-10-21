4f28b2e5378e7efa911dd58c5a994e5a1c9c5d24
Andy Buckley <andy.buckley@cern.ch>
2014-10-21 20:46:01 +0100

    Convert MCUtils and HEP_Simple_Lib to new HEPUtils+MCUtils packages

---
+++ modules/contrib/mcutils/include/MCUtils/Clustering.h
@@ -0,0 +1,73 @@
+// -*- C++ -*-
+//
+// This file is part of MCUtils -- https://bitbucket.org/andybuckley/mcutils
+// Copyright (C) 2013-2014 Andy Buckley <andy.buckley@cern.ch>
+//
+// Embedding of MCUtils code in other projects is permitted provided this
+// notice is retained and the MCUtils namespace and include path are changed.
+//
+#pragma once
+
+/// @file FastJet clustering on HepMC events
+/// @author Andy Buckley <andy.buckley@cern.ch>
+
+#include "HEPUtils/FastJet.h"
+#include "MCUtils/HepMCEventUtils.h"
+
+namespace MCUtils {
+
+
+  /// @name Converters between HepMC and FastJet momentum types
+  //@{
+
+  /// Convert a HepMC FourVector to a FastJet PseudoJet
+  inline fastjet::PseudoJet mk_pseudojet(const HepMC::FourVector& p4) {
+    return fastjet::PseudoJet(p4.px(), p4.py(), p4.pz(), p4.e());
+  }
+
+  /// Convert a HepMC GenParticle to a FastJet PseudoJet
+  inline fastjet::PseudoJet mk_pseudojet(const HepMC::GenParticle* gp) {
+    fastjet::PseudoJet pj = mk_pseudojet(gp->momentum());
+    //pj.set_user_info(new HepMCInfo(p));
+    return pj;
+  }
+
+  /// Convert a vector of HepMC GenParticles to a vector of FastJet PseudoJets
+  inline std::vector<fastjet::PseudoJet> mk_pseudojets(const std::vector<const HepMC::GenParticle*>& gps) {
+    std::vector<fastjet::PseudoJet> pjs;
+    BOOST_FOREACH (const HepMC::GenParticle* gp, gps) {
+      pjs.push_back( mk_pseudojet(gp) );
+    }
+    return pjs;
+  }
+
+  //@}
+
+
+  /// @name Jet builders from HepMC input types
+  //@{
+
+  /// Construct pT-sorted jets using the @a alg measure with jet @a R parameter, and min pT @a ptmin (in MeV)
+  inline std::vector<fastjet::PseudoJet> get_jets(const GenParticlesC& particles, double R, double ptmin,
+                                                  fastjet::JetAlgorithm alg=fastjet::antikt_algorithm) {
+    /// @todo Convert to use filter_jet_inputs
+    return get_jets(mk_pseudojets(filter_stable(particles)), R, ptmin, alg);
+  }
+
+
+  /// Construct pT-sorted jets using the @a alg measure with jet @a R parameter, and min pT @a ptmin (in MeV)
+  inline std::vector<fastjet::PseudoJet> get_jets(const HepMC::GenEvent* evt, double R, double ptmin,
+                                                  fastjet::JetAlgorithm alg=fastjet::antikt_algorithm) {
+    /// @todo Convert to use get_jet_inputs
+    return get_jets(mk_pseudojets(get_stable(evt)), R, ptmin, alg);
+  }
+
+  //@}
+
+
+  /// @todo Make safe photon clustering for charged leptons
+
+  /// @todo Make tau, B, top and W/Z finders (both safe and direct versions)
+
+
+}
---
+++ modules/contrib/mcutils/include/MCUtils/HepMCParticleClassifiers.h
@@ -0,0 +1,557 @@
+// -*- C++ -*-
+//
+// This file is part of MCUtils -- https://bitbucket.org/andybuckley/mcutils
+// Copyright (C) 2013-2014 Andy Buckley <andy.buckley@cern.ch>
+//
+// Embedding of MCUtils code in other projects is permitted provided this
+// notice is retained and the MCUtils namespace and include path are changed.
+//
+#pragma once
+
+/// @file Functions for filtering and classifying HepMC record contents
+/// @author Andy Buckley <andy.buckley@cern.ch>
+/// @author Nataliia Kondrashova <Nataliia.Kondrashova@cern.ch>
+
+#include "HepMC/GenEvent.h"
+#include "boost/function.hpp" //< Replace with std::function when possible
+#include <vector>
+
+#include "MCUtils/HepMCEventUtils.h"
+#include "MCUtils/PIDUtils.h"
+
+// Macro to map HepMC functions to PID:: functions of the same name
+#define HEPMC_TO_PID_FN(fname) fname (const HepMC::GenParticle* p) { return PID:: fname (p->pdg_id()); }
+
+namespace MCUtils {
+
+  using HEPUtils::in_range;
+
+  // Fwd declaration
+  bool _isDecayedHFHadronOrTau(const HepMC::GenParticle* p);
+
+
+  /// @name GenParticle classifier functions
+  //@{
+
+  /// Convenient type name for a generic classifier function / function object
+  typedef boost::function<bool(const HepMC::GenParticle*)> PClassifier;
+
+
+  /// Is this particle species charged?
+  inline bool HEPMC_TO_PID_FN(isCharged)
+
+  /// Is this particle species neutral?
+  inline bool HEPMC_TO_PID_FN(isNeutral)
+
+
+  /// Is this a neutrino?
+  inline bool HEPMC_TO_PID_FN(isNeutrino)
+
+  /// Determine if the PID is that of a charged lepton
+  inline bool HEPMC_TO_PID_FN(isChLepton)
+
+  /// Determine if the PID is that of a photon
+  inline bool HEPMC_TO_PID_FN(isPhoton)
+
+  /// Determine if the PID is that of an electron or positron
+  inline bool HEPMC_TO_PID_FN(isElectron)
+
+  /// Determine if the PID is that of an muon or antimuon
+  inline bool HEPMC_TO_PID_FN(isMuon)
+
+  /// Determine if the PID is that of an tau or antitau
+  inline bool HEPMC_TO_PID_FN(isTau)
+
+  /// Determine if the PID is that of a hadron
+  inline bool HEPMC_TO_PID_FN(isHadron)
+
+  /// Determine if the PID is that of a meson
+  inline bool HEPMC_TO_PID_FN(isMeson)
+
+  /// Determine if the PID is that of a baryon
+  inline bool HEPMC_TO_PID_FN(isBaryon)
+
+  /// Determine if the PID is that of a quark
+  inline bool HEPMC_TO_PID_FN(isQuark)
+
+  /// Determine if the PID is that of a gluon
+  inline bool HEPMC_TO_PID_FN(isGluon)
+
+  /// Determine if the PID is that of a parton (quark or gluon)
+  inline bool HEPMC_TO_PID_FN(isParton)
+
+
+
+  /// Determine if the PID is that of a W+
+  inline bool HEPMC_TO_PID_FN(isWplus)
+
+  /// Determine if the PID is that of a W-
+  inline bool HEPMC_TO_PID_FN(isWminus)
+
+  /// Determine if the PID is that of a W+-
+  inline bool HEPMC_TO_PID_FN(isW)
+
+  /// Determine if the PID is that of a Z0
+  inline bool HEPMC_TO_PID_FN(isZ)
+
+  /// Determine if the PID is that of an SM/lightest SUSY Higgs
+  inline bool HEPMC_TO_PID_FN(isHiggs)
+
+  /// Determine if the PID is that of a t/tbar
+  inline bool HEPMC_TO_PID_FN(isTop)
+
+
+  /// Determine if the particle is a heavy flavour hadron or parton
+  inline bool HEPMC_TO_PID_FN(isHeavyFlavour)
+
+  /// Determine if the PID is that of a heavy parton (c,b,t)
+  inline bool HEPMC_TO_PID_FN(isHeavyParton)
+
+  /// Determine if the PID is that of a light parton (u,d,s)
+  inline bool HEPMC_TO_PID_FN(isLightParton)
+
+
+  /// Determine if the PID is that of a heavy flavour (b or c) meson
+  inline bool HEPMC_TO_PID_FN(isHeavyMeson)
+
+  /// Determine if the PID is that of a heavy flavour (b or c) baryon
+  inline bool HEPMC_TO_PID_FN(isHeavyBaryon)
+
+  /// Determine if the PID is that of a heavy flavour (b or c) hadron
+  inline bool HEPMC_TO_PID_FN(isHeavyHadron)
+
+
+  /// Determine if the PID is that of a light flavour (not b or c) meson
+  inline bool HEPMC_TO_PID_FN(isLightMeson)
+
+  /// Determine if the PID is that of a light flavour (not b or c) baryon
+  inline bool HEPMC_TO_PID_FN(isLightBaryon)
+
+  /// Determine if the PID is that of a light flavour (not b or c) hadron
+  inline bool HEPMC_TO_PID_FN(isLightHadron)
+
+
+  /// Determine if the PID is that of a b-meson.
+  inline bool HEPMC_TO_PID_FN(isBottomMeson)
+
+  /// Determine if the PID is that of a b-baryon.
+  inline bool HEPMC_TO_PID_FN(isBottomBaryon)
+
+  /// Determine if the PID is that of a b-hadron.
+  inline bool HEPMC_TO_PID_FN(isBottomHadron)
+
+
+  /// @brief Determine if the PID is that of a c-meson.
+  ///
+  /// Specifically, the _heaviest_ quark is a c: a B_c is a b-meson and NOT a c-meson.
+  /// Charmonia (closed charm) are counted as c-mesons here.
+  inline bool HEPMC_TO_PID_FN(isCharmMeson)
+
+  /// @brief Determine if the PID is that of a c-baryon.
+  ///
+  /// Specifically, the _heaviest_ quark is a c: a baryon containing a b & c
+  /// is a b-baryon and NOT a c-baryon. To test for the simpler case, just use
+  /// a combination of hasCharm() and isBaryon().
+  inline bool HEPMC_TO_PID_FN(isCharmBaryon)
+
+  /// Determine if the PID is that of a c-hadron.
+  inline bool HEPMC_TO_PID_FN(isCharmHadron)
+
+
+  /// Determine if the PID is that of a strange meson
+  inline bool HEPMC_TO_PID_FN(isStrangeMeson)
+
+  /// Determine if the PID is that of a strange baryon
+  inline bool HEPMC_TO_PID_FN(isStrangeBaryon)
+
+  /// Determine if the PID is that of a strange hadron
+  inline bool HEPMC_TO_PID_FN(isStrangeHadron)
+
+
+
+  /// Is this a pomeron, odderon, or generic reggeon?
+  inline bool HEPMC_TO_PID_FN(isReggeon)
+
+  /// Determine if the PID is that of a diquark (used in hadronization models)
+  inline bool HEPMC_TO_PID_FN(isDiquark)
+
+  /// Determine if the PID is that of a pentaquark (hypothetical hadron)
+  inline bool HEPMC_TO_PID_FN(isPentaquark)
+
+  /// Is this a fundamental SUSY particle?
+  inline bool HEPMC_TO_PID_FN(isSUSY)
+
+  /// Is this an R-hadron?
+  inline bool HEPMC_TO_PID_FN(isRhadron)
+
+  /// Is this a technicolor particle?
+  inline bool HEPMC_TO_PID_FN(isTechnicolor)
+
+  /// Is this an excited (composite) quark or lepton?
+  inline bool HEPMC_TO_PID_FN(isExcited)
+
+  /// Is this a Kaluza-Klein excitation?
+  inline bool HEPMC_TO_PID_FN(isKK)
+
+  /// Is this a graviton?
+  inline bool HEPMC_TO_PID_FN(isGraviton)
+
+  /// Is this a BSM particle (including graviton)?
+  inline bool HEPMC_TO_PID_FN(isBSM)
+
+
+
+  /// Determine if the PID is in the generator-specific range
+  inline bool HEPMC_TO_PID_FN(isGenSpecific)
+
+  /// Determine if the PID is that of an EW scale resonance
+  inline bool HEPMC_TO_PID_FN(isResonance)
+
+  /// Check the PID for usability in transport codes like Geant4
+  inline bool HEPMC_TO_PID_FN(isTransportable)
+
+
+
+  /// Does this particle contain an up quark?
+  inline bool HEPMC_TO_PID_FN(hasUp)
+
+  /// Does this particle contain a down quark?
+  inline bool HEPMC_TO_PID_FN(hasDown)
+
+  /// Does this particle contain a strange quark?
+  inline bool HEPMC_TO_PID_FN(hasStrange)
+
+  /// Does this particle contain a charm quark?
+  inline bool HEPMC_TO_PID_FN(hasCharm)
+
+  /// Does this particle contain a bottom quark?
+  inline bool HEPMC_TO_PID_FN(hasBottom)
+
+  /// Does this particle contain a top quark?
+  inline bool HEPMC_TO_PID_FN(hasTop)
+
+
+  /// Does this particle interact strongly?
+  inline bool HEPMC_TO_PID_FN(isStrongInteracting)
+
+  /// Does this particle interact electromagnetically?
+  inline bool HEPMC_TO_PID_FN(isEMInteracting)
+
+  /// Does this particle interact weakly?
+  inline bool HEPMC_TO_PID_FN(isWeakInteracting)
+
+
+  /// jSpin returns 2J+1, where J is the total spin
+  inline int HEPMC_TO_PID_FN(jSpin)
+
+  /// sSpin returns 2S+1, where S is the spin
+  inline int HEPMC_TO_PID_FN(sSpin)
+
+  /// lSpin returns 2L+1, where L is the orbital angular momentum
+  inline int HEPMC_TO_PID_FN(lSpin)
+
+
+  /// Return 3 times the charge (3 x quark charge is an int)
+  inline int HEPMC_TO_PID_FN(threeCharge)
+
+  /// Return the charge
+  inline double HEPMC_TO_PID_FN(charge)
+
+
+  /// @brief Is this particle the first in the 'decay' chain to match property F?
+  ///
+  /// i.e. F(const GenParticle* p) is true, and p has no parents which match F
+  template <typename F>
+  inline bool isFirstWith(const HepMC::GenParticle* p, const F& classifier) {
+    if (!F(p)) return false; // p doesn't have property F so it can't be the first particle with it
+    if (!p->production_vertex()) return true; // p has no parents, so it's the first
+    BOOST_FOREACH (const HepMC::GenParticle* parent, const_parents(p->production_vertex())) {
+      if (F(parent)) return false; // if its parent has property F, p can't be the first
+    }
+    return true; // p has F and no parents do
+  }
+
+  /// @brief Is this particle the last in the 'decay' chain to match property F?
+  ///
+  /// i.e. F(const GenParticle* p) is true, and p has no children which match F
+  template <typename F>
+  inline bool isLastWith(const HepMC::GenParticle* p, const F& classifier) {
+    if (!F(p)) return false; // p doesn't have property F so it can't be the last particle with it
+    if (!p->end_vertex()) return true; // p has no children, so it's the last
+    BOOST_FOREACH (const HepMC::GenParticle* child, const_children(p->end_vertex())) {
+      if (F(child)) return false; // if its child has property F, p can't be the first
+    }
+    return true; // p has F and no children do
+  }
+
+
+  /// @brief Is this particle the first of its type in the 'decay' chain?
+  ///
+  /// i.e. it has no parents with the same PID
+  inline bool isFirstReplica(const HepMC::GenParticle* p) {
+    /// @todo Rewrite with a lambda based on matching p->pdg_id
+    if (!p->production_vertex()) return true;
+    bool isfirst = true;
+    BOOST_FOREACH (const HepMC::GenParticle* m, const_parents(p->production_vertex())) {
+      if (m->pdg_id() == p->pdg_id()) {
+        isfirst = false;
+        break;
+      }
+    }
+    return isfirst;
+  }
+
+  /// @brief Is this particle the last of its type in the 'decay' chain?
+  ///
+  /// i.e. it has no daughters with the same PID
+  inline bool isLastReplica(const HepMC::GenParticle* p) {
+    /// @todo Rewrite with a lambda based on matching p->pdg_id
+    if (!p->end_vertex()) return true;
+    bool islast = true;
+    BOOST_FOREACH (const HepMC::GenParticle* d, const_children(p->end_vertex())) {
+      if (d->pdg_id() == p->pdg_id()) {
+        islast = false;
+        break;
+      }
+    }
+    return islast;
+  }
+
+
+  /// @todo Add isPrompt (with what definition?)
+
+
+  /// A function object with state of the status code to be tested for
+  /// @todo Generalise to allow comparison to multiple statuses
+  struct HasStatus {
+    HasStatus(int status) : status(status) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return p->status() == status;
+    }
+    int status;
+  };
+
+  /// A function object with state of the PID codes to be tested for
+  struct HasPID {
+    HasPID(long pid) { pids.push_back(pid); }
+    HasPID(const std::vector<long>& pids) : pids(pids) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return std::find(pids.begin(), pids.end(), p->pdg_id()) != pids.end();
+    }
+    std::vector<long> pids;
+  };
+
+  /// A function object with state of the PID codes to be tested for (including antiparticles)
+  struct HasAbsPID {
+    HasAbsPID(long pid) { pids.push_back(pid); }
+    HasAbsPID(const std::vector<long>& pids) : pids(pids) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return (std::find(pids.begin(), pids.end(), p->pdg_id()) != pids.end()) ||
+        (std::find(pids.begin(), pids.end(), -p->pdg_id()) != pids.end());
+    }
+    std::vector<long> pids;
+  };
+
+
+  /// @todo Specify and handle units
+  inline bool ptLess(const HepMC::GenParticle* p, double ptmax) {
+    return p->momentum().perp() < ptmax;
+  }
+  /// @todo Specify and handle units
+  struct PtLess {
+    PtLess(double ptmax) : ptmax(ptmax) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return ptLess(p, ptmax);
+    }
+    double ptmax;
+  };
+
+
+  /// @todo Specify and handle units
+  inline bool ptGtr(const HepMC::GenParticle* p, double ptmin) {
+    return p->momentum().perp() >= ptmin;
+  }
+  /// @todo Specify and handle units
+  struct PtGtr {
+    PtGtr(double ptmin) : ptmin(ptmin) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return ptGtr(p, ptmin);
+    }
+    double ptmin;
+  };
+
+
+  inline bool inEtaRange(const HepMC::GenParticle* p, double etamin, double etamax) {
+    return HEPUtils::in_range(eta(p->momentum()), etamin, etamax);
+  }
+  struct InEtaRange {
+    InEtaRange(double etamin, double etamax) : etamin(etamin), etamax(etamax) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return inEtaRange(p, etamin, etamax);
+    }
+    double etamin, etamax;
+  };
+  struct NotInEtaRange {
+    NotInEtaRange(double etamin, double etamax) : etamin(etamin), etamax(etamax) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return !inEtaRange(p, etamin, etamax);
+    }
+    double etamin, etamax;
+  };
+
+
+  inline bool inRapRange(const HepMC::GenParticle* p, double ymin, double ymax) {
+    return HEPUtils::in_range(rap(p->momentum()), ymin, ymax);
+  }
+  struct InRapRange {
+    InRapRange(double ymin, double ymax) : ymin(ymin), ymax(ymax) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return inRapRange(p, ymin, ymax);
+    }
+    double ymin, ymax;
+  };
+  struct NotInRapRange {
+    NotInRapRange(double ymin, double ymax) : ymin(ymin), ymax(ymax) { }
+    bool operator()(const HepMC::GenParticle* p) {
+      return !inRapRange(p, ymin, ymax);
+    }
+    double ymin, ymax;
+  };
+
+  //@}
+
+
+
+  /// @name Non-PID-based particle classifiers
+  //@{
+
+  /// Determine whether the particle is stable
+  inline bool isStable(const HepMC::GenParticle* p) {
+    return p->status() == 1;
+  }
+
+  /// Determine whether the particle is physical but decayed
+  inline bool isDecayed(const HepMC::GenParticle* p) {
+    return p->status() == 2;
+  }
+
+  /// @brief Determine whether the particle is outside the physical status code range
+  ///
+  /// If the answer is "true", use with extreme, generator-specific care!
+  inline bool hasNonStandardStatus(const HepMC::GenParticle* p) { return !in_range(p->status(), 1, 5); }
+
+  /// Is this a physically reliable final state particle? (as opposed to a debugging entry)
+  inline bool isPhysical(const HepMC::GenParticle* p) {
+    return isStable(p) || isDecayed(p);
+  }
+
+  /// Is this a physically reliable hadron? (as opposed to a debugging entry)
+  inline bool isPhysicalHadron(const HepMC::GenParticle* p) {
+    return isHadron(p) && isPhysical(p);
+  }
+  /// Determine if the particle is a decayed hadron
+  inline bool isDecayedHadron(const HepMC::GenParticle* p) {
+    return isDecayed(p) && isHadron(p);
+  }
+
+  /// Determine if the particle is a decayed tau
+  inline bool isDecayedTau(const HepMC::GenParticle* p) {
+    return isDecayed(p) && isTau(p);
+  }
+
+  //@}
+
+
+
+  /// @name Classifiers for particles based on their relationships to others
+  //@{
+
+  /// @brief Determine whether the given particle has an ancestor that matches the given classifier
+  inline bool fromAncestorWith(const HepMC::GenParticle* p, const PClassifier& c) {
+    /// @todo *sigh* Missing const methods in HepMC...
+    HepMC::GenVertex* gv = const_cast<HepMC::GenVertex*>(p->production_vertex());
+    if (gv == NULL) return false;
+    /// @todo HepMC needs const versions of these iterator methods :-/
+    for (HepMC::GenVertex::particle_iterator pi = gv->particles_begin(HepMC::ancestors); pi != gv->particles_end(HepMC::ancestors); ++pi) {
+      if (c(*pi)) return true;
+    }
+    return false;
+  }
+
+  /// @brief Determine whether the given particle has a descendant that matches the given classifier
+  inline bool hasDescendantWith(const HepMC::GenParticle* p, const PClassifier& c) {
+    /// @todo *sigh* Missing const methods in HepMC...
+    HepMC::GenVertex* gv = const_cast<HepMC::GenVertex*>(p->end_vertex());
+    if (gv == NULL) return false;
+    /// @todo HepMC needs const versions of these iterator methods :-/
+    for (HepMC::GenVertex::particle_iterator pi = gv->particles_begin(HepMC::descendants); pi != gv->particles_end(HepMC::descendants); ++pi) {
+      if (c(*pi)) return true;
+    }
+    return false;
+  }
+
+
+  /// Determine if the particle is a hadronically decayed tau
+  inline bool isHadronicTau(const HepMC::GenParticle* p) {
+    return isDecayedTau(p) && hasDescendantWith(p, isHadron);
+  }
+  /// Determine if the particle is a leptonically decayed tau
+  inline bool isLeptonicTau(const HepMC::GenParticle* p) {
+    return isDecayedTau(p) && !hasDescendantWith(p, isHadron);
+  }
+
+
+  /// @brief Helper function used in detecting hadron/tau decay daughters
+  ///
+  /// @todo Use C++11 lambda when available
+  inline bool _isDecayedHadronOrTau(const HepMC::GenParticle* p) {
+    return isDecayed(p) && (isTau(p) || isHadron(p));
+  }
+  /// @brief Determine whether the given particle is from a hadron or tau decay
+  ///
+  /// Specifically, walk up the ancestor chain until a status 2 hadron or tau is found, if at all.
+  inline bool fromDecay(const HepMC::GenParticle* p) {
+    return fromAncestorWith(p, _isDecayedHadronOrTau);
+  }
+
+
+  /// @brief Determine whether the given particle is from a hadron decay
+  ///
+  /// Specifically, walk up the ancestor chain until a status 2 hadron is found, if at all.
+  inline bool fromHadronDecay(const HepMC::GenParticle* p) {
+    return fromAncestorWith(p, isDecayedHadron);
+  }
+
+  /// @brief Determine whether the given particle is from a tau decay
+  ///
+  /// Specifically, walk up the ancestor chain until a status 2 tau is found, if at all.
+  inline bool fromTauDecay(const HepMC::GenParticle* p) {
+    return fromAncestorWith(p, isDecayedTau);
+  }
+
+  /// @brief Determine whether the given particle is from a hadronic tau decay
+  ///
+  /// Specifically, walk up the ancestor chain until a status 2 tau is found, if at all.
+  inline bool fromHadronicTauDecay(const HepMC::GenParticle* p) {
+    return fromAncestorWith(p, isHadronicTau);
+  }
+
+
+  /// Helper function used in detecting HF hadron/tau decay daughters
+  /// @todo Use a C++11 inline lambda when available
+  inline bool _isDecayedHFHadronOrTau(const HepMC::GenParticle* p) {
+    // if (isDecayed(p) && (abs(p->pdg_id()) == PID::TAU || isHeavyHadron(p)))
+    //   std::cout << "HFTau: " << p->barcode() << ", " << p->pdg_id() << p->status() << std::endl;
+    return isDecayed(p) && (isTau(p) || isHeavyHadron(p));
+  }
+  /// @brief Determine whether the given particle is from a HF hadron or tau decay
+  ///
+  /// Specifically, walk up the ancestor chain until a status 2 c/b hadron or tau is found, if at all.
+  inline bool fromTauOrHFDecay(const HepMC::GenParticle* p) {
+    return fromAncestorWith(p, _isDecayedHFHadronOrTau);
+  }
+
+  //@}
+
+
+}
