acb059926bb5f54a9617e163ef94bae4807f8109
Pat Scott <patscott@physics.mcgill.ca>
2014-12-03 00:55:16 +0000

    Made backends, modules and models excludable from the cmake command line.  Just do
     cmake -Ditch="blah1;blah2;etc" ..
    
    to exclude all model/module/backends with names beginning with the
    strings "blah1", "blah2" and "etc. For example,
    
     cmake -Dith="Coll;Dark;Example;nulike" ..
    
    would exclude
    modules ColliderBit
            DarkBit
            ExampleBit_A (including its standalone example)
            ExampleBit_B
    as well as all versions of backend nulike.  Version 1.0.x would be
    specified by saying nulike_1_0_ instead.
    
    Also added a check for ROOT, and disabled Delphes and ColliderBit
    automatically if it is not found.
    
    	modified:   ../CMakeLists.txt
    	new file:   ../cmake/FindROOT.cmake
    	modified:   ../cmake/contrib.cmake
    	new file:   ../cmake/executables.cmake
    	modified:   ../cmake/gambit.cmake
    	new file:   ../cmake/optional.cmake

--- modules/cmake/gambit.cmake
+++ modules/cmake/gambit.cmake
@@ -9,8 +9,7 @@ set(GAMBIT_INCDIRS ${GAMBIT_INCDIRS} "${PROJECT_SOURCE_DIR}/Printers/include")
 set(GAMBIT_INCDIRS ${GAMBIT_INCDIRS} "${PROJECT_SOURCE_DIR}/Core/include")
 
 #set include directores for GAMBIT modules
-#set(EXCLUDES "ColliderBit")  #FIXME make input args
-retrieve_bits(GAMBIT_BITS ${PROJECT_SOURCE_DIR} "${EXCLUDES}" "Quiet")
+retrieve_bits(GAMBIT_BITS ${PROJECT_SOURCE_DIR} "${itch}" "Quiet")
 foreach(bit ${GAMBIT_BITS})
   set(GAMBIT_INCDIRS ${GAMBIT_INCDIRS} "${PROJECT_SOURCE_DIR}/${bit}/include")
 endforeach()
