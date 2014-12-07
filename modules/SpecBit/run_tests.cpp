0f86915ca79e3af761a3d9237e8ba0fac2e0ee9a
Peter Athron <peter.athron@adelaide.edu.au>
2014-12-07 20:11:44 +1100

    do slha conversion after spectrum is calculated otherwise physical_slha is empty.

--- modules/SpecBit/run_tests.cpp
+++ modules/SpecBit/run_tests.cpp
@@ -13,28 +13,30 @@ int main(int argc, char* argv[])
    std::cout << "Running Spectrum tests..." << std::endl;
 
    // Create spectrum for testing
-   CMSSM_slha<Two_scale> FS_model; //start with empty flexiblesusy object
+   CMSSM<Two_scale> FS_model; //start with empty flexiblesusy object
+  
+   setup(FS_model); //fill with some parameters
+   FS_model.calculate_DRbar_parameters(); //calculated DRbar masses 
+   FS_model.calculate_pole_masses();//now calculate pole masses
+   
+   //Create a model with SLHA conventions (and pole mass getters)
+   CMSSM_slha<Two_scale> FS_model_slha(FS_model);
    
    // Create Spectrum object to wrap flexiblesusy object
-   MSSMSpec<CMSSM_slha<Two_scale>> mssm(FS_model);
-
-   // I think these objects should only get created once since they are static...      
-   // ...and they should be destructed automatically when the program ends.
-
-   setup(mssm.model); //fill with some parameters
-   mssm.model.calculate_DRbar_parameters(); //calculated DRbar masses 
-   mssm.model.calculate_pole_masses();//now calculate pole masses
+   MSSMSpec<CMSSM_slha<Two_scale>> mssm(FS_model_slha);
 
    // Check contents
    std::cout << "This is specbit_tests. Checking Spectrum object contents..." << std::endl;
    if(TestMssmParGets(mssm, mssm.model)==false){
        std::cout << "TestMssmParGets fail." << std::endl;
        //return 1;
-   }
+   } 
+   else std::cout << "TestMssmParGets passed."  << std::endl;
    if(TestMssmPoleGets(mssm, mssm.model)==false){
       std::cout << "TestMssmPoleGets fail." << std::endl;
       //return 1;
    }
+   else std::cout << "TestMssmPoleGets passed."  << std::endl;
    //So now we have a mssm1 model object filled, as it will be
    //stored in Gambit after the spectrum generator has run
    // mssm.mass2_par_mapping(); //call mapping - this needs to be changed.
