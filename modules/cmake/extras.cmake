7be76bf52a93010f6cd1ba4551120003a76ab0e9
Pat Scott <patscott@physics.mcgill.ca>
2014-12-18 18:56:59 +0000

    Added last remaining backends to cmake system, and mothballed old make system in extras folder.
    	renamed:    makefile.common -> ../extras/old_make/makefile.common
    	renamed:    makefile.g++ -> ../extras/old_make/makefile.g++
    	renamed:    makefile.icc -> ../extras/old_make/makefile.icc
    	modified:   cmake/extras.cmake

--- modules/cmake/extras.cmake
+++ modules/cmake/extras.cmake
@@ -48,6 +48,42 @@ ExternalProject_Add(micromegas
 
 set(clean_files ${clean_files} "${PROJECT_SOURCE_DIR}/../extras/micromegas/libmicromegas.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libmicromegas.so")
 
+ExternalProject_Add(pythia
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/../extras/boss/bossed_pythia_source
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND make CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=${CMAKE_CXX_FLAGS} LDFLAGS=${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS} COMMAND cp lib/libpythia8.so ${PROJECT_SOURCE_DIR}/Backends/lib/libpythia8.so
+  INSTALL_COMMAND ""
+  INSTALL_DIR ${CMAKE_BINARY_DIR}/install
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install
+)
+
+set(clean_files ${clean_files} "${PROJECT_SOURCE_DIR}/../extras/boss/bossed_pythia_source/lib/libpythia8.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libpythia8.so")
+
+ExternalProject_Add(FastSim
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/../extras/fast_sim
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND make CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=${CMAKE_CXX_FLAGS} LDFLAGS=${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS} libfastsim.so COMMAND cp lib/libfastsim.so ${PROJECT_SOURCE_DIR}/Backends/lib/libfastsim.so
+  INSTALL_COMMAND ""
+  INSTALL_DIR ${CMAKE_BINARY_DIR}/install
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install
+)
+
+set(clean_files ${clean_files} "${PROJECT_SOURCE_DIR}/../extras/fast_sim/lib/libfastsim.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libfastsim.so")
+
+ExternalProject_Add(BOSSMinimalExample
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/../extras/boss
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND make CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=${CMAKE_CXX_FLAGS} LDFLAGS=${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS} all COMMAND cp libminimal_1_0.so ${PROJECT_SOURCE_DIR}/Backends/lib/ COMMAND cp libminimal_1_1.so ${PROJECT_SOURCE_DIR}/Backends/lib/ COMMAND cp libminimal_1_2.so ${PROJECT_SOURCE_DIR}/Backends/lib/
+  INSTALL_COMMAND ""
+  INSTALL_DIR ${CMAKE_BINARY_DIR}/install
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install
+)
+
+set(clean_files ${clean_files} "${PROJECT_SOURCE_DIR}/../extras/boss/*.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libminimal_1_0.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libminimal_1_1.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libminimal_1_2.so")
+
 if("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "Intel")
   set(FMODULE "module")
 elseif("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "GNU")
@@ -67,7 +103,7 @@ ExternalProject_Add(nulike
 
 set(clean_files ${clean_files} "${PROJECT_SOURCE_DIR}/../extras/nulike/lib/libnulike.so" "${PROJECT_SOURCE_DIR}/Backends/lib/libnulike.so")
 
-set_target_properties(DDCal0 gamLike DarkSUSY micromegas nulike PROPERTIES EXCLUDE_FROM_ALL 1)
+set_target_properties(DDCal0 gamLike DarkSUSY micromegas nulike pythia FastSim BOSSMinimalExample PROPERTIES EXCLUDE_FROM_ALL 1)
 
-add_custom_target(backends COMMAND make gamLike nulike DDCal0) #DarkSUSY micromegas
+add_custom_target(backends COMMAND make gamLike nulike DDCal0 pythia FastSim BOSSMinimalExample) #DarkSUSY micromegas
 
