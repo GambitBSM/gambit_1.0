b06d89fe418ce28354dad63496e96848954b52df
Pat Scott <patscott@physics.mcgill.ca>
2015-01-01 16:47:22 +0000

    More scanner merging.
    	modified:   ScannerBit/include/scanner_utils.hpp

--- modules/ScannerBit/include/scanner_utils.hpp
+++ modules/ScannerBit/include/scanner_utils.hpp
@@ -42,25 +42,25 @@
 #define scan_flush      SCAN_FLUSH
 
 #define SCAN_ERR                                                \
-Scanner::Errors::_bool_() = true,                               \
-Scanner::Errors::_err_()                                        \
+Gambit::Scanner::Errors::_bool_() = true,                       \
+Gambit::Scanner::Errors::_err_()                                \
 
 #define SCAN_WARN                                               \
-Scanner::Errors::_bool_() = false,                              \
-Scanner::Errors::_warn_()                                       \
+Gambit::Scanner::Errors::_bool_() = false,                      \
+Gambit::Scanner::Errors::_warn_()                               \
 
 #define SCAN_END_INTERNAL                                       \
-(Scanner::Errors::_bool_()) ?                                   \
+(Gambit::Scanner::Errors::_bool_()) ?                           \
 (                                                               \
-        Scanner::scan_error().raise(LOCAL_INFO,                 \
-         Scanner::Errors::_err_().str()),                       \
-        Scanner::Errors::_err_().str("")                        \
+        Gambit::Scanner::scan_error().raise(LOCAL_INFO,         \
+         Gambit::Scanner::Errors::_err_().str()),               \
+        Gambit::Scanner::Errors::_err_().str("")                \
 )                                                               \
 :                                                               \
 (                                                               \
-        Scanner::scan_warning().raise(LOCAL_INFO,               \
-         Scanner::Errors::_warn_().str()),                      \
-        Scanner::Errors::_warn_().str("")                       \
+        Gambit::Scanner::scan_warning().raise(LOCAL_INFO,       \
+         Gambit::Scanner::Errors::_warn_().str()),              \
+        Gambit::Scanner::Errors::_warn_().str("")               \
 )                                                               \
 
 #define SCAN_END std::endl, SCAN_END_INTERNAL
