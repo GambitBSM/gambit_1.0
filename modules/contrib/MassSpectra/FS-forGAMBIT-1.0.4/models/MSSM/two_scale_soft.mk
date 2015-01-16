e36103885fc672cc8810809c799b197abd3879ea
Peter Athron <peter.athron@adelaide.edu.au>
2015-01-16 13:46:20 +0100

    adding updated FS.

---
+++ modules/contrib/MassSpectra/FS-forGAMBIT-1.0.4/models/MSSM/two_scale_soft.mk
@@ -0,0 +1,34 @@
+#  ====================================================================
+#  This file is part of FlexibleSUSY.
+#
+#  FlexibleSUSY is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published
+#  by the Free Software Foundation, either version 3 of the License,
+#  or (at your option) any later version.
+#
+#  FlexibleSUSY is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with FlexibleSUSY.  If not, see
+#  <http://www.gnu.org/licenses/>.
+#  ====================================================================
+
+
+LIBMSSM_SRC += \
+	models/MSSM/MSSM_two_scale_soft_beta_TYd.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_TYe.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_TYu.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_BMu.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_mq2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_ml2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_mHd2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_mHu2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_md2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_mu2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_me2.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_MassB.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_MassWB.cpp \
+	models/MSSM/MSSM_two_scale_soft_beta_MassG.cpp
---
+++ modules/contrib/MassSpectra/FS-forGAMBIT-1.0.4/models/MSSMNoFV/MSSMNoFV_utilities.cpp
@@ -0,0 +1,103 @@
+// ====================================================================
+// This file is part of FlexibleSUSY.
+//
+// FlexibleSUSY is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// FlexibleSUSY is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FlexibleSUSY.  If not, see
+// <http://www.gnu.org/licenses/>.
+// ====================================================================
+
+// File generated at Fri 16 Jan 2015 12:51:35
+
+#include "MSSMNoFV_utilities.hpp"
+#include "MSSMNoFV_input_parameters.hpp"
+#include "logger.hpp"
+#include "wrappers.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace softsusy;
+
+namespace flexiblesusy {
+
+MSSMNoFV_spectrum_plotter::MSSMNoFV_spectrum_plotter()
+   : spectrum()
+   , scale(0.0)
+   , width(16)
+{
+}
+
+void MSSMNoFV_spectrum_plotter::write_to_file(const std::string& file_name) const
+{
+   if (spectrum.empty())
+      return;
+
+   std::ofstream filestr(file_name.c_str(), std::ios::out);
+   VERBOSE_MSG("MSSMNoFV_spectrum_plotter::write_to_file: opening file: "
+               << file_name.c_str());
+   if (filestr.fail()) {
+      ERROR("MSSMNoFV_spectrum_plotter::write_to_file: can't open file "
+            << file_name);
+      return;
+   }
+
+   filestr << "### one-loop pole masses (Q = " << scale << " GeV)\n";
+   write_spectrum(spectrum, filestr);
+
+   filestr.close();
+   VERBOSE_MSG("MSSMNoFV_spectrum_plotter::write_to_file: file written: "
+               << file_name.c_str());
+}
+
+void MSSMNoFV_spectrum_plotter::write_spectrum(const TSpectrum& spectrum, std::ofstream& filestr) const
+{
+   for (std::size_t s = 0; s < spectrum.size(); ++s) {
+      if (!filestr.good()) {
+         ERROR("MSSMNoFV_spectrum_plotter::write_spectrum: "
+               "file stream is corrupted");
+         break;
+      }
+
+      const std::string& name = spectrum[s].name;
+      const std::string& latex_name = spectrum[s].latex_name;
+      const std::valarray<double>& masses = spectrum[s].masses;
+      const std::size_t multiplicity = masses.size();
+
+      filestr << std::left << "# " << name << '\n';
+
+      for (std::size_t i = 0; i < multiplicity; ++i) {
+         std::string lname("$" + latex_name + "$");
+         std::stringstream lname_with_index;
+         lname_with_index << "$" << latex_name;
+         if (multiplicity > 1)
+            lname_with_index << "_{" << (i+1) << "}";
+         lname_with_index  << "$";
+
+         filestr << std::left << std::setw(width) << s
+                 << std::left << std::setw(width) << masses[i]
+                 << std::left << std::setw(width) << name
+                 << std::left << std::setw(2*width) << lname
+                 << std::left << std::setw(2*width) << lname_with_index.str()
+                 << '\n';
+      }
+   }
+}
+
+std::valarray<double> MSSMNoFV_spectrum_plotter::to_valarray(double v)
+{
+   return std::valarray<double>(&v, 1);
+}
+
+} // namespace flexiblesusy
