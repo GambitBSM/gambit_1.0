718383f265af76092f4ba98b198105e1d3c7b4aa
Christopher Rogan <crogan@cern.ch>
2015-02-05 02:43:56 -0500

    FeynHiggs backend added

---
+++ modules/Backends/include/backend_types/FeynHiggs.hpp
@@ -0,0 +1,33 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Definitions of types
+///  for the FeynHiggs backend.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Christopher Rogan
+///          (crogan@cern.ch)
+///  \date 2015 Feb
+///
+///  *********************************************
+
+
+#ifndef __FeynHiggs_types_hpp__
+#define __FeynHiggs_types_hpp__
+
+namespace Gambit
+{
+  
+  typedef double fh_real;
+  typedef std::complex<double> fh_complex;
+  typedef const fh_real fh_creal;
+  typedef const fh_complex fh_ccomplex;
+  
+
+}
+
+#endif /* defined __FeynHiggs_types_hpp__ */
--- modules/EWPOBit/include/EWPOBit_rollcall.hpp
+++ modules/EWPOBit/include/EWPOBit_rollcall.hpp
@@ -21,16 +21,60 @@
 ///          (p.scott@imperial.ac.uk)
 ///  \date 2014 Nov
 ///
+///  \author Christopher Rogan
+///          (christophersrogan@gmail.com)
+///  \date 2015 Feb
+///
 ///  *********************************************
 
 
 #ifndef __EWPOBit_rollcall_hpp__
 #define __EWPOBit_rollcall_hpp__
 
+#include "EWPOBit_types.hpp"
+
 #define MODULE EWPOBit
 START_MODULE
 
-  //QUICK_FUNCTION(EWPOBit, test_deleteme, NEW_CAPABILITY, deleteme, int)
+  #define CAPABILITY FH_Precision            // FeynHiggs EWK precision observables
+  START_CAPABILITY
+    #define FUNCTION FH_Precision
+    START_FUNCTION(double)
+      BACKEND_REQ(FHConstraints, (libfeynhiggs), void, (int&,fh_creal&,fh_creal&,fh_creal&,fh_creal&,
+							fh_creal&,fh_creal&,fh_creal&,fh_creal&,fh_creal&))
+      BACKEND_OPTION( (FeynHiggs, 2.10), (libfeynhiggs) )
+    #undef FUNCTION
+  #undef CAPABILITY 
+
+  #define CAPABILITY FH_Masses               // FeynHiggs SUSY masses and mixings
+  START_CAPABILITY
+    #define FUNCTION FH_Masses
+    START_FUNCTION(double)
+      BACKEND_REQ(FHGetPara, (libfeynhiggs), void, (int&,int&,fh_real*,fh_complex*,fh_real*,fh_complex*,
+						    fh_real*,fh_complex*,fh_complex*,fh_real*,fh_complex*,
+						    fh_complex&,fh_real&,fh_real*,fh_real&))
+      BACKEND_OPTION( (FeynHiggs, 2.10), (libfeynhiggs) )
+    #undef FUNCTION
+  #undef CAPABILITY 
+
+  #define CAPABILITY FH_Flavor               // FeynHiggs flavor observables
+  START_CAPABILITY
+    #define FUNCTION FH_Flavor
+    START_FUNCTION(double)
+      BACKEND_REQ(FHFlavour, (libfeynhiggs), void, (int&,fh_real&,fh_real&,fh_real&,fh_real&,fh_real&,fh_real&))
+      BACKEND_OPTION( (FeynHiggs, 2.10), (libfeynhiggs) )
+    #undef FUNCTION
+  #undef CAPABILITY 
+
+  #define CAPABILITY FH_Higgs               // Higgs observables with errors
+  START_CAPABILITY
+    #define FUNCTION FH_Higgs
+    START_FUNCTION(double)
+      BACKEND_REQ(FHHiggsCorr, (libfeynhiggs), void, (int&,fh_real*,fh_complex&,fh_complex*,fh_complex*))
+      BACKEND_REQ(FHUncertainties, (libfeynhiggs), void, (int&,fh_real*,fh_complex&,fh_complex*,fh_complex*))
+      BACKEND_OPTION( (FeynHiggs, 2.10), (libfeynhiggs) )
+    #undef FUNCTION
+  #undef CAPABILITY 
 
 #undef MODULE
 
