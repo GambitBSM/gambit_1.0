01ff34418107779b8f7ad2085ad8ae861e083c0e
Pat Scott <p.scott@imperial.ac.uk>
2015-02-09 20:58:08 +0000

    More tweaking of dict script.
    	modified:   ColliderBit/src/delphes/make_dicts.sh

--- modules/ColliderBit/src/delphes/make_dicts.sh
+++ modules/ColliderBit/src/delphes/make_dicts.sh
@@ -1,4 +1,4 @@
-rootcint -f BTaggingWithTruthModule_dict.cc -c -I../../../contrib/Delphes-3.1.2 -I../../../contrib/Delphes-3.1.2/external ../../include/gambit/ColliderBit/delphes/BTaggingWithTruthModule.h ../../include/gambit/ColliderBit/delphes/BTaggingWithTruthModuleLinkDef.h 
-rootcint -f AbsoluteIsolationModule_dict.cc -c -I../../../contrib/Delphes-3.1.2 -I../../../contrib/Delphes-3.1.2/external ../../include/gambit/ColliderBit/delphes/AbsoluteIsolationModule.h ../../include/gambit/ColliderBit/delphes/AbsoluteIsolationModuleLinkDef.h 
+rootcint -f BTaggingWithTruthModule_dict.cc -c -I../../../contrib/Delphes-3.1.2 -I../../../contrib/Delphes-3.1.2/external -I../../include/gambit/ColliderBit/delphes BTaggingWithTruthModule.h BTaggingWithTruthModuleLinkDef.h 
+rootcint -f AbsoluteIsolationModule_dict.cc -c -I../../../contrib/Delphes-3.1.2 -I../../../contrib/Delphes-3.1.2/external -I../../include/gambit/ColliderBit/delphes AbsoluteIsolationModule.h AbsoluteIsolationModuleLinkDef.h 
 mv BTaggingWithTruthModule_dict.h ../../include/gambit/ColliderBit/delphes/
 mv AbsoluteIsolationModule_dict.h ../../include/gambit/ColliderBit/delphes/
