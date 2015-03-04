0771db9f9cf8b74196e400edc46ed04797d332c3
Abram Krislock <a.m.b.krislock@fys.uio.no>
2015-03-04 15:46:37 -0600

    Delphes "source-like" stuff removed from Detector.hpp.

--- modules/ColliderBit/include/gambit/ColliderBit/Detector.hpp
+++ modules/ColliderBit/include/gambit/ColliderBit/Detector.hpp
@@ -4,9 +4,6 @@
 #include <exception>
 #include <memory>
 
-/// @note To configure a new detector, follow these steps:
-/// @note (To configure a new subprocess group, only do STEPS >= 5)
-/// @note STEP1)  BOSS your favorite collider simulator. Then:
 #include "gambit/Utils/shared_types.hpp"
 #include "gambit/ColliderBit/Py8Utils.hpp"
 #include "gambit/ColliderBit/ColliderBit_macros.hpp"
@@ -77,46 +74,29 @@ namespace Gambit {
 
     /// @note Abstract base class Delphes_ToHEPUtilsBase
     class DelphesBase : public Detector<Pythia8::Event, HEPUtils::Event> {
+    protected:
+      /// @name Class variables
+      //@{
+      // Modularity of Delphes is set by Config File.
+      /// @note: init of modularDelphes left to subclasses.
+      Delphes *modularDelphes;
+      //@}
+
     public:
 
-      /// @todo Move lots of this into the .cpp files -- it shouldn't be in the interface/base-class definitions.
+      /// @todo Move lots of this into the .cpp files
+      ///   -- it shouldn't be in the interface/base-class definitions.
 
       /// @name Initialization functions
       //@{
       virtual void defaults() {}
-      virtual void init(const std::vector<std::string>& settings) {
-        try {
-          // To read Delphes Config File
-          std::string configFilename = settings[0];
-          confReader = new ExRootConfReader();
-          confReader->ReadFile(configFilename.c_str());
-
-          // Modularity of Delphes set by Config File
-          modularDelphes = new Delphes("Delphes");
-          modularDelphes->SetConfReader(confReader);
-
-          // Factory production of particle "candidates"
-          factory = modularDelphes->GetFactory();
-
-          // Delphes particle arrays: Pre-Detector-Sim
-          allParticleOutputArray = modularDelphes->ExportArray("allParticles");
-          stableParticleOutputArray = modularDelphes->ExportArray("stableParticles");
-          partonOutputArray = modularDelphes->ExportArray("partons");
-
-          // Database of PDG codes and particle info
-          pdg = TDatabasePDG::Instance();
-
-          modularDelphes->InitTask();
-        } catch(std::runtime_error &e) {
-          std::cerr << "** ERROR: " << e.what() << endl;
-          exit(EXIT_FAILURE);
-        }
-      }
+      virtual void init(const std::vector<std::string>&) = 0; //< @note Pure virtual.
       //@}
-
 
       /// @name Event detection simulation.
       //@{
+      virtual void convertInput(const Pythia8::Event&) = 0; //< @note Pure virtual.
+      virtual void convertOutput(HEPUtils::Event&) = 0; //< @note Pure virtual.
       virtual void processEvent(const Pythia8::Event& eventIn, HEPUtils::Event& eventOut)  {
         try {
           modularDelphes->Clear();
@@ -130,130 +110,6 @@ namespace Gambit {
       }
       //@}
 
-    protected:
-
-      /// @name Event conversion functions.
-      //@{
-      virtual void convertInput(const Pythia8::Event& event) {
-        for (int ip = 0; ip < event.size(); ++ip) {
-          const Pythia8::Particle& p = event[ip];
-          candidate = factory->NewCandidate();
-
-          /// @TODO How to convert Py8 events without hadronisation?
-          candidate->PID = p.id();
-          pdgCode = abs(candidate->PID);
-
-          candidate->Status=p.status();
-          pdgParticle = pdg->GetParticle(p.id());
-
-          candidate->Charge = pdgParticle ? Int_t(pdgParticle->Charge()/3.0) : -999;
-          candidate->Mass = pdgParticle ? pdgParticle->Mass() : -999.9;
-
-          candidate->Momentum.SetPxPyPzE(p.px(), p.py(), p.pz(), p.e());
-          candidate->Position.SetXYZT(p.xProd(), p.yProd(), p.zProd(), p.tProd());
-          candidate->D1 = p.daughter1();
-          candidate->D2 = p.daughter2();
-          /// @TODO Why do the non-final particles (other than B's and taus) need to be passed? Speedup?
-          allParticleOutputArray->Add(candidate);
-          if (!pdgParticle) continue;
-          if (p.isFinal()) stableParticleOutputArray->Add(candidate);
-
-          if (pdgCode <= 5 || pdgCode == 21 || pdgCode == 15) partonOutputArray->Add(candidate);
-        }
-      }
-
-
-      virtual void convertOutput(HEPUtils::Event &event) {
-        event.clear();
-
-        HEPUtils::Particle *recoParticle;
-        HEPUtils::Jet *recoJet;
-        // Delphes particle arrays: Post-Detector Sim
-        //    MISSING ET:
-        const TObjArray *arrayMissingET = modularDelphes->ImportArray("MissingET/momentum");
-        if ((candidate = static_cast<Candidate*>(arrayMissingET->At(0)))) {
-          const TLorentzVector &momentum = candidate->Momentum;
-          event.set_missingmom(P4::mkXYZM(-1*momentum.Px(), -1*momentum.Py(), 0., 0.));
-        }
-
-        // Delphes particle arrays: Post-Detector Sim
-        //    PHOTONS:
-        const TObjArray *arrayPhotons = modularDelphes->ImportArray("PhotonIsolation/photons");
-        TIter iteratorPhotons(arrayPhotons);
-        iteratorPhotons.Reset();
-        while ((candidate = static_cast<Candidate*>(iteratorPhotons.Next()))) {
-          const TLorentzVector &momentum = candidate->Momentum;
-          recoParticle = new HEPUtils::Particle(P4::mkXYZM(momentum.Px(), momentum.Py(), momentum.Pz(), 0.), PID::PHOTON);
-          recoParticle->set_prompt(true);
-          event.add_particle(recoParticle);
-        }
-
-        // Delphes particle arrays: Post-Detector Sim
-        //    ELECTRONS:
-        const TObjArray *arrayElectrons = modularDelphes->ImportArray("ElectronIsolation/electrons");
-        TIter iteratorElectrons(arrayElectrons);
-        iteratorElectrons.Reset();
-        while ((candidate = static_cast<Candidate*>(iteratorElectrons.Next()))) {
-          const TLorentzVector &momentum = candidate->Momentum;
-          recoParticle = new HEPUtils::Particle(P4::mkXYZM(momentum.Px(), momentum.Py(), momentum.Pz(), 0.000510998902),
-                                      -sign(candidate->Charge) * PID::ELECTRON);
-          recoParticle->set_prompt(true);
-          event.add_particle(recoParticle);
-        }
-
-        // Delphes particle arrays: Post-Detector Sim
-        //    MUONS:
-        const TObjArray *arrayMuons = modularDelphes->ImportArray("MuonIsolation/muons");
-        TIter iteratorMuons(arrayMuons);
-        iteratorMuons.Reset();
-        while ((candidate = static_cast<Candidate*>(iteratorMuons.Next()))) {
-          const TLorentzVector &momentum = candidate->Momentum;
-          recoParticle = new HEPUtils::Particle(P4::mkXYZM(momentum.Px(), momentum.Py(), momentum.Pz(), 0.105658389),
-                                      -sign(candidate->Charge) * PID::MUON);
-          recoParticle->set_prompt(true);
-          event.add_particle(recoParticle);
-        }
-
-        // Delphes particle arrays: Post-Detector Sim
-        //    JETS and TAUS:
-        const TObjArray *arrayJets = modularDelphes->ImportArray("FastJetFinder/jets");
-        TIter iteratorJets(arrayJets);
-        iteratorJets.Reset();
-        while ((candidate = static_cast<Candidate*>(iteratorJets.Next()))) {
-          const TLorentzVector &momentum = candidate->Momentum;
-          if (candidate->TauTag) {
-            recoParticle = new HEPUtils::Particle(P4::mkXYZM(momentum.Px(), momentum.Py(), momentum.Pz(), 1e-6),
-                                        -sign(candidate->Charge) * PID::TAU);
-            recoParticle->set_prompt(true);
-            event.add_particle(recoParticle);
-            //continue;
-          }
-          else {
-            /// @todo Should the jet mass be assigned properly rather than set as microscopic?
-            recoJet = new HEPUtils::Jet(P4::mkXYZM(momentum.Px(), momentum.Py(), momentum.Pz(), 1e-6), candidate->BTag);
-            event.add_jet(recoJet);
-          }
-        }
-      }
-      //@}
-
-
-      // To read Delphes Config File
-      ExRootConfReader *confReader;
-      // Modularity of Delphes is set by said Config File.
-      Delphes *modularDelphes;
-      // Factory production of particle "candidates"
-      DelphesFactory *factory;
-      // Delphes particle arrays: Pre-Detector-Sim
-      TObjArray *allParticleOutputArray;
-      TObjArray *stableParticleOutputArray;
-      TObjArray *partonOutputArray;
-      // Database of PDG codes and particle info
-      TDatabasePDG *pdg;
-      // Used within the event Converters below
-      Candidate *candidate;
-      TParticlePDG *pdgParticle;
-      Int_t pdgCode;
     };
 
 
