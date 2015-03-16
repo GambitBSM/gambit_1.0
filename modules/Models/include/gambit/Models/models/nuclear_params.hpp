81bcacc67b357c2be7c17011dc004af1ce2c3bd2
Jonathan Cornell <jcornell@ucsc.edu>
2015-03-16 15:31:47 -0700

    Added nuclear_params model.

---
+++ modules/Models/include/gambit/Models/models/nuclear_params.hpp
@@ -0,0 +1,26 @@
+//  GAMBIT: Global and Modular BSM Inference Tool
+//  *********************************************
+//
+//  Nuclear parameters model definitions
+//
+//  *********************************************
+//
+//  Authors
+//  =======
+//
+//  (add name and date if you modify)
+//
+//  Jonathan Cornell
+//  2015 March
+//
+//  *********************************************
+
+#ifndef __nuclear_params_hpp__
+#define __nuclear_params_hpp__
+
+#define MODEL nuclear_params
+  START_MODEL
+  DEFINEPARS(sigma0, SigmaPiN)
+#undef MODEL
+
+#endif /* __nuclear_params_hpp__ */
