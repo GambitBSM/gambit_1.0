2dd152ed0238aafefddc30865096523848cda5d1
Jonathan Cornell <jcornell@ucsc.edu>
2015-03-16 17:16:46 -0700

    Added multiple parameterizations of the nuclear parameters and the functions to convert between them.

--- modules/Models/include/gambit/Models/models/nuclear_params.hpp
+++ modules/Models/include/gambit/Models/models/nuclear_params.hpp
@@ -18,10 +18,31 @@
 #ifndef __nuclear_params_hpp__
 #define __nuclear_params_hpp__
 
-#define MODEL nuclear_params
+// Explicitly defined hadronic matrix elements
+#define MODEL nuclear_params_fnq
+  START_MODEL
+  DEFINEPARS(fpd, fpu, fps, fnd, fnu, fns)
+  DEFINEPARS(deltad, deltau, deltas)
+#undef MODEL
+
+// sigma0 and SigmaPiN used to calculate hadronic matrix elements
+#define MODEL nuclear_params_sigma0_SigmaPiN
+#define PARENT nuclear_params_fnq
   START_MODEL
   DEFINEPARS(sigma0, SigmaPiN)
-  DEFINEPARS(deltau, deltad, deltas)
+  DEFINEPARS(deltad, deltau, deltas)
+  INTERPRET_AS_PARENT__FUNCTION(sigma0_SigmaPiN_to_fnq)
+#undef PARENT
+#undef MODEL
+
+// sigmas and SigmaPiN used to calculate hadronic matrix elements
+#define MODEL nuclear_params_sigmas_SigmaPiN
+#define PARENT nuclear_params_fnq
+  START_MODEL
+  DEFINEPARS(sigmas, SigmaPiN)
+  DEFINEPARS(deltad, deltau, deltas)
+  INTERPRET_AS_PARENT__FUNCTION(sigmas_to_sigma0)
+#undef PARENT
 #undef MODEL
 
 #endif /* __nuclear_params_hpp__ */
