b1db1dc3fa778c789105f43452773d4b6da570c5
Pat Scott <p.scott@imperial.ac.uk>
2015-04-09 13:52:19 +0100

    Shifted preprocessor variables into cmake_variables.hpp.
    	modified:   CMakeLists.txt
    	new file:   cmake/cmake_variables.hpp.in
    	deleted:    cmake/cmake_variables.py
    	modified:   cmake/preprocessor.cmake
    	modified:   cmake/utilities.cmake
    	deleted:    gambit_config.h.in

--- modules/cmake/preprocessor.cmake
+++ modules/cmake/preprocessor.cmake
@@ -1,7 +1,35 @@
+# GAMBIT: Global and Modular BSM Inference Tool  
+#************************************************
+# \file                                          
+#                                                
+#  Cmake preprocessor variable configuration. 
+#    
+#************************************************
+#                                                
+#  Authors (add name and date if you modify):                                    
+#                                                
+#  \author Antje Putze
+#          (antje.putze@lapth.cnrs.fr)              
+#  \date 2015 Apr
+#
+#  \author Pat Scott
+#          (p.scott@imperial.ac.uk)              
+#  \date 2015 Apr
+#                                               
+#************************************************
+
+message("${Yellow}-- Updating GAMBIT with config data${ColourReset}")
+
+# Check for STL regex
 include(CheckIncludeFiles)
 if(COMPILER_SUPPORTS_CXX11 OR COMPILER_SUPPORTS_CXX0X)
   check_include_files(regex.h HAVE_REGEX_H)
 endif()
 
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/gambit_config.h.in ${CMAKE_CURRENT_BINARY_DIR}/gambit_config.h)
-include_directories(${PROJECT_BINARY_DIR})
+# Configure the file
+set(outhdr "${PROJECT_SOURCE_DIR}/cmake/include/gambit/cmake/cmake_variables.hpp")
+configure_file("${PROJECT_SOURCE_DIR}/cmake/cmake_variables.hpp.in" ${outhdr})
+message("${BoldYellow}   Updated ${outhdr}${ColourReset}")
+
+message("${Yellow}-- Updating GAMBIT with config data - done.${ColourReset}")
+
