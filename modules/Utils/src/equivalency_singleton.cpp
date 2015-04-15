8a227c954ac8707438fad5df7186c09c7c0ce218
Pat Scott <p.scott@imperial.ac.uk>
2015-04-15 20:15:20 +0100

    Added overlooked files.
    	new file:   DecayBit/data/PDG/rpp2014-sum-mesons.pdf
    	new file:   Utils/src/equivalency_singleton.cpp
    	new file:   Utils/src/type_equivalency.cpp

---
+++ modules/Utils/src/equivalency_singleton.cpp
@@ -0,0 +1,34 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  GAMBIT type equivalency object accessor function.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Pat Scott
+///  \date 2014 Oct
+///
+///  *********************************************
+
+#include "gambit/Utils/equivalency_singleton.hpp"
+
+namespace Gambit
+{
+
+  namespace Utils
+  {
+
+    /// Type equivalency accessor function
+    type_equivalency& typeEquivalencies()
+    {
+      static type_equivalency local;
+      return local;
+    }
+
+  }
+
+}
+
