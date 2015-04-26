daa91dc9d82909027165b6b81068cbf886c2e622
Peter Athron <peter.athron@adelaide.edu.au>
2015-04-26 21:09:51 +1000

    fix types

--- modules/Backends/include/gambit/Backends/frontends/SUSYPOPE.hpp
+++ modules/Backends/include/gambit/Backends/frontends/SUSYPOPE.hpp
@@ -32,101 +32,101 @@ BE_FUNCTION(SetPara_SUSYPOPE, void,
              Fdouble_complex&,Fdouble_complex&,Fdouble_complex&,
              Fdouble&,Fdouble&,
              Fdouble&,Fdouble&,Fdouble&), 
-   "setpara_susypope_", "SetPara_SUSYPOPE")
+            "setpara_susypope_", "SetPara_SUSYPOPE")
 /* This is CalcObs_AMW via #define */  
- BE_FUNCTION(CalcObs_SUSYPOPE, void,
-             (int&, Farray<Fdouble,1,35>&,Farray<Fdouble,1,35>&),
-             " calcobs_susypope_","CalcObs_SUSYPOPE")
- ///This is just fake version for testing.  Will need to get spectrum.
+BE_FUNCTION(CalcObs_SUSYPOPE, void,
+            (int&, Farray<Fdouble,1,35>&,Farray<Fdouble,1,35>&),
+            " calcobs_susypope_","CalcObs_SUSYPOPE")
+///This is just fake version for testing.  Will need to get spectrum.
 BE_INI_FUNCTION{
-    /// set flags for SUSYPOPE
-    /// all flags as given in Appendix F (page 2 / 154 of) found in doc/ 
-    /// directory of SUSYPOPE-0.2
-    int error = 0, L = 5, iter = 1, Obs = 1, SMObs = 1, HOpt = 1, Verb = 1;
-    SetFlags_SUSYPOPE(error, L, iter, Obs, SMObs, HOpt, Verb);
-    //should probably test the error since we pass it
-
-    //parameters as given in Appendix F (page 2 / 154 of) found in doc/ 
-    /// directory of SUSYPOPE-0.2
-    Fdouble MT = 172; //top quark mass, pole?
-    Fdouble MB = 4.2; // bottom quark mass mb(mb) MSbar mass?
-    Fdouble ML = 1.777; //tau lepton (pole?) mass 
-    Fdouble MW = 80.385; //W (pole?) mass? starting value for iter
-    Fdouble MZexp = 91.1876; //Z boson (pole?) mass
-    Fdouble DeltaAlfa5had = 1e-2; /// hadronic contributions to photon vaccum 
+   /// set flags for SUSYPOPE
+   /// all flags as given in Appendix F (page 2 / 154 of) found in doc/ 
+   /// directory of SUSYPOPE-0.2
+   int error = 0, L = 5, iter = 1, Obs = 1, SMObs = 1, HOpt = 1, Verb = 1;
+   SetFlags_SUSYPOPE(error, L, iter, Obs, SMObs, HOpt, Verb);
+   //should probably test the error since we pass it
+   
+   //parameters as given in Appendix F (page 2 / 154 of) found in doc/ 
+   /// directory of SUSYPOPE-0.2
+   Fdouble MT = 172; //top quark mass, pole?
+   Fdouble MB = 4.2; // bottom quark mass mb(mb) MSbar mass?
+   Fdouble ML = 1.777; //tau lepton (pole?) mass 
+   Fdouble MW = 80.385; //W (pole?) mass? starting value for iter
+   Fdouble MZexp = 91.1876; //Z boson (pole?) mass
+   Fdouble DeltaAlfa5had = 1e-2; /// hadronic contributions to photon vaccum 
                                  /// polarisation 
-    Fdouble DeltaAlfaLept = 1e-2; /// leptonic contributions to photon vaccum 
+   Fdouble DeltaAlfaLept = 1e-2; /// leptonic contributions to photon vaccum 
                                  /// polarisation //DeltaAlfaQED in code
-    Fdouble ZWidthexp = 1; /// experimental Z bosonm width (used to compute
+   Fdouble ZWidthexp = 1; /// experimental Z bosonm width (used to compute
                           /// Z boson pole mass
-    
-    Fdouble TB = 10; ///tan(beta)
-    fh_real MA0 = 250; /// masses of CP-odd and (real MSSM only) 
-                       /// set to -1 if complex MSSM
-    fh_real MHp = -1;  /// charged Higgs (complex MSSM only)
-                       /// set to -1 if real MSSM
    
-    Fdouble Mho = 125;  /// light Higgs mass (specified for HOpt = 1)
-    Fdouble MHH = 1250;  /// heavy Higgs mass (specified for HOpt = 1)
-    Fdouble_complex SA; /// Higgs mixing angle
-    SA.re =0.1;
-    SA.im = 0;
-    Farray< Fdouble,1,4> MHiggs;  /// complex MSSM 3 higgs masses 
-                                  /// don't specify in real MSSM or HOpt=3
-    Farray<Fdouble_complex,1,3,1,3> UHiggs; ///complex mising matrix for MHiggs
-                                       /// don't specify in real MSSM or HOpt=3
+   Fdouble TB = 10; ///tan(beta)
+   Fdouble MA0 = 250; /// masses of CP-odd and (real MSSM only) 
+   /// set to -1 if complex MSSM
+   Fdouble MHp = -1;  /// charged Higgs (complex MSSM only)
+   /// set to -1 if real MSSM
    
-    
-    // soft-SUSY breaking parameters for g=1,2,3 generation sfermions
-    fh_real MSusy = 1000;
-    fh_real M3SL = MSusy, M2SL = M3SL, M1SL = M2SL; // slepton doublet
-    fh_real M3SE = MSusy, M2SE = M3SE, M1SE = M2SE; // slepton singlet
-    fh_real M3SQ = MSusy, M2SQ = M3SQ, M1SQ = M2SQ; // squark doublet
-    fh_real M3SU = MSusy, M2SU = M3SU, M1SU = M2SU; // up-type squark singlet
-    fh_real M3SD = MSusy, M2SD = M3SD, M1SD = M2SD; // down-type squark singlet
-    Fdouble_complex MUE;  // Higgs mixing parameter mu
-    MUE.re = 200; 
-    MUE.im = 0;
-    Fdouble MUEPhase=0; //aditional phase rotation to above parameter
-    // soft-SUSY breaking parameters
-    fh_complex Af; 
-    Af.re = 2000; 
-    Af.im = 0.;
-    fh_complex At = Af, Ac = At, Au = Ac;
-    fh_complex Ab = Af, As = Ab, Ad = As;
-    fh_complex Atau = Af, Amu = Atau, Ae = Amu;
-    //aditional phase rotation to above parameters
-    Fdouble  AtPhase=0,AbPhase=0,AtauPhase=0;
-    // gaugino mass parameters. M_1 == 0 => GUT relation is used
-    fh_complex M_1, M_2, M_3; 
-    M_1.re = 0;   M_1.im = 0;
-    M_2.re = 500; M_2.im = 0;
-    M_3.re = 800; M_3.im = 0;
-    //aditional phase rotation to above parameters
-    Fdouble M1Phase=0,M2Phase=0;
-    // soft-SUSY breaking parameters
-    // the scales at which the sfermion input parameters M3S are given
-    // < 0 indicates on-shell parameters which is reccomended in doc
-    fh_real Qtau = 1000; /// (not implemented doc says)
-    fh_real Qt = 1000;
-    fh_real Qb = 100;
-
-    /// Set the parameters
-    SetPara_SUSYPOPE(error, MT, MB, ML, MW, MZexp, 
-                     DeltaAlfa5had, DeltaAlfaLept, ZWidthexp,
-                     TB, MA0, MHp, Mho, MHH, SA, MHiggs, UHiggs,
-                     M3SL, M3SE, M3SQ, M3SU, M3SD,
-                     M2SL, M2SE, M2SQ, M2SU, M2SD,
-                     M1SL, M1SE, M1SQ, M1SU, M1SD,
-                     MUE, MUEPhase,
-                     Atau, At, Ab, Amu, Ac, As, Ae, Au, Ad, 
-                     AtPhase, AbPhase, AtauPhase,
-                     M_1, M_2, M_3, 
-                     M2Phase, M1Phase,
-                     Qtau, Qt, Qb);   
-    //should probably test the error since we pass it
+   Fdouble Mho = 125;  /// light Higgs mass (specified for HOpt = 1)
+   Fdouble MHH = 1250;  /// heavy Higgs mass (specified for HOpt = 1)
+   Fdouble_complex SA; /// Higgs mixing angle
+   SA.re =0.1;
+   SA.im = 0;
+   Farray< Fdouble,1,4> MHiggs;  /// complex MSSM 3 higgs masses 
+   /// don't specify in real MSSM or HOpt=3
+   Farray<Fdouble_complex,1,3,1,3> UHiggs; ///complex mising matrix for MHiggs
+   /// don't specify in real MSSM or HOpt=3
    
- }
+   
+   // soft-SUSY breaking parameters for g=1,2,3 generation sfermions
+   Fdouble MSusy = 1000;
+   Fdouble M3SL = MSusy, M2SL = M3SL, M1SL = M2SL; // sleptonFdoublet
+   Fdouble M3SE = MSusy, M2SE = M3SE, M1SE = M2SE; // slepton singlet
+   Fdouble M3SQ = MSusy, M2SQ = M3SQ, M1SQ = M2SQ; // squarkFdoublet
+   Fdouble M3SU = MSusy, M2SU = M3SU, M1SU = M2SU; // up-type squark singlet
+   Fdouble M3SD = MSusy, M2SD = M3SD, M1SD = M2SD; // down-type squark singlet
+   Fdouble_complex MUE;  // Higgs mixing parameter mu
+   MUE.re = 200; 
+   MUE.im = 0;
+   Fdouble MUEPhase=0; //aditional phase rotation to above parameter
+   // soft-SUSY breaking parameters
+   Fdouble_complex Af; 
+   Af.re = 2000; 
+   Af.im = 0.;
+   Fdouble_complex At = Af, Ac = At, Au = Ac;
+   Fdouble_complex Ab = Af, As = Ab, Ad = As;
+   Fdouble_complex Atau = Af, Amu = Atau, Ae = Amu;
+   //aditional phase rotation to above parameters
+   Fdouble  AtPhase=0,AbPhase=0,AtauPhase=0;
+   // gaugino mass parameters. M_1 == 0 => GUT relation is used
+   Fdouble_complex M_1, M_2, M_3; 
+   M_1.re = 0;   M_1.im = 0;
+   M_2.re = 500; M_2.im = 0;
+   M_3.re = 800; M_3.im = 0;
+   //aditional phase rotation to above parameters
+   Fdouble M1Phase=0,M2Phase=0;
+   // soft-SUSY breaking parameters
+   // the scales at which the sfermion input parameters M3S are given
+   // < 0 indicates on-shell parameters which is reccomended in doc
+   Fdouble Qtau = 1000; /// (not implemented doc says)
+   Fdouble Qt = 1000;
+   Fdouble Qb = 100;
+   
+   /// Set the parameters
+   SetPara_SUSYPOPE(error, MT, MB, ML, MW, MZexp, 
+                    DeltaAlfa5had, DeltaAlfaLept, ZWidthexp,
+                    TB, MA0, MHp, Mho, MHH, SA, MHiggs, UHiggs,
+                    M3SL, M3SE, M3SQ, M3SU, M3SD,
+                    M2SL, M2SE, M2SQ, M2SU, M2SD,
+                    M1SL, M1SE, M1SQ, M1SU, M1SD,
+                    MUE, MUEPhase,
+                    Atau, At, Ab, Amu, Ac, As, Ae, Au, Ad, 
+                    AtPhase, AbPhase, AtauPhase,
+                    M_1, M_2, M_3, 
+                    M2Phase, M1Phase,
+                    Qtau, Qt, Qb);   
+   //should probably test the error since we pass it
+   
+}
 
                
 DONE
