babc139c62334e685e1c24ee18b331e2b31c6d95
Christoph Weniger <c.weniger@uva.nl>
2015-05-08 15:05:45 +0200

    Get Higgs and other masses from Process Catalog (SingletDM)
    	modified:   DarkBit/include/gambit/DarkBit/DarkBit_rollcall.hpp
    	modified:   DarkBit/src/SingletDM.cpp
    	modified:   SpecBit/SpecNames.txt (slightly reformatted)
    	modified:   gambit_SingletDM.yml

--- modules/DarkBit/include/gambit/DarkBit/DarkBit_rollcall.hpp
+++ modules/DarkBit/include/gambit/DarkBit/DarkBit_rollcall.hpp
@@ -103,6 +103,8 @@ START_MODULE
     #undef FUNCTION
     #define FUNCTION RD_spectrum_SingletDM
       START_FUNCTION(Gambit::DarkBit::RD_spectrum_type)
+      DEPENDENCY(TH_ProcessCatalog, Gambit::DarkBit::TH_ProcessCatalog)
+      ALLOW_MODELS(SingletDM)
     #undef FUNCTION
   #undef CAPABILITY
 
@@ -120,6 +122,7 @@ START_MODULE
     #undef FUNCTION
     #define FUNCTION RD_thresholds_resonances_SingletDM
       START_FUNCTION(Gambit::DarkBit::TH_resonances_thresholds)
+      DEPENDENCY(TH_ProcessCatalog, Gambit::DarkBit::TH_ProcessCatalog)
       ALLOW_MODELS(SingletDM)
       BACKEND_REQ(rdmgev, (), DS_RDMGEV)
     #undef FUNCTION
@@ -452,6 +455,7 @@ START_MODULE
     #undef FUNCTION
     #define FUNCTION DD_couplings_SingletDM
       START_FUNCTION(Gambit::DarkBit::DD_couplings)
+      DEPENDENCY(TH_ProcessCatalog, Gambit::DarkBit::TH_ProcessCatalog)
       ALLOW_MODEL_DEPENDENCE(nuclear_params_fnq, SingletDM)
       MODEL_GROUP(group1, (nuclear_params_fnq))
       MODEL_GROUP(group2, (SingletDM))
