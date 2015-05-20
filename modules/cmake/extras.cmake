741f302d498dbfccb9c6e194231a04291ffc8928
Pat Scott <p.scott@imperial.ac.uk>
2015-05-20 14:47:42 +0100

    More attempts to get OSX sed working.
    	modified:   ../cmake/extras.cmake

--- modules/cmake/extras.cmake
+++ modules/cmake/extras.cmake
@@ -29,8 +29,10 @@ include(ExternalProject)
 # Define the sed command to use differently for OSX and linux
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   set(dashi "-i ''")
+  set(nl "\\'\$'\\n")
 else()
   set(dashi "-i")
+  set(nl "\\n")
 endif()
 
 set(remove_files_from_libdarksusy dssetdsinstall.o dssetdsversion.o ddilog.o drkstp.o eisrs1.o tql2.o tred2.o)
@@ -186,28 +188,28 @@ ExternalProject_Add(susyhit
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND cp -n makefile makefile.orig COMMAND cp -n sdecay.f sdecay.orig COMMAND cp -n hdecay.f hdecay.orig 
             COMMAND cp    makefile.orig makefile COMMAND cp    sdecay.orig sdecay.f COMMAND cp    hdecay.orig hdecay.f
-            COMMAND sed ${dashi} -e "s#FC=\\(.*\\)\\s*$#FF=\\1\\n\\nFC=\\$\\(FF\\)\\nFFLAGS= -O2 -fPIC#g"
+            COMMAND sed ${dashi} -e "s#FC=\\(.*\\)\\s*$#FF=\\1${nl}${nl}FC=\\$\\(FF\\)${nl}FFLAGS= -O2 -fPIC#g"
                                  -e "s#FC)\\s*-c#FC\\) \\$\\(FFLAGS\\) -c#g" 
-                                 -e "s#\\(clean:.*\\)$#${susyhit_lib}.so:\\$\\(OBJS1\\) \\$\\(OBJS2\\) \\$\\(OBJS3\\)\\n\t\\$\\(FC\\) -shared -o \\$@ \\$\\(OBJS1\\) \\$\\(OBJS2\\) \\$\\(OBJS3\\)\\n\\n\\1#g"
+                                 -e "s#\\(clean:.*\\)$#${susyhit_lib}.so:\\$\\(OBJS1\\) \\$\\(OBJS2\\) \\$\\(OBJS3\\)${nl}\t\\$\\(FC\\) -shared -o \\$@ \\$\\(OBJS1\\) \\$\\(OBJS2\\) \\$\\(OBJS3\\)${nl}${nl}\\1#g"
                                  <SOURCE_DIR>/makefile
-            COMMAND sed ${dashi} -e "s#^\\s*program sdecay\\s*$#      !Added by GAMBIT to make 100% sure \\'unlikely\\' matches the fortran value.\\n      double precision function s_hit_unlikely()\\n        s_hit_unlikely = -123456789D0\\n      end\\n\\n      subroutine sdecay                          !Modified by GAMBIT.#g"
+            COMMAND sed ${dashi} -e "s#^\\s*program sdecay\\s*$#      !Added by GAMBIT to make 100% sure \\'unlikely\\' matches the fortran value.${nl}      double precision function s_hit_unlikely()${nl}        s_hit_unlikely = -123456789D0${nl}      end${nl}${nl}      subroutine sdecay                          !Modified by GAMBIT.#g"
                                  -e "s#COMMON/SD_stopwidth/stoptot4\\s*$#COMMON/SD_stopwidth/stoptot4,stoptot       !Modified by GAMBIT#g"
-                                 -e "s#\\(\\s*open(ninshs,file=.*\\)$#      if (.false.) then                          !Added by GAMBIT.\\n\\n\\1#g"
-                                 -e "s#\\(^\\s*flagshsin=2d0\\)$#\\1\\n      endif                                      !Added by GAMBIT.\\n\\n      else                                       !Added by GAMBIT.\\n\\n        flagshsin=0d0                            !Added by GAMBIT.\\n        flagoutput=2d0                           !Added by GAMBIT.\\n        i4bod=1                                  !Added by GAMBIT.\\n#g"
+                                 -e "s#\\(\\s*open(ninshs,file=.*\\)$#      if (.false.) then                          !Added by GAMBIT.${nl}${nl}\\1#g"
+                                 -e "s#\\(^\\s*flagshsin=2d0\\)$#\\1${nl}      endif                                      !Added by GAMBIT.${nl}${nl}      else                                       !Added by GAMBIT.${nl}${nl}        flagshsin=0d0                            !Added by GAMBIT.${nl}        flagoutput=2d0                           !Added by GAMBIT.${nl}        i4bod=1                                  !Added by GAMBIT.${nl}#g"
                                  -e "s#\\(\\s*if((flagoutput\\.eq\\.1\\.D0).or.(ifavvio\\.eq\\.1)) then\\)#c\\1 !Commented out by GAMBIT.#g"
                                  -e "s#\\!\\(\\s*if(flagoutput\\.eq\\.1\\.D0) then\\)\\s*$#\\1                !Reinstated by GAMBIT.#g"
                                  -e "/output not a la Les Houches accord/{" -e "N" -e "N" -e "s/else/elseif(flagoutput.eq.0.D0) then            !Modified by GAMBIT./" -e "}"
                                  -e "s#\\(^\\s*if(flagshsin\\.\\)eq\\(\\.1\\.D0) then\\)#\\1le\\2                 !Modified by GAMBIT.#g"
                                  <SOURCE_DIR>/sdecay.f
-            COMMAND sed ${dashi} -e "/=1: READ SUSY LES HOUCHES ACCORD INPUT/{" -e "N" -e "N" -e "s/\\(.*\\nc end change susyhit\\s\\)/\\1C           =2: SLHA INPUT PROVIDED BY CALLING PROGRAM  !Added by GAMBIT.\\n/" -e "}"
-                                 -e "/=1: WRITE SUSY LES HOUCHES ACCORD OUTPUT/{" -e "N" -e "N" -e "s/\\(.*\\nc end change susyhit\\s\\)/\\1C           =2: WRITE NOTHING                           !Added by GAMBIT.\\n/" -e "}"
+            COMMAND sed ${dashi} -e "/=1: READ SUSY LES HOUCHES ACCORD INPUT/{" -e "N" -e "N" -e "s/\\(.*${nl}c end change susyhit\\s\\)/\\1C           =2: SLHA INPUT PROVIDED BY CALLING PROGRAM  !Added by GAMBIT.${nl}/" -e "}"
+                                 -e "/=1: WRITE SUSY LES HOUCHES ACCORD OUTPUT/{" -e "N" -e "N" -e "s/\\(.*${nl}c end change susyhit\\s\\)/\\1C           =2: WRITE NOTHING                           !Added by GAMBIT.${nl}/" -e "}"
                                  -e "s#\\s*if(islhao\\.ne\\.1) then\\s*\$#      if(islhao.eq.0) then                       !Modified by GAMBIT.#g"
                                  -e "s#\\s*\\(CALL CLOSE_HDEC\\)\\s*\$#      if(islhao.eq.0) \\1            !Modified by GAMBIT.#g"
                                  -e "s#\\s*islhai\\s*=\\s*1\\s*\$#      islhai  = 2                                !Modified by GAMBIT.#g"
                                  -e "s#\\s*islhao\\s*=\\s*1\\s*\$#      islhao  = 2                                !Modified by GAMBIT.#g"
-                                 -e "/\\s*if(islhai\\.eq\\.1)\\s*then\\s*\$/{" -e "N" -e "s#\\(\\s*if(islhai\\.eq\\.1)\\s*then\\s*\\n\\)#      if(islhai.ge.1) then                       !Added by GAMBIT.\\n  \\1#" -e "}"
-                                 -e "s#\\s*call SLHA_read_leshouches_HDEC(ninlha)#         call SLHA_read_leshouches_HDEC(ninlha)\\n        endif                                    !Added by GAMBIT.#g"
-                                 -e "s#\\(c -- calculation of the mb pole mass from mb(mb)_MSbar --\\s*\\)\$#      if(ishai.eq.2) fmb = massval(34)           !Added by GAMBIT.\\n\\1#g"
+                                 -e "/\\s*if(islhai\\.eq\\.1)\\s*then\\s*\$/{" -e "N" -e "s#\\(\\s*if(islhai\\.eq\\.1)\\s*then\\s*${nl}\\)#      if(islhai.ge.1) then                       !Added by GAMBIT.${nl}  \\1#" -e "}"
+                                 -e "s#\\s*call SLHA_read_leshouches_HDEC(ninlha)#         call SLHA_read_leshouches_HDEC(ninlha)${nl}        endif                                    !Added by GAMBIT.#g"
+                                 -e "s#\\(c -- calculation of the mb pole mass from mb(mb)_MSbar --\\s*\\)\$#      if(ishai.eq.2) fmb = massval(34)           !Added by GAMBIT.${nl}\\1#g"
                                  -e "/c -- calculation of the mb pole mass from mb(mb)_MSbar --\\s*\$/{" -e "N" -e "s#\\(\\s*if(smval(5)\\.ne\\.0\\.D0\\))\\s*then\\s*\$#\\1 .and. ishai.ne.2) then !Modified by GAMBIT.#" -e "}"
                                  -e "/\\s*close(nout)\\s*\$/{" -e "N" -e "N" -e "s#else#elseif (islhao .eq. 0) then                !Modified by GAMBIT.#" -e "}"
                                  <SOURCE_DIR>/hdecay.f
