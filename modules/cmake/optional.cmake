1b92d3e6cffe2269a3fd8558a39cc4d87ea22363
Pat Scott <p.scott@imperial.ac.uk>
2015-06-24 17:25:12 +0100

    Debug.
    	modified:   cmake/optional.cmake

--- modules/cmake/optional.cmake
+++ modules/cmake/optional.cmake
@@ -63,9 +63,7 @@ endif()
 # Check for LAPACK
 find_package(LAPACK)
 if(NOT LAPACK_FOUND)
-  message("${BLAS_LIBRARIES}")
-  message("${LAPACK_LIBRARIES}")
-  message(FATAL_ERROR "${BoldRed}    LAPACK is currently required in order to build GAMBIT.${ColourReset}")
+  #message(FATAL_ERROR "${BoldRed}    LAPACK is currently required in order to build GAMBIT.${ColourReset}")
   # In future MN and FS need to be ditched if lapack cannot be found, and the build allowed to continue.
   message("${BoldRed}   No LAPACK installation found. Excluding FlexibleSUSY and MultiNest from GAMBIT configuration. ${ColourReset}")
 else()
