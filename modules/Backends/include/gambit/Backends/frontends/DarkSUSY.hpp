f45dcb17614ae16e2c506947e5b25ff7fc28917a
Torsten Bringmann <torsten.bringmann@desy.de>
2015-06-26 15:28:26 +0200

    forgot to comment one line...

--- modules/Backends/include/gambit/Backends/frontends/DarkSUSY.hpp
+++ modules/Backends/include/gambit/Backends/frontends/DarkSUSY.hpp
@@ -286,7 +286,7 @@ BE_NAMESPACE
     GAMBITparticle_mass.clear();
     for (int i = 0; i < IBfinalstate.size(); i++ )
     {
-      DSparticle_mass.push_back(
+//      DSparticle_mass.push_back(
 //         mspctm->mass(DarkBit::DarkBit_utils::DSparticle_code(IBfinalstate[i])));
 
 //For CW/PS: The above line sets masses in the desired way, but does not compile. It seems 
