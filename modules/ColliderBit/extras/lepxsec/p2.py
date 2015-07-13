2771a3b004adac9aa0ce525de62858649bd1314a
Abram Krislock <a.m.b.krislock@fys.uio.no>
2015-07-13 20:04:51 +0200

    Pulled in the lepxsec directory from the master branch.

--- modules/ColliderBit/extras/lepxsec/p2.py
+++ modules/ColliderBit/extras/lepxsec/p2.py
@@ -1,6 +1,7 @@
 # -*- python -*-
 
 from __future__ import division
+import numpy as np
 import math
 
 
@@ -20,7 +21,7 @@ class P2(object):
         if i == 0: self.x = v
         if i == 1: self.y = v
 
-    def __len__(self):
+    def __abs__(self):
         return math.sqrt(self.x**2 + self.y**2)
 
     def __add__(self, other):
@@ -63,3 +64,93 @@ def unitnormal(pt1, pt2):
         sgn = dxdy / abs(dxdy)
         norm = 1.0 / np.sqrt(1 + dxdy**2)
         return -sgn * P2(norm, -norm*dxdy) #< TODO: signs and orientability
+
+
+class LineSegment(object):
+    def __init__(self, pt1, pt2, tolerance=0):
+        if any([not type(pt1) == P2, not type(pt2) == P2]):
+            raise TypeError("Please use Andy's nifty P2 class for the endpoints")
+        tolerance = abs(tolerance)
+        if pt1.x > pt2.x or (pt1.x == pt2.x and pt1.y > pt2.y):
+            rawpt2 = pt1
+            rawpt1 = pt2
+        else:
+            rawpt1 = pt1
+            rawpt2 = pt2
+        adjustEnds = (rawpt2 - rawpt1) * tolerance
+        self.pt2 = rawpt2 + adjustEnds
+        self.pt1 = rawpt1 - adjustEnds
+
+    def __abs__(self):
+        return abs(self.pt2 - self.pt1)
+
+    def __repr__(self):
+        return repr(self.pt1) + " - " + repr(self.pt2)
+
+    def endpoints(self):
+        return self.pt1, self.pt2
+    def midpoint(self):
+        return (self.pt1 + self.pt2)/2.0
+    def allpoints(self, nsteps):
+        xList = []
+        yList = []
+        for t in np.linspace(0., 1., nsteps):
+            xList.append(self.pt1.x * t + self.pt2.x * (1. - t))
+            yList.append(self.pt1.y * t + self.pt2.y * (1. - t))
+        return np.array(xList), np.array(yList)
+
+    def b(self):
+        return self.intercept()
+    def intercept(self):
+        if self.pt1.x == self.pt2.x:
+            return np.inf
+        else:
+            return self.pt1.y - self.m() * self.pt1.x
+
+    def m(self):
+        return self.slope()
+    def slope(self):
+        if self.pt1.x == self.pt2.x:
+            return np.inf
+        else:
+            return (self.pt2.y - self.pt1.y) / (self.pt2.x - self.pt1.x)
+
+    def intersectsAt(self, other):
+        if not type(other) == LineSegment:
+            raise ValueError("Not sure how to determine intersection with " + str(args))
+
+        # If the slopes are equal, they will never intersect
+        if self.slope() == other.slope():
+            return P2(np.inf, np.inf)
+
+        # If self or other has an infinite slope, change the intercept calculation
+        if self.slope() == np.inf:
+            assert self.pt2.y >= self.pt1.y
+            assert other.pt2.x > other.pt1.x
+            xintersect = self.pt1.x
+            yintersect = other.m() * xintersect + other.b()
+            if all([xintersect >= other.pt1.x, xintersect <= other.pt2.x,
+                    yintersect >= self.pt1.y, yintersect <= self.pt2.y]):
+                return P2(xintersect, yintersect)
+            else:
+                return P2(np.inf, np.inf)
+        elif other.slope() == np.inf:
+            assert other.pt2.y >= other.pt1.y
+            assert self.pt2.x > self.pt1.x
+            xintersect = other.pt1.x
+            yintersect = self.m() * xintersect + self.b()
+            if all([xintersect >= self.pt1.x, xintersect <= self.pt2.x,
+                    yintersect >= other.pt1.y, yintersect <= other.pt2.y]):
+                return P2(xintersect, yintersect)
+            else:
+                return P2(np.inf, np.inf)
+        else:   # Regular intercept calculation
+            assert self.pt2.x > self.pt1.x
+            assert other.pt2.x > other.pt1.x
+            xintersect = (other.b() - self.b()) / (self.m() - other.m())
+            yintersect = self.m() * xintersect + self.b()
+            if all([xintersect >= self.pt1.x, xintersect <= self.pt2.x,
+                    xintersect >= other.pt1.x, xintersect <= other.pt2.x]):
+                return P2(xintersect, yintersect)
+            else:
+                return P2(np.inf, np.inf)
