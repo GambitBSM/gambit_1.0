77e1d2debfe38ffbe5527860c0db097c224953a1
Christoph Weniger <c.weniger@uva.nl>
2015-07-22 13:27:38 +0200

    Bugfix
    	modified:   Backends/include/gambit/Backends/frontends/MicrOmegasSingletDM.hpp

--- modules/Backends/include/gambit/Backends/frontends/MicrOmegasSingletDM.hpp
+++ modules/Backends/include/gambit/Backends/frontends/MicrOmegasSingletDM.hpp
@@ -64,10 +64,10 @@ BE_INI_FUNCTION
      // Currently only works correctly in unitary gauge
      *ForceUG=1;
 
-     error = assignVal("MS", *Param["mass"]);
+     error = assignVal("MS", *Param["mS"]);
      if (error != 0) BackendIniBit_error().raise(LOCAL_INFO, "Unable to set DM mass in"
              "MicrOmegas. MicrOmegas error code: " + std::to_string(error));
-     error = assignVal("lambda", *Param["lambda"]);
+     error = assignVal("lambda", *Param["lambda_hS"]);
      if (error != 0) BackendIniBit_error().raise(LOCAL_INFO, "Unable to set lambda in"
              "MicrOmegas. MicrOmegas error code: " + std::to_string(error));
      error = sortOddParticles(byVal(cdmName));
