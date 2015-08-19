b6a3614296a94b05c8c57b8b200b26702fb9323c
Christoph Weniger <c.weniger@uva.nl>
2015-08-19 13:31:34 +0200

    Add DarkSUSY OMP patch (untested)
     Changes to be committed:
    	new file:   ../extras/DarkSUSY/patchDS_OMP_include.dif
    	new file:   ../extras/DarkSUSY/patchDS_OMP_src.dif
    	modified:   cmake/extras.cmake

--- modules/cmake/extras.cmake
+++ modules/cmake/extras.cmake
@@ -134,7 +134,11 @@ ExternalProject_Add(darksusy
   DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}/../extras/DarkSUSY
   SOURCE_DIR ${PROJECT_SOURCE_DIR}/../extras/DarkSUSY/DarkSUSY
   BUILD_IN_SOURCE 1
-  PATCH_COMMAND patch -b -p1 -d src < ../patchDS.dif COMMAND patch -b -p1 -d contrib/isajet781-for-darksusy  < ../patchISA.dif
+  PATCH_COMMAND patch -b -p1 -d src < ../patchDS.dif 
+        COMMAND patch -b -p1 -d contrib/isajet781-for-darksusy  < ../patchISA.dif 
+        COMMAND patch -b -p1 -d src < ../patchDS_OMP_src.dif 
+        COMMAND patch -b -p1 -d include < ../patchDS_OMP_include.dif 
+#  MESSAGE ( STATUS "Fortran compiler flags: " ${CMAKE_Fortran_FLAGS} )
   CONFIGURE_COMMAND <SOURCE_DIR>/configure FC=${CMAKE_Fortran_COMPILER} FCFLAGS=${CMAKE_Fortran_FLAGS} FFLAGS=${CMAKE_Fortran_FLAGS} CC=${CMAKE_C_COMPILER} CFLAGS=${CMAKE_C_FLAGS} CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=${CMAKE_CXX_FLAGS}
   BUILD_COMMAND make 
         COMMAND ar d <SOURCE_DIR>/lib/libdarksusy.a ${remove_files_from_libdarksusy} 
