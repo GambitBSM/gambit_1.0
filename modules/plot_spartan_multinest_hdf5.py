50a7f4c8ab65c7b0e93c7b36e2ec78b2ba38da9f
Ben Farmer <farmer@farmer-macbook-jessie.farmer>
2015-09-10 09:25:30 +0200

    combination script seems to work! Needs to be cleaned up, but I am committing this working version to use as a checkpoint :).

--- modules/plot_spartan_multinest_hdf5.py
+++ modules/plot_spartan_multinest_hdf5.py
@@ -18,16 +18,16 @@ mu_isvalid = np.array(group["#NormalDist_parameters @NormalDist::primary_paramet
 sigma = group["#NormalDist_parameters @NormalDist::primary_parameters::sigma"]
 sigma_isvalid = np.array(group["#NormalDist_parameters @NormalDist::primary_parameters::sigma_isvalid"],dtype=np.bool)
 
-print group["pointID"][:]
-print group["pointID_isvalid"][:]
-print group["MPIrank"][:]
-print group["MPIrank_isvalid"][:]
-quit()
-
-print mu[-100:]
-print mu_isvalid[-100:]
-print sigma[-100:]
-print sigma_isvalid[-100:]
+#print group["pointID"][:]
+#print group["pointID_isvalid"][:]
+#print group["MPIrank"][:]
+#print group["MPIrank_isvalid"][:]
+#quit()
+#
+#print mu[-100:]
+#print mu_isvalid[-100:]
+#print sigma[-100:]
+#print sigma_isvalid[-100:]
 
 P = group["Posterior"]
 P_isvalid = np.array(group["Posterior_isvalid"],dtype=np.bool)
