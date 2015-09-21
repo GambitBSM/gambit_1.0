3bb557148931965e3cacb2d09b2ae9d3fb339830
Pat Scott <p.scott@imperial.ac.uk>
2015-09-21 15:43:35 +0100

    Fixed up GUT relation between gaugino mass parameters in MSSM7 definition. Now determines sin^2 theta_W using tree-level relations between alpha_em, alpha_S, m_Z and G_F. 	modified:   Elements/include/gambit/Elements/numerical_constants.hpp 	modified:   Models/include/gambit/Models/models/MSSM7atQ.hpp 	modified:   Models/src/models/MSSM7atQ.cpp 	modified:   Models/src/models/nuclear_params.cpp

--- Models/src/models/MSSM7atQ.cpp
+++ Models/src/models/MSSM7atQ.cpp
@@ -17,6 +17,7 @@
 #include "gambit/Models/model_macros.hpp"
 #include "gambit/Models/model_helpers.hpp"
 #include "gambit/Logs/log.hpp"
+#include "gambit/Elements/numerical_constants.hpp"
 
 #include "gambit/Models/models/MSSM7atQ.hpp"
 
@@ -28,6 +29,8 @@
 
   void MODEL_NAMESPACE::MSSM7atQ_to_MSSM9atQ (const ModelParameters &myP, ModelParameters &targetP)
   {
+     USE_MODEL_PIPE(MSSM9atQ)
+
      logger()<<"Running interpret_as_parent calculations for " STRINGIFY(MODEL) " --> MSSM9atQ."<<LogTags::info<<EOM;
 
      // Send all parameter values upstream to matching parameters in parent.
@@ -35,8 +38,11 @@
      targetP.setValues(myP,false);
 
      // Gaugino masses
-     targetP.setValue("M1", myP["M2"]); //FIXME *5/3 sin2thew / (1-sin2thew)
-     targetP.setValue("M3", myP["M2"]); //FIXME *alpha_s/alpha sin2thew
+     double mz = Dep::SMINPUTS->mZ;
+     double am1 = Dep::SMINPUTS->alphainv;
+     double sin2thetaW_tree = 0.5 - sqrt(0.25 - pi / (root2*mz*mz*am1*Dep::SMINPUTS->GF));
+     targetP.setValue("M1", myP["M2"] * 5.0/3.0 * sin2thetaW_tree / (1.0-sin2thetaW_tree));
+     targetP.setValue("M3", myP["M2"] * Dep::SMINPUTS->alphaS * am1 * sin2thetaW_tree);
 
      // Done
      #ifdef MSSM7atQ_DBUG
