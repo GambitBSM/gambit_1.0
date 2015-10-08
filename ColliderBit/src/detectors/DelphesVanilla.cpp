09a04ea6403bcf44a4d54c092fb973eae1ff5081
Andy Buckley <andy.buckley@cern.ch>
2015-10-08 16:25:22 +0100

    Merge DelphesVanillaImpl.hpp into DelphesVanilla.cpp since it's not for public or even GAMBIT-wide use, but completely specific to that class.

--- ColliderBit/src/detectors/DelphesVanilla.cpp
+++ ColliderBit/src/detectors/DelphesVanilla.cpp
@@ -1,11 +1,71 @@
 #include "gambit/ColliderBit/detectors/DelphesVanilla.hpp"
 #ifndef EXCLUDE_DELPHES
-#include "gambit/ColliderBit/detectors/DelphesVanillaImpl.hpp"
+
+#include "TROOT.h"
+#include "TTask.h"
+#include "TApplication.h"
+#include "TObjArray.h"
+#include "TDatabasePDG.h"
+#include "TParticlePDG.h"
+#include "TLorentzVector.h"
+#include "modules/Delphes.h"
+#include "classes/DelphesClasses.h"
+#include "classes/DelphesFactory.h"
+#include "ExRootAnalysis/ExRootConfReader.h"
 
 using namespace std;
 
 namespace Gambit {
   namespace ColliderBit {
+
+
+    /// Container for external types, allowing them to be decoupled from public headers cf. the PIMPL idiom
+    /// @todo Use smart ptrs to get automatic clean-up?
+    /// @author Andy Buckley
+    struct DelphesVanillaImpl {
+
+      DelphesVanillaImpl() :
+        modularDelphes(nullptr),
+        confReader(nullptr),
+        factory(nullptr),
+        allParticleOutputArray(nullptr),
+        stableParticleOutputArray(nullptr),
+        partonOutputArray(nullptr),
+        pdg(nullptr) {}
+
+      ~DelphesVanillaImpl() {
+        delete confReader; confReader=nullptr;
+        /// @todo Hmm... I wonder whether or not Delphes cleans up its own memory?
+        delete factory; factory=nullptr;
+        delete allParticleOutputArray; allParticleOutputArray=nullptr;
+        delete stableParticleOutputArray; stableParticleOutputArray=nullptr;
+        delete partonOutputArray; partonOutputArray=nullptr;
+        delete pdg; pdg=nullptr;
+        delete modularDelphes; modularDelphes = nullptr;
+      }
+
+      ////////////////////
+
+      // Modularity of Delphes is set by Config File.
+      /// @note: init of _modularDelphes left to subclasses.
+      Delphes* modularDelphes;
+
+      // To read Delphes Config File
+      ExRootConfReader *confReader;
+
+      // Factory production of particle "candidates"
+      DelphesFactory *factory;
+
+      // Delphes particle arrays: Pre-Detector-Sim
+      TObjArray *allParticleOutputArray;
+      TObjArray *stableParticleOutputArray;
+      TObjArray *partonOutputArray;
+
+      // Database of PDG codes and particle info
+      TDatabasePDG *pdg;
+
+    };
+
 
 
     void DelphesVanilla::init(const vector<string>& settings) {
@@ -171,4 +231,5 @@ namespace Gambit {
 
   }
 }
+
 #endif // not defined EXCLUDE_DELPHES
