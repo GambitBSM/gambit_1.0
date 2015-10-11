b08084f741134b346e2d3f77c572ed96f386e55f
Pat Scott <p.scott@imperial.ac.uk>
2015-10-11 01:29:18 +0100

    Removed -O3 in pythia cmake settings, as this gets passed by CMAKE_BUILD_TYPE=Release anyway (and you don't need it if you are building for Debug).
    	modified:   cmake/backends.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -178,11 +178,11 @@ add_external_clean(micromegasSingletDM ${micromegasSingletDM_dir} clean)
 # Pythia
 # - Pythia will not accept the -std=c++11 flag. Create a special pythia_CXXFLAGS variable without it. 
 string(REGEX REPLACE "(-std=c\\+\\+11)" "" pythia_CXXFLAGS ${GAMBIT_CXX_FLAGS})
-# - Add compiler-specific optimisation flags and suppress warnings from -Wextra when building Pythia with gcc
+# - Add additional compiler-specific optimisation flags and suppress warnings from -Wextra when building Pythia with gcc
 if("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "Intel") 
   set(pythia_CXXFLAGS "${pythia_CXXFLAGS} -fast -xavx")
 elseif("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "GNU") 
-  set(pythia_CXXFLAGS "${pythia_CXXFLAGS} -Wno-extra -O3 -ffast-math")
+  set(pythia_CXXFLAGS "${pythia_CXXFLAGS} -Wno-extra -ffast-math")
   if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(pythia_CXXFLAGS "${pythia_CXXFLAGS} -mavx")  
   endif()
