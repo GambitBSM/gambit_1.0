a93cb1ddc8898b48767cd9927bd15522dbe8c073
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2015-10-12 18:08:05 +0200

    CMAKE: BOSS-generated files are now copied to the correct location withing Backends/include/... Removed the corresponding old Pythia files from the repos and added a couple of entries in gitignore.

--- Backends/include/gambit/Backends/backend_types/Pythia_8_209/wrapper_Particle.h
+++
@@ -1,9 +0,0 @@
-#ifndef __wrapper_Particle_Pythia_8_209_h__
-#define __wrapper_Particle_Pythia_8_209_h__
-
-
-#include "wrapper_Particle_decl.h"
-#include "wrapper_Particle_def.h"
-
-
-#endif /* __wrapper_Particle_Pythia_8_209_h__ */
