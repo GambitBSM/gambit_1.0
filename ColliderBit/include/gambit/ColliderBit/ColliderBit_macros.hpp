1e0dde27a22024ea56b559cdf0a6448bdbb88d49
Abram Krislock <a.m.b.krislock@fys.uio.no>
2015-11-10 16:17:30 +0100

    Changed the timing of ColliderBit to be more "internal" like my Pythia timing hack. The times are now absolutely trustworthy.
     Changes to be committed:
    	modified:   ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/ColliderBit_types.hpp
    	modified:   ColliderBit/src/ColliderBit.cpp

--- ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
+++ ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
@@ -17,25 +17,48 @@
 #define __ColliderBit_macros_hpp__
 
 /// Macros for analysis factory fns
-#define DECLARE_ANALYSIS_FACTORY(ANAME)                                  \
+#define DECLARE_ANALYSIS_FACTORY(ANAME)                                    \
   HEPUtilsAnalysis* create_Analysis_ ## ANAME();
-#define DEFINE_ANALYSIS_FACTORY(ANAME)                                   \
-  HEPUtilsAnalysis* create_Analysis_ ## ANAME() {                        \
-    return new Analysis_ ## ANAME();                                     \
+#define DEFINE_ANALYSIS_FACTORY(ANAME)                                     \
+  HEPUtilsAnalysis* create_Analysis_ ## ANAME() {                          \
+    return new Analysis_ ## ANAME();                                       \
   }
 
 /// Convenience macro for getting mandatory runoptions
-#define GET_COLLIDER_RUNOPTION(OPTION_NAME, OPTION_TYPE)                 \
-  do { try {                                                             \
-    OPTION_NAME = runOptions->getValue<OPTION_TYPE>(#OPTION_NAME);       \
-  } catch (...) {                                                        \
-    std::string errMsg = "Specify '";                                    \
-    errMsg += #OPTION_NAME;                                              \
-    errMsg += "' of type '";                                             \
-    errMsg += #OPTION_TYPE;                                              \
-    errMsg += "' in yaml file.";                                         \
-    ColliderBit_error().raise(LOCAL_INFO, errMsg);                       \
+#define GET_COLLIDER_RUNOPTION(OPTION_NAME, OPTION_TYPE)                   \
+  do { try {                                                               \
+    OPTION_NAME = runOptions->getValue<OPTION_TYPE>(#OPTION_NAME);         \
+  } catch (...) {                                                          \
+    std::string errMsg = "Specify '";                                      \
+    errMsg += #OPTION_NAME;                                                \
+    errMsg += "' of type '";                                               \
+    errMsg += #OPTION_TYPE;                                                \
+    errMsg += "' in yaml file.";                                           \
+    ColliderBit_error().raise(LOCAL_INFO, errMsg);                         \
   } } while (0)
 
+// My TIME_THAT_SHIT macros usually require some extra curly braces. Example:
+// { TIME_THAT_SHIT(two_times_timer) { double f = 5.*9.; f = 7.*-3.; } }   
+#define TIME_THAT_SHIT(TIMER_NAME)                                         \
+ms __delta_##TIMER_NAME;                                                   \
+const int me = omp_get_thread_num();                                       \
+double TIMER_NAME = -1.;                                                   \
+for(tp __start_##TIMER_NAME = steady_clock::now(); TIMER_NAME < 0;         \
+    __delta_##TIMER_NAME = std::chrono::duration_cast<ms> (                \
+            steady_clock::now() - __start_##TIMER_NAME),                   \
+    TIMER_NAME = __delta_##TIMER_NAME.count(),                             \
+    timers[me][#TIMER_NAME] += TIMER_NAME / omp_get_max_threads(),         \
+    timer_maxima[me][#TIMER_NAME] =                                        \
+            TIMER_NAME > timer_maxima[me][#TIMER_NAME] ?                   \
+            TIMER_NAME : timer_maxima[me][#TIMER_NAME])
+
+#define TIME_THAT_SHIT_GLOBALLY(TIMER_NAME)                                \
+ms __delta_##TIMER_NAME;                                                   \
+double TIMER_NAME = -1.;                                                   \
+for(tp __start_##TIMER_NAME = steady_clock::now(); TIMER_NAME < 0;         \
+    __delta_##TIMER_NAME = std::chrono::duration_cast<ms> (                \
+            steady_clock::now() - __start_##TIMER_NAME),                   \
+    TIMER_NAME = __delta_##TIMER_NAME.count(),                             \
+    global_timers[#TIMER_NAME] += TIMER_NAME)
 
 #endif /* defined __ColliderBit_macros_hpp__ */
