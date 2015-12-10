cb8a893bd7404dc98a3541e2e77de7be9ad6c66e
Abram Krislock <a.m.b.krislock@fys.uio.no>
2015-12-10 13:32:43 +0100

    Added the logger statements which I have been using to compare output between the master branch and ColliderBit_development. Also, I got rid of all those vulgar timers, and their macro definitions.
     Changes to be committed:
    	modified:   ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/ColliderBit_rollcall.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/analyses/BaseAnalysis.hpp
    	modified:   ColliderBit/src/ColliderBit.cpp

--- ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
+++ ColliderBit/include/gambit/ColliderBit/ColliderBit_macros.hpp
@@ -37,28 +37,4 @@
     ColliderBit_error().raise(LOCAL_INFO, errMsg);                         \
   } } while (0)
 
-// My TIME_THAT_SHIT macros usually require some extra curly braces. Example:
-// { TIME_THAT_SHIT(two_times_timer) { double f = 5.*9.; f = 7.*-3.; } }   
-/*#define TIME_THAT_SHIT(TIMER_NAME)					\
-ms __delta_##TIMER_NAME;                                                   \
-const int me = omp_get_thread_num();                                       \
-double TIMER_NAME = -1.;                                                   \
-for(tp __start_##TIMER_NAME = steady_clock::now(); TIMER_NAME < 0;         \
-    __delta_##TIMER_NAME = std::chrono::duration_cast<ms> (                \
-            steady_clock::now() - __start_##TIMER_NAME),                   \
-    TIMER_NAME = __delta_##TIMER_NAME.count(),                             \
-    timers[me][#TIMER_NAME] += TIMER_NAME / omp_get_max_threads(),         \
-    timer_maxima[me][#TIMER_NAME] =                                        \
-            TIMER_NAME > timer_maxima[me][#TIMER_NAME] ?                   \
-            TIMER_NAME : timer_maxima[me][#TIMER_NAME])
-
-#define TIME_THAT_SHIT_GLOBALLY(TIMER_NAME)                                \
-ms __delta_##TIMER_NAME;                                                   \
-double TIMER_NAME = -1.;                                                   \
-for(tp __start_##TIMER_NAME = steady_clock::now(); TIMER_NAME < 0;         \
-    __delta_##TIMER_NAME = std::chrono::duration_cast<ms> (                \
-            steady_clock::now() - __start_##TIMER_NAME),                   \
-    TIMER_NAME = __delta_##TIMER_NAME.count(),                             \
-    global_timers[#TIMER_NAME] += TIMER_NAME)*/
-
 #endif /* defined __ColliderBit_macros_hpp__ */
