a6c11d2225e223b2bde5fdfdce489d5fe100cf8f
mwhi <martin.white@coepp.org.au>
2016-01-11 15:16:56 +1030

    First version of ColliderBit_standalone that runs.

--- ColliderBit/examples/ColliderBit_standalone_example.cpp
+++ ColliderBit/examples/ColliderBit_standalone_example.cpp
@@ -56,66 +56,118 @@ int main()
   logger()<<"Running ColliderBit standalone example"<<LogTags::info<<EOM;
 
 
-  /*std::cout << std::endl << "My name is " << name() << std::endl;
+  std::cout << std::endl << "My name is " << name() << std::endl;
   std::cout << " I can calculate: " << endl << iCanDo << std::endl;
-  std::cout << " ...but I may need: " << endl << iMayNeed << std::endl << std::endl;*/
+  std::cout << " ...but I may need: " << endl << iMayNeed << std::endl << std::endl;
   //std::cout << "I can do nevents: " << provides("nevents") << std::endl;
-
-  // Resolve backend requirements 'by hand'.  Must be done before dependencies are resolved.
-  // In our case, this means nulike (for likelihood calculations)
+  
+  // Resolve dependencies of nested functions on each other
+  // calc_LHC_loglike needs AnalysisNumbers (provided by runAnalyses)
+  // It also has a backend requirement of lnlike_marg_poisson
+  calc_LHC_LogLike.resolveDependency(&runAnalyses);
   calc_LHC_LogLike.resolveBackendReq(&Backends::nulike_1_0_0::Functown::nulike_lnpiln); //treat systematics with a log normal distribution
   // calc_LHC_LogLike.resolveBackendReq(&Backends::nulike_1_0_0::Functown::nulike_lnpin); // treat systematics with a Gaussian distribution
-  
-  // Notify any module functions that care of the model(s) being scanned.
-  // 'Care' means where they depend on model parameters directly, or have dependencies or backend requirements that are
-  // conditional on the model being analysed. 
-  Models::CMSSM_demo::Functown::MSSM_demo_parameters.notifyOfModel("CMSSM_demo");
-  getPythia.notifyOfModel("CMSSM_demo");
-  
-  // CMSSM SUSY example
-  
-  ModelParameters* CMSSM_primary_parameters = Models::CMSSM_demo::Functown::primary_parameters.getcontentsPtr();
-  
-  
-  // Resolve dependencies for module functions by hand
-  Models::CMSSM_demo::Functown::MSSM_demo_parameters.resolveDependency(&Models::CMSSM_demo::Functown::primary_parameters);
-  Models::CMSSM_demo::Functown::MSSM_demo_parameters.resolveDependency(&id_dummy);
-  
-  convertPythia8ParticleEvent.resolveDependency(&generatePythia8Event);
-  getAnalysisContainer.resolveDependency(&getPythia);  
+  // runAnalyses needs a ReconstructedEvent, a HardScatteringSim and an AnalysisContainer  
+  runAnalyses.resolveDependency(&getAnalysisContainer);
+  runAnalyses.resolveDependency(&getPythiaFileReader);
+  runAnalyses.resolveDependency(&reconstructBuckFastEvent);
+  // getAnalysisContainer needs a HardScatteringSim (MJW- why?)
+  getAnalysisContainer.resolveDependency(&getPythiaFileReader);
+  // reconstructBuckFastEvent needs a ConvertedScatteringEvent and a SimpleSmearingSim
   reconstructBuckFastEvent.resolveDependency(&convertPythia8ParticleEvent);
   reconstructBuckFastEvent.resolveDependency(&getBuckFast);
-  runAnalyses.resolveDependency(&getAnalysisContainer);
-  runAnalyses.resolveDependency(&getPythia);
-  runAnalyses.resolveDependency(&reconstructBuckFastEvent);
-  calc_LHC_LogLike.resolveDependency(&runAnalyses);
+  // convertPythia8ParticleEvent needs a HardScatteringEvent
+  convertPythia8ParticleEvent.resolveDependency(&generatePythia8Event);
+  // generatePythia8Event needs a HardScatteringSim
+  generatePythia8Event.resolveDependency(&getPythiaFileReader);
+
+  // Now set the loop manager for nested functions
+  getPythiaFileReader.resolveLoopManager(&operateLHCLoop);
+  getBuckFast.resolveLoopManager(&operateLHCLoop);
+  getAnalysisContainer.resolveLoopManager(&operateLHCLoop);
+  generatePythia8Event.resolveLoopManager(&operateLHCLoop);
+  convertPythia8ParticleEvent.resolveLoopManager(&operateLHCLoop);
+  reconstructBuckFastEvent.resolveLoopManager(&operateLHCLoop);
+  runAnalyses.resolveLoopManager(&operateLHCLoop);
+
+  std::vector<functor*> nested_functions = initVector<functor*>(&getPythiaFileReader, &getBuckFast, &getAnalysisContainer,&generatePythia8Event,&convertPythia8ParticleEvent,&reconstructBuckFastEvent,&runAnalyses);
+
+  operateLHCLoop.setNestedList(nested_functions);
+
+  // Double-check which backend requirements have been filled with what
+  std::cout << std::endl << "My function calc_LHC_LogLike has had its backend requirement on lnlike_marg_poisson filled by:" << std::endl;
+  std::cout << ColliderBit::Pipes::calc_LHC_LogLike::BEreq::lnlike_marg_poisson_lognormal_error.origin() << "::";
+  std::cout << ColliderBit::Pipes::calc_LHC_LogLike::BEreq::lnlike_marg_poisson_lognormal_error.name() << std::endl;
+
+  // Double-check which dependencies have been filled with what (not every combo is done)
+  std::cout << std::endl << "My function calc_LHC_LogLike has had its dependency on AnalysisNumbers filled by:" << endl;
+  std::cout << ColliderBit::Pipes::calc_LHC_LogLike::Dep::AnalysisNumbers.origin() << "::";
+  std::cout << ColliderBit::Pipes::calc_LHC_LogLike::Dep::AnalysisNumbers.name() << std::endl;
+  std::cout << std::endl << "My function runAnalyses has had its dependency on ReconstructedEvent filled by:" << endl;
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::ReconstructedEvent.origin() << "::";
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::ReconstructedEvent.name() << std::endl;
+  std::cout << std::endl << "My function runAnalyses has had its dependency on HardScatteringSim filled by:" << endl;
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::HardScatteringSim.origin() << "::";
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::HardScatteringSim.name() << std::endl;
+  std::cout << std::endl << "My function runAnalyses has had its dependency on AnalysisContainer filled by:" << endl;
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::AnalysisContainer.origin() << "::";
+  std::cout << ColliderBit::Pipes::runAnalyses::Dep::AnalysisContainer.name() << std::endl;
+  std::cout << std::endl << "My function getAnalysisContainer has had its dependency on HardScatteringSim filled by:" << endl;
+  std::cout << ColliderBit::Pipes::getAnalysisContainer::Dep::HardScatteringSim.origin() << "::";
+  std::cout << ColliderBit::Pipes::getAnalysisContainer::Dep::HardScatteringSim.name() << std::endl;
+  std::cout << std::endl << "My function reconstructBuckFastEvent has had its dependency on ConvertedScatteringEvent filled by:" << endl;
+  std::cout << ColliderBit::Pipes::reconstructBuckFastEvent::Dep::ConvertedScatteringEvent.origin() << "::";
+  std::cout << ColliderBit::Pipes::reconstructBuckFastEvent::Dep::ConvertedScatteringEvent.name() << std::endl;
+  std::cout << std::endl << "My function reconstructBuckFastEvent has had its dependency on SimpleSmearingSim filled by:" << endl;
+  std::cout << ColliderBit::Pipes::reconstructBuckFastEvent::Dep::SimpleSmearingSim.origin() << "::";
+  std::cout << ColliderBit::Pipes::reconstructBuckFastEvent::Dep::SimpleSmearingSim.name() << std::endl;
+  std::cout << std::endl << "My function convertPythia8ParticleEvent has had its dependency on HardScatteringEvent filled by:" << endl;
+  std::cout << ColliderBit::Pipes::convertPythia8ParticleEvent::Dep::HardScatteringEvent.origin() << "::";
+  std::cout << ColliderBit::Pipes::convertPythia8ParticleEvent::Dep::HardScatteringEvent.name() << std::endl;
+  std::cout << std::endl << "My function generatePythia8Event has had its dependency on HardScatteringSim filled by:" << endl;
+  std::cout << ColliderBit::Pipes::generatePythia8Event::Dep::HardScatteringSim.origin() << "::";
+  std::cout << ColliderBit::Pipes::generatePythia8Event::Dep::HardScatteringSim.name() << std::endl;
   
+  // Set loop manager for nested functions
+  /*exampleEventGen.resolveLoopManager(&eventLoopManager);
+  exampleCut.resolveLoopManager(&eventLoopManager);
+  eventAccumulator.resolveLoopManager(&eventLoopManager);
+  
+  // Set up the mini dependency tree to be run by EventLoopManager
+  std::vector<functor*> nested_functions = initVector<functor*>(&exampleEventGen, &exampleCut, &eventAccumulator);
+  
+  // Notify the loop manager of that tree
+  eventLoopManager.setNestedList(nested_functions);
+
+  */
+    
+  // Set some module function options
+  //nevents_int.setOption<double>("probability_of_validity", 0.1);
+
+  std::vector<std::string> runTheseAnalyses;
+  runTheseAnalyses.push_back("ATLAS_0LEP_20invfb");
+  getAnalysisContainer.setOption<std::vector<std::string>>("analysisNames",runTheseAnalyses);
+
+  getPythiaFileReader.setOption<std::string>("Pythia_doc_path","Backends/installed/Pythia/8.212/share/Pythia8/xmldoc/");
+  std::vector<std::string> inputFileName;
+  inputFileName.push_back("ColliderBit/data/sps1aWithDecays.spc");
+  getPythiaFileReader.setOption<std::vector<std::string>>("SLHA_filenames",inputFileName);
+
+  std::vector<std::string> pythiaNames;
+  pythiaNames.push_back("Pythia_SUSY_LHC_8TeV");
+  operateLHCLoop.setOption<std::vector<std::string>>("pythiaNames",pythiaNames);
+  operateLHCLoop.setOption<int>("nEvents",5000.);
 
   //Start processing some points here (eventually this will become a scan)
 
   {
-    // Give the primary model parameters
-    CMSSM_primary_parameters->setValue("M0",100.);
-    CMSSM_primary_parameters->setValue("A0",0.);
-    CMSSM_primary_parameters->setValue("M12",400.);
-    CMSSM_primary_parameters->setValue("tanb",10.);
-    CMSSM_primary_parameters->setValue("sgnmu",1.);
-    
+        
     // Call the initialisation functions for all backends that are in use. 
     nulike_1_0_0_init.reset_and_calculate();
-
-    // Call the actual module functions, taking care to calculate in the order implied by how the dependencies have been filled;
-    // i.e. calculate quantities that other quantities depend on first.
-    logger()<<"Calculating model parameters"<<LogTags::info<<EOM;
-    Models::CMSSM_demo::Functown::MSSM_demo_parameters.reset_and_calculate();
     
-    logger()<<"Running getPythia()"<<LogTags::info<<EOM;
-    getPythia.reset_and_calculate();
-    
-    getAnalysisContainer.reset_and_calculate();
-    reconstructBuckFastEvent.reset_and_calculate();
-    runAnalyses.reset_and_calculate();
+    operateLHCLoop.reset_and_calculate();
     calc_LHC_LogLike.reset_and_calculate();
+    
   }
     
 }
