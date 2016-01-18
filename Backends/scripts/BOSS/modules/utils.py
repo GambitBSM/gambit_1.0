cde43a7c5f6f9f0d6c61a05701656ef8e6904a9d
Anders Kvellestad <anderkve@login-0-0.local>
2016-01-19 00:56:00 +0100

    BOSS: If CastXML fails when run with --castxml-cc=icpc, it will now automatically retry with --castxml-cc=g++.

--- Backends/scripts/BOSS/modules/utils.py
+++ Backends/scripts/BOSS/modules/utils.py
@@ -2254,7 +2254,7 @@ def castxmlRunner(input_file_path, include_paths_list, xml_output_path, timeout_
 
     # Avoid including intel headers when in "gnu mode" by
     # temporarily unsetting some environment variables
-    if cfg.castxml_cc_id  == 'gnu':
+    if 'gnu' in cfg.castxml_cc_id:
         temp_env_vars = {}
         for var_name in ['CPATH', 'C_INCLUDE_PATH', 'CPLUS_INCLUDE_PATH']:
             try:
@@ -2292,21 +2292,33 @@ def castxmlRunner(input_file_path, include_paths_list, xml_output_path, timeout_
     proc, output, timed_out = shelltimeout.shrun(castxml_cmd, timeout_limit, use_exec=True, poll_interval=poll_interval)
 
     # Reset environment variables
-    if cfg.castxml_cc_id  == 'gnu':
+    if 'gnu' in cfg.castxml_cc_id:
         for var_name, value in temp_env_vars.items():
             os.environ[var_name] = value
 
     # Check for timeout or error
     did_fail = False
     if timed_out:
-        print '  ERROR: castxml timed out.'
+        print '  ERROR: CastXML timed out.'
         did_fail = True
     elif proc.returncode != 0:        
-        print '  ERROR: castxml failed.'
-        did_fail = True
+        print '  ERROR: CastXML failed.'
+	did_fail = True
+        # Get error message
+        for line in output.splitlines():
+            if 'error:' in line:
+                print '  ERROR: ' + line
+                break
 
+    # If it fails with icpc, try again with g++.
+    if ("gnu" in cfg.castxml_cc_id) and ("icpc" in cfg.castxml_cc) and did_fail:
+        print
+        print '  Will retry with --castxml-cc=g++'
+        print
+        cfg.castxml_cc = 'g++'
+        castxmlRunner(input_file_path, include_paths_list, xml_output_path, timeout_limit=timeout_limit, poll_interval=poll_interval)
     # Print error report
-    if did_fail:
+    elif did_fail:
         print
         print 'START CASTXML OUTPUT'
         print '--------------------'
