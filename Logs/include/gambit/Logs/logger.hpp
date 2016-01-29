6f5f9fb48c0887fd0645387b9928f75b752822e2
Ben Farmer <farmer@farmer-macbook-jessie.farmer>
2016-01-29 19:31:29 +0100

    stream operator broken, trying to fix...

---
+++ Logs/include/gambit/Logs/logger.hpp
@@ -0,0 +1,95 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Logging access header for GAMBIT. Declares
+///  the minimal amount needed for standard
+///  use of the logger.
+///  For more advanced use, include 'logmaster.hpp'
+///  as well (provides the full declaration of the
+///  LogMaster class).
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Ben Farmer
+///          (benjamin.farmer@fysik.su.se)
+///  \date 2014 Mar, 2016 Jan
+///
+///  \author Pat Scott
+///          (patscott@physics.mcgill.ca)
+///  \date 2014 Mar
+///
+///  *********************************************
+
+#ifndef __logger_hpp__
+#define __logger_hpp__
+
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
+#include "gambit/Logs/log_tags.hpp"
+#include "gambit/Utils/stream_overloads.hpp"
+
+namespace Gambit
+{
+  /// Forward declare minimial logging components needed to use logger
+  namespace Logging 
+  { 
+     class LogMaster; 
+     /// Special (empty) struct for signalling end of message to LogMaster stream
+     struct endofmessage {};
+
+     // Function to do the reverse search of tag map (brute force)
+     int str2tag(const std::string&);
+
+     // Function to retrieve the 'tag2str' map outside of this compilation unit
+     // (needed by module and backend macros so they can add to it)
+     std::map<int,std::string>& tag2str();
+
+     // Function to retrieve the 'components' set (needed by module and backend macros so they can add to it)
+     std::set<int>& components();
+
+     // Typedefs for standard stream manpulators. We need stream operator overloads for all of them. 
+     typedef std::ostream& (*manip1)( std::ostream& );
+     typedef std::basic_ios< std::ostream::char_type, std::ostream::traits_type > ios_type;
+     typedef ios_type& (*manip2)( ios_type& );
+     typedef std::ios_base& (*manip3)( std::ios_base& );
+
+     /// @{ Stream functions for use with LogMaster
+     LogMaster& operator<<(LogMaster&, const std::string&);
+     LogMaster& operator<<(LogMaster&, const LogTag&);
+     LogMaster& operator<<(LogMaster&, const endofmessage&);
+     // Overloads for stream manipulators (typedefs above)
+     LogMaster& operator<<(LogMaster&, const manip1);
+     LogMaster& operator<<(LogMaster&, const manip2);
+     LogMaster& operator<<(LogMaster&, const manip3);
+
+     // Stream function to convert everything else to strings before
+     // feeding into LogMaster (this way no-one needs to have the full
+     // declaration of the LogMaster class; I think the overhead
+     // should be negligible and is worth the segregation).
+     // Use 'ostringstream' so that any type with an ostream operator defined
+     // will be able to stream into the loggers.
+     template <typename TYPE>
+     LogMaster& operator << (LogMaster& logobj, const TYPE& input)
+     {
+       std::stringstream ss;
+       ss << input;
+       logobj << ss.str();
+       return logobj;
+     }
+     /// @}
+  }
+
+  /// Explicit const instance of the end of message struct in Gambit namespace
+  const Logging::endofmessage EOM = Logging::endofmessage();
+
+  /// Function to retrieve a reference to the Gambit global log object.
+  Logging::LogMaster& logger();
+
+}
+
+#endif /* defined(__logger_hpp__) */
---
+++ Logs/include/gambit/Logs/logmaster.hpp
@@ -0,0 +1,186 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Header for logging classes
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Ben Farmer
+///          (benjamin.farmer@monash.edu.au)
+///  \date 2014 Mar
+///
+///  \author Pat Scott
+///          (patscott@physics.mcgill.ca)
+///  \date 2014 Mar
+///
+///  *********************************************
+
+#ifndef __logmaster_hpp__
+#define __logmaster_hpp__
+
+// Standard libraries
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <deque>
+#include <fstream>
+#include <chrono> 
+#include <omp.h>
+
+// Gambit
+#include "gambit/Logs/logger.hpp"
+#include "gambit/Logs/log_tags.hpp"
+
+namespace Gambit
+{
+
+  namespace Logging
+  {
+    /// Forward declarations
+    class Message;
+    class BaseLogger;
+
+    /// Logging "controller" object
+    /// Keeps track of the various "Logger" objects
+    class LogMaster
+    {  
+       public:
+        /// Default constructor
+        LogMaster();
+
+        /// Alternate constructor
+        /// Mainly for testing; lets you pass in pre-built loggers and their tags
+        LogMaster(std::map<std::set<int>,BaseLogger*>&);
+
+        /// Destructor
+        /// If errors happen before the inifile is loaded, we need to dump the log messages 
+        /// (that have been buffered) into a default log file. These will be log messages coming from initialisation code and so on.
+        ~LogMaster();
+
+        /// Initialise dynamic memory required for thread safety
+        void init_memory();
+
+        /// Function to construct loggers according to blueprint
+        // This is the function that yaml_parser.hpp uses. You provide tags as a set of strings, and the filename as a string. We then construct the logger objects in here.
+        // This needs to be a vector of pairs rather than a map in case people want duplicate output streams of certain logs. In this case there will be duplicate keys, which a map cannot allow.
+        void initialise(std::vector<std::pair< std::set<std::string>, std::string>>&);
+
+        // Overload to allow using maps as input
+        void initialise(std::map<std::set<std::string>, std::string>&);
+
+        // Overload of initialise function to allow easier manual initialisation in standalone modules
+        void initialise(std::map<std::string, std::string>&);
+
+        // Function to silence all log messages
+        void disable();
+        // Function to check if all log messages are silenced
+        bool disabled();
+
+        /// Functions for stream input (actual stream operators which use these are defined in logger.cpp)
+        void input(const std::string&);
+        void input(const LogTag&);
+        void input(const endofmessage&);
+        // Overloads for stream manipulators (typedefs in logger.hpp)
+        void input(const manip1);
+        void input(const manip2);
+        void input(const manip3);
+
+        /// Main logging function (user-friendly overloaded version)
+        // Need a bunch of overloads of this to deal with 
+        void send(const std::string&);
+        void send(const std::string&,LogTag);
+        void send(const std::string&,LogTag,LogTag);
+        void send(const std::string&,LogTag,LogTag,LogTag);
+        void send(const std::string&,LogTag,LogTag,LogTag,LogTag);
+        void send(const std::string&,LogTag,LogTag,LogTag,LogTag,LogTag);
+        //...add more as needed
+
+        // stringstream versions of the above
+        void send(const std::ostringstream&);
+        void send(const std::ostringstream&,LogTag);
+        void send(const std::ostringstream&,LogTag,LogTag);
+        void send(const std::ostringstream&,LogTag,LogTag,LogTag);
+        void send(const std::ostringstream&,LogTag,LogTag,LogTag,LogTag);
+        void send(const std::ostringstream&,LogTag,LogTag,LogTag,LogTag,LogTag);
+        //...add more as needed
+
+        /// Internal version of main logging function
+        void send(const std::string&, std::set<LogTag>&);
+        void send(const std::string&, std::set<int>&);
+        void finalsend(const Message&);
+
+        // stringstream versions...
+        void send(const std::ostringstream&, std::set<LogTag>&);
+        void send(const std::ostringstream&, std::set<int>&);
+
+        /// Set the internal variables tracking which module and/or backend is currently running
+        void entering_module(int);
+        void leaving_module();
+        void entering_backend(int);
+        void leaving_backend();
+
+        /// @{ Setters for behaviour options 
+        /// Must be used before "initialise" in order to have any effect
+        /// Choose whether a separate log file for each MPI process is used
+        void set_separate_file_per_process(bool flag) {separate_file_per_process=flag;}
+
+        /// Choose whether "Debug" tagged log messages will be ignored (i.e. not logged)
+        void set_log_debug_messages(bool flag) {log_debug_messages=flag;}
+
+        /// @}        
+
+      private:
+        /// Empty the backlog buffer to the 'send' function
+        void empty_backlog();
+     
+        /// Map to identify loggers
+        std::map<std::set<int>,BaseLogger*> loggers;
+
+        /// Global ignore set; if these tags/integers are seen, ignore messages containing them.
+        std::set<int> ignore;
+  
+        /// Flag to set whether loggers have been initialised not
+        bool loggers_readyQ;
+
+        /// Flag to silence logger 
+        bool silenced;
+
+        /// Flag to store log messages for different processes in separate files
+        bool separate_file_per_process;
+
+        /// Flag to ignore Debug tagged messages
+        bool log_debug_messages;
+ 
+        /// MPI variables
+        int MPIrank;
+        int MPIsize;
+
+        /// Max number of threads that could potentially be running
+        int globlMaxThreads;
+
+        /// @{ Variables that need to be threadsafe
+
+        /// int current_function; Can generalise to this if we discover that we really want to...
+        int* current_module;  // index -1 means "not in any module"
+        int* current_backend; // index -1 means "not in any backend"
+
+        /// Buffer variables needed for stream logging
+        std::ostringstream* stream;
+        std::set<int>* streamtags;
+
+        /// Messages sent before logger objects are created will be buffered
+        /// Same for messages sent while inside omp parallel blocks
+        std::deque<Message>* backlog;
+
+        /// @}
+    };
+
+  } //end namespace Logging
+
+} // end namespace Gambit
+
+#endif
