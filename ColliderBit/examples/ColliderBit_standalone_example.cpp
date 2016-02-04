7054de4796484672e8cb0ea41225d08fa3fd3512
mwhi <martin.white@coepp.org.au>
2016-02-05 10:12:51 +1030

    The ColliderBit standalone example is finished!

--- ColliderBit/examples/ColliderBit_standalone_example.cpp
+++ ColliderBit/examples/ColliderBit_standalone_example.cpp
@@ -24,6 +24,8 @@ using namespace ColliderBit::Accessors;     // Helper functions that provide som
 using namespace ColliderBit::Functown;      // Functors wrapping the module's actual module functions
 using namespace BackendIniBit::Functown;    // Functors wrapping the backend initialisation functions
 
+// These functions are defined to allow GAMBIT dependencies to be satisfied properly without using GAMBIT
+// The user does not need to change these
 QUICK_FUNCTION(ColliderBit, MSSM_spectrum, NEW_CAPABILITY, createSpectrum, const Spectrum*, (MSSM30atQ,MSSM30atMGUT))
 QUICK_FUNCTION(ColliderBit, decay_rates, NEW_CAPABILITY, createDecays, DecayTable, (MSSM30atQ,MSSM30atMGUT), (MSSM_spectrum, const Spectrum*))
 QUICK_FUNCTION(ColliderBit, Z_decay_rates, NEW_CAPABILITY, createZDecays, DecayTable::Entry, (MSSM30atQ,MSSM30atMGUT))
@@ -35,8 +37,8 @@ QUICK_FUNCTION(ColliderBit, stau_1_decay_rates, NEW_CAPABILITY, createStau1Decay
 QUICK_FUNCTION(ColliderBit, stau_2_decay_rates, NEW_CAPABILITY, createStau2Decays, DecayTable::Entry, (MSSM30atQ,MSSM30atMGUT), (decay_rates, DecayTable), (MSSM_spectrum, const Spectrum*))
 
 // SLHA file for input: user can change name here
-// Note that it must contain the decay table for the LEP likelihoods to function properly
-
+// Note that it must contain the full decay table for the LEP likelihoods to function properly
+// See example below for reference
 const std::string inputFileName = "ColliderBit/data/standalone.slha";
 
 namespace Gambit
@@ -52,28 +54,13 @@ namespace Gambit
     
     void createDecays(DecayTable& outDecays)
     {
-      // Here we create a GAMBIT DecayTable from an SLHA file.
-      // This is a bit of a nasty example, as the DecayTable class stores
-      // stuff internally using SLHA2 PDG codes for sfermions, but we want
-      // to read an SLHA1 file -- but this is possible!  First we need to get
-      // a spectrum object that has already read the SLHA1 file and worked out
-      // which PDG codes need to be remapped to which others:
+      // This function makes a decay table from an input SLHA file
       const Spectrum* spec = (*Pipes::createDecays::Dep::MSSM_spectrum);
-      
-      // Then we need to pass the SLHA1 PDG --> SLHA2 PDG map to the constructor of a DecayTable, along with our SLHA1 file.
-      // The third argument below is the default context integer to give to PDG pairs identified with the particles
-      // involved in the decays.  The fourth argument forces context = 1 for all SM fermions though, so that we use gauge
-      // instead of mass eigenstates for them.
       outDecays = DecayTable(inputFileName, spec->PDG_translator(), 0, true);
-
-      // The equivalent code for reading decays from an SLHA2 file would be just
-      //  outDecays = DecayTable(inputFileName);
-      // or, if you needed to have SM fermions identified as their gauge eigenstates,
-      //  outDecays = DecayTable(inputFileName, 0, true);
-      // i.e. with SLHA2 files no spectrum object is required at all to make a DecayTable object.
     }
     
     void createZDecays(DecayTable::Entry& result){
+      // This function extracts the Z decay entry
       result.width_in_GeV = 2.4952;
       result.positive_error = 2.3e-03;
       result.negative_error = 2.3e-03;
@@ -85,7 +72,7 @@ namespace Gambit
     
     
     void createSelDecays(DecayTable::Entry& outSelDecays){
-
+      // This function extracts the left-handed selectron decay table
       // This is a little more complicated than the previous function
       // Need to get the string that corresponds to a left-handed selectron (the decay table entries are in the mass eigenstate basis)
       double max_mixing;
@@ -95,9 +82,7 @@ namespace Gambit
     }
 
     void createSerDecays(DecayTable::Entry& outSerDecays){
-
-      // This is a little more complicated than the previous function
-      // Need to get the string that corresponds to a left-handed selectron (the decay table entries are in the mass eigenstate basis)
+      // This function extracts the right-handed selectron decay table
       double max_mixing;
       const SubSpectrum* mssm = (*Pipes::createSerDecays::Dep::MSSM_spectrum)->get_HE();
       str x = slhahelp::mass_es_from_gauge_es("~e_R", max_mixing, mssm);
@@ -105,16 +90,15 @@ namespace Gambit
     }
 
     void createSmulDecays(DecayTable::Entry& outSmulDecays){
-      
+      // This function extracts the left-handed smuon decay table
       double max_mixing;
       const SubSpectrum* mssm = (*Pipes::createSmulDecays::Dep::MSSM_spectrum)->get_HE();
       str x = slhahelp::mass_es_from_gauge_es("~mu_L", max_mixing, mssm);
-      std::cout << "I think that the smu is " << x << std::endl;
       outSmulDecays = (*Pipes::createSmulDecays::Dep::decay_rates)(x);
     }
     
     void createSmurDecays(DecayTable::Entry& outSmurDecays){
-
+      //This function extracts the right-handed smuon decay table
       double max_mixing;
       const SubSpectrum* mssm = (*Pipes::createSmurDecays::Dep::MSSM_spectrum)->get_HE();
       str x = slhahelp::mass_es_from_gauge_es("~mu_R", max_mixing, mssm);
@@ -122,7 +106,7 @@ namespace Gambit
     }
     
     void createStau1Decays(DecayTable::Entry& outStau1Decays){
-
+      //This function extracts the stau1 decay table
       const SubSpectrum* mssm = (*Pipes::createStau1Decays::Dep::MSSM_spectrum)->get_HE();
       // Set these arguments by hand for this example
       const static double tol = 0.001;
@@ -132,7 +116,7 @@ namespace Gambit
     }
 
     void createStau2Decays(DecayTable::Entry& outStau2Decays){
-
+      //This function extracts the stau2 decay table
       const SubSpectrum* mssm = (*Pipes::createStau1Decays::Dep::MSSM_spectrum)->get_HE();
       const static double tol = 0.001;
       const static bool pterror=false;
@@ -152,6 +136,7 @@ int main()
     std::map<std::string, std::string> loggerinfo;
     
     // Define where the logs will end up
+    // User could change this if required
     std::string prefix("runs/ColliderBit_standalone/logs/");
 
     // Ensure that the above directory exists
@@ -168,17 +153,15 @@ int main()
     
     logger()<<"Running ColliderBit standalone example"<<LogTags::info<<EOM;
     
-    /*std::cout << std::endl << "My name is " << name() << std::endl;
-      std::cout << " I can calculate: " << endl << iCanDo << std::endl;
-      std::cout << " ...but I may need: " << endl << iMayNeed << std::endl << std::endl;*/
-    //std::cout << "I can do nevents: " << provides("nevents") << std::endl;
-    
     std::cout << std::endl << "My name is " << name() << std::endl;
     std::cout << " I can calculate: " << endl << iCanDo << std::endl;
     std::cout << " ...but I may need: " << endl << iMayNeed << std::endl << std::endl;
     
-    // Set up the LHC likelihood calculations
+    // We now set up the module functions that we wish use
+    // Dependencies and backend requirements of module functions are resolved by hand
     // WARNING: DO NOT EDIT UNLESS YOU ARE AN EXPERT
+
+    // Set up the LHC likelihood calculations
     calc_LHC_LogLike.resolveDependency(&runAnalyses);
     calc_LHC_LogLike.resolveBackendReq(&Backends::nulike_1_0_1::Functown::nulike_lnpiln); //treat systematics with a log normal distribution
     runAnalyses.resolveDependency(&getAnalysisContainer);
@@ -297,8 +280,6 @@ int main()
     LEP205_SLHA1_convention_xsec_stau2stau2bar.resolveDependency(&createZDecays);
 
     // L3 Neutralino all channels
-    //QUICK_FUNCTION(ColliderBit, L3_Neutralino_All_Channels_LLike, NEW_CAPABILITY, L3_Neutralino_All_Channels_Conservative_LLike, double, (MSSM30atQ, MSSM30atMGUT), (MSSM_spectrum, const Spectrum*), (LEP188_xsec_chi00_12, triplet<double>), (LEP188_xsec_chi00_13, triplet<double>), (LEP188_xsec_chi00_14, triplet<double>), (decay_rates, DecayTable))
-
     L3_Neutralino_All_Channels_Conservative_LLike.notifyOfModel("MSSM30atQ");
     L3_Neutralino_All_Channels_Conservative_LLike.resolveDependency(&createSpectrum);
     L3_Neutralino_All_Channels_Conservative_LLike.resolveDependency(&LEP188_SLHA1_convention_xsec_chi00_12);
@@ -311,7 +292,7 @@ int main()
     LEP188_SLHA1_convention_xsec_chi00_13.resolveDependency(&createZDecays);
     LEP188_SLHA1_convention_xsec_chi00_14.resolveDependency(&createSpectrum);
     LEP188_SLHA1_convention_xsec_chi00_14.resolveDependency(&createZDecays);
-    
+
     // Double-check which backend requirements have been filled with what
     std::cout << std::endl << "My function calc_LHC_LogLike has had its backend requirement on lnlike_marg_poisson filled by:" << std::endl;
     std::cout << ColliderBit::Pipes::calc_LHC_LogLike::BEreq::lnlike_marg_poisson_lognormal_error.origin() << "::";
@@ -350,28 +331,16 @@ int main()
     // User can edit this section to configure ColliderBit
     // See the ColiderBit manual for available options
     
-    // TO DO: Need a way of handling pythia options (they are not currently being used).
-    // This requires handling nested yaml options 
-    
     // First we have the LHC options
     std::vector<std::string> runTheseAnalyses;
     runTheseAnalyses.push_back("ATLAS_0LEP_20invfb");  // specify which LHC analyses to run
     getAnalysisContainer.setOption<std::vector<std::string>>("analysisNames",runTheseAnalyses);
 
+    // The standalone Pythia instance is given a name
+    // Can be set to anything, provided it matches the same name given below
     std::vector<std::string> pythiaNames;
     pythiaNames.push_back("Pythia_Standalone");
-    
-    std::vector<std::string> inputFiles;
-    inputFiles.push_back(inputFileName); // specify the input SLHA filename(s)
-    std::vector<std::string> pythiaOptions; // use this vector to store Pythia options
-    /*pythiaOptions.push_back("PartonLevel:MPI = off");
-    pythiaOptions.push_back("PartonLevel:ISR = on");
-    pythiaOptions.push_back("PartonLevel:FSR = on");
-    pythiaOptions.push_back("HadronLevel:all = on");
-    pythiaOptions.push_back("TauDecays:mode = 0");
-    pythiaOptions.push_back("SUSY:all = off");*/
-
-    YAML::Node Pythia_Standalone;
+    YAML::Node Pythia_Standalone;   
     Pythia_Standalone["pythiaOptions_1"].push_back("PartonLevel:MPI = off");
     Pythia_Standalone["pythiaOptions_1"].push_back("PartonLevel:ISR = on");
     Pythia_Standalone["pythiaOptions_1"].push_back("PartonLevel:FSR = on");
@@ -381,17 +350,11 @@ int main()
     Pythia_Standalone["pythiaOptions_1"].push_back("Beams:eCM = 8000");
     Pythia_Standalone["pythiaOptions_1"].push_back("Main:timesAllowErrors = 1000");
     getPythiaFileReader.setOption<YAML::Node>("Pythia_Standalone",Pythia_Standalone);
+    std::vector<std::string> inputFiles;
+    inputFiles.push_back(inputFileName); // specify the input SLHA filename for Pythia
     getPythiaFileReader.setOption<std::string>("Pythia_doc_path","Backends/installed/Pythia/8.212/share/Pythia8/xmldoc/"); // specify the Pythia xml file location
     getPythiaFileReader.setOption<std::vector<std::string>>("SLHA_filenames",inputFiles);
 
-    // Try and parse nested pythia options
-    //YAML::Node Pythia_Standalone = YAML::Load("{pythiaOptions_1: [PartonLevel:MPI = off,PartonLevel:ISR = on]}");
-    /*YAML::Node Pythia_Standalone;
-    Pythia_Standalone["key"] = "value";
-    Pythia_Standalone["pythiaOptions_1"].push_back("PartonLevel:MPI = off");
-    Pythia_Standalone["pythiaOptions_1"].push_back("SUSY:all = off");
-    std::cout << Pythia_Standalone["pythiaOptions_1"][0].as<std::string>() << "\n";*/
-    
     operateLHCLoop.setOption<std::vector<std::string>>("pythiaNames",pythiaNames);
     operateLHCLoop.setOption<int>("nEvents",5000.); // specify the number of simulated LHC events
     
@@ -410,8 +373,7 @@ int main()
       // Retrieve and print the LHC likelihood
       double loglike = calc_LHC_LogLike(0);
       std::cout << "LHC log likelihood is " << loglike << std::endl;
-      
-
+  
       // Call the ALEPH slepton likelihoods
       createSpectrum.reset_and_calculate();
       createDecays.reset_and_calculate();
@@ -453,7 +415,7 @@ int main()
       L3_Neutralino_All_Channels_Conservative_LLike.reset_and_calculate();
       
       std::cout << "L3 neutralino log likes " << L3_Neutralino_All_Channels_Conservative_LLike(0) << std::endl;
-      
+     
     }
   }
   catch (std::exception& e)
