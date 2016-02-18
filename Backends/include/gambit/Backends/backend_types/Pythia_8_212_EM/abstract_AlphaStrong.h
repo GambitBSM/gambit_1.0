41eaf1a7703d62fe40cac419152943ce8daaad0a
Martin <martin.white@coepp.org.au>
2016-02-19 08:22:46 +1100

    MJW adds lots of Pythia EM boss outputs.

---
+++ Backends/include/gambit/Backends/backend_types/Pythia_8_212_EM/abstract_AlphaStrong.h
@@ -0,0 +1,98 @@
+#ifndef __abstract_AlphaStrong_Pythia_8_212_EM_h__
+#define __abstract_AlphaStrong_Pythia_8_212_EM_h__
+
+#include "gambit/Backends/abstractbase.hpp"
+#include "forward_decls_abstract_classes.h"
+#include "forward_decls_wrapper_classes.h"
+#include <cstddef>
+
+#include "identification.hpp"
+
+// Forward declaration needed by the destructor pattern.
+void wrapper_deleter(CAT_3(BACKENDNAME,_,SAFE_VERSION)::Pythia8::AlphaStrong*);
+
+
+namespace CAT_3(BACKENDNAME,_,SAFE_VERSION)
+{
+    
+    
+    namespace Pythia8
+    {
+        class Abstract_AlphaStrong : virtual public AbstractBase
+        {
+            public:
+    
+                virtual void init(double, int, int, bool) =0;
+    
+                virtual void init__BOSS(double, int, int) =0;
+    
+                virtual void init__BOSS(double, int) =0;
+    
+                virtual void init__BOSS(double) =0;
+    
+                virtual void init__BOSS() =0;
+    
+                virtual void setThresholds(double, double, double) =0;
+    
+                virtual double alphaS(double) =0;
+    
+                virtual double alphaS1Ord(double) =0;
+    
+                virtual double alphaS2OrdCorr(double) =0;
+    
+                virtual double Lambda3() const =0;
+    
+                virtual double Lambda4() const =0;
+    
+                virtual double Lambda5() const =0;
+    
+                virtual double Lambda6() const =0;
+    
+                virtual double muThres(int) =0;
+    
+                virtual double muThres2(int) =0;
+    
+                virtual double facCMW(int) =0;
+    
+            public:
+                virtual void pointerAssign__BOSS(Abstract_AlphaStrong*) =0;
+                virtual Abstract_AlphaStrong* pointerCopy__BOSS() =0;
+    
+            private:
+                mutable AlphaStrong* wptr;
+    
+            public:
+                Abstract_AlphaStrong()
+                {
+                }
+    
+                void wrapper__BOSS(AlphaStrong* wptr_in)
+                {
+                    wptr = wptr_in;
+                    is_wrapped(true);
+                    can_delete_wrapper(true);
+                }
+    
+                AlphaStrong* wrapper__BOSS()
+                {
+                    return wptr;
+                }
+    
+                virtual ~Abstract_AlphaStrong()
+                {
+                    if (can_delete_wrapper())
+                    {
+                        can_delete_me(false);
+                        wrapper_deleter(wptr);
+                    }
+                }
+        };
+    }
+    
+}
+
+
+#include "gambit/Backends/backend_undefs.hpp"
+
+
+#endif /* __abstract_AlphaStrong_Pythia_8_212_EM_h__ */
---
+++ Backends/include/gambit/Backends/backend_types/Pythia_8_212_EM/abstract_SusyLesHouches.h
@@ -0,0 +1,121 @@
+#ifndef __abstract_SusyLesHouches_Pythia_8_212_EM_h__
+#define __abstract_SusyLesHouches_Pythia_8_212_EM_h__
+
+#include "gambit/Backends/abstractbase.hpp"
+#include "forward_decls_abstract_classes.h"
+#include "forward_decls_wrapper_classes.h"
+#include <string>
+#include <istream>
+#include "SLHAea/slhaea.h"
+#include <map>
+#include <vector>
+#include <cstddef>
+
+#include "identification.hpp"
+
+// Forward declaration needed by the destructor pattern.
+void wrapper_deleter(CAT_3(BACKENDNAME,_,SAFE_VERSION)::Pythia8::SusyLesHouches*);
+
+
+namespace CAT_3(BACKENDNAME,_,SAFE_VERSION)
+{
+    
+    
+    namespace Pythia8
+    {
+        class Abstract_SusyLesHouches : virtual public AbstractBase
+        {
+            public:
+    
+                virtual int readFile(::std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, bool) =0;
+    
+                virtual int readFile__BOSS(::std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int) =0;
+    
+                virtual int readFile__BOSS(::std::basic_string<char, std::char_traits<char>, std::allocator<char> >) =0;
+    
+                virtual int readFile__BOSS() =0;
+    
+                virtual int readFile(::std::basic_istream<char, std::char_traits<char> >&, int, bool) =0;
+    
+                virtual int readFile__BOSS(::std::basic_istream<char, std::char_traits<char> >&, int) =0;
+    
+                virtual int readFile__BOSS(::std::basic_istream<char, std::char_traits<char> >&) =0;
+    
+                virtual int readSLHAea(int, bool) =0;
+    
+                virtual int readSLHAea__BOSS(int) =0;
+    
+                virtual int readSLHAea__BOSS() =0;
+    
+                virtual void setSLHAea(const ::SLHAea::Coll*) =0;
+    
+                virtual void printHeader() =0;
+    
+                virtual void printFooter() =0;
+    
+                virtual void printSpectrum(int) =0;
+    
+                virtual void printSpectrum__BOSS() =0;
+    
+                virtual int checkSpectrum() =0;
+    
+                virtual std::basic_string<char, std::char_traits<char>, std::allocator<char> >& slhaFile_ref__BOSS() =0;
+    
+                virtual std::map<int, int, std::less<int>, std::allocator<std::pair<const int, int> > >& decayIndices_ref__BOSS() =0;
+    
+                virtual std::vector<std::basic_string<char>, std::allocator<std::basic_string<char> > >& qnumbersName_ref__BOSS() =0;
+    
+                virtual std::vector<std::basic_string<char>, std::allocator<std::basic_string<char> > >& qnumbersAntiName_ref__BOSS() =0;
+    
+                virtual int verbose() =0;
+    
+                virtual void verbose(int) =0;
+    
+                virtual void message(int, ::std::basic_string<char, std::char_traits<char>, std::allocator<char> >, ::std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int) =0;
+    
+                virtual void message__BOSS(int, ::std::basic_string<char, std::char_traits<char>, std::allocator<char> >, ::std::basic_string<char, std::char_traits<char>, std::allocator<char> >) =0;
+    
+                virtual void toLower(::std::basic_string<char, std::char_traits<char>, std::allocator<char> >&) =0;
+    
+            public:
+                virtual void pointerAssign__BOSS(Abstract_SusyLesHouches*) =0;
+                virtual Abstract_SusyLesHouches* pointerCopy__BOSS() =0;
+    
+            private:
+                mutable SusyLesHouches* wptr;
+    
+            public:
+                Abstract_SusyLesHouches()
+                {
+                }
+    
+                void wrapper__BOSS(SusyLesHouches* wptr_in)
+                {
+                    wptr = wptr_in;
+                    is_wrapped(true);
+                    can_delete_wrapper(true);
+                }
+    
+                SusyLesHouches* wrapper__BOSS()
+                {
+                    return wptr;
+                }
+    
+                virtual ~Abstract_SusyLesHouches()
+                {
+                    if (can_delete_wrapper())
+                    {
+                        can_delete_me(false);
+                        wrapper_deleter(wptr);
+                    }
+                }
+        };
+    }
+    
+}
+
+
+#include "gambit/Backends/backend_undefs.hpp"
+
+
+#endif /* __abstract_SusyLesHouches_Pythia_8_212_EM_h__ */
