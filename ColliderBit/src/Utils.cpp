5fde2e35df6bb3a6cada3560098a448819bab7b0
Andy Buckley <andy.buckley@cern.ch>
2016-02-26 16:03:24 +0000

    Tweaks to filtering functions, not trying a less efficient way without the lambda functions

--- ColliderBit/src/Utils.cpp
+++ ColliderBit/src/Utils.cpp
@@ -1,4 +1,4 @@
-#include "gambit/ColliderBit/Utils.h"
+#include "gambit/ColliderBit/Utils.hpp"
 using namespace std;
 
 namespace Gambit {
@@ -14,11 +14,11 @@ namespace Gambit {
     void filtereff(std::vector<HEPUtils::Particle*>& particles, double eff) {
       vector<HEPUtils::Particle*> todel;
       for (HEPUtils::Particle* p : particles) {
-        if (!random_bool(eff)) todel.push_back();
+        if (!random_bool(eff)) todel.push_back(p);
       }
       for (HEPUtils::Particle* p : todel) {
         delete p;
-        particles.erase(p);
+        particles.erase(find(particles.begin(), particles.end(), p));
       }
 
       /// @todo This is better... but segfaults?
@@ -35,11 +35,11 @@ namespace Gambit {
     inline void filtereff_etapt(std::vector<HEPUtils::Particle*>& particles, const HEPUtils::BinnedFn2D<double>& eff_etapt) {
       vector<HEPUtils::Particle*> todel;
       for (HEPUtils::Particle* p : particles) {
-        if (!random_bool(eff_etapt, p->abseta(), p->pT())) todel.push_back();
+        if (!random_bool(eff_etapt, p->abseta(), p->pT())) todel.push_back(p);
       }
       for (HEPUtils::Particle* p : todel) {
         delete p;
-        particles.erase(p);
+        particles.erase(find(particles.begin(), particles.end(), p));
       }
 
       /// @todo This is better... but segfaults?
