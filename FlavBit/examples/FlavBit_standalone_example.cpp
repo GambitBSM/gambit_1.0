2baab0687e5c6ca0cb62799f49b90ebfbda5562d
Martin <martin.white@coepp.org.au>
2016-02-27 21:13:48 +1100

    Finished first draft of FlavBit_standalone_example. It now needs to be tested by Marcin and Nazila.

--- FlavBit/examples/FlavBit_standalone_example.cpp
+++ FlavBit/examples/FlavBit_standalone_example.cpp
@@ -155,6 +155,137 @@ int main()
     SI_BRBKstarmumu_17_19.resolveDependency(&Debug);
     SI_BRBKstarmumu_17_19.resolveDependency(&Debug_LL);
     SI_BRBKstarmumu_17_19.resolveBackendReq(&Backends::SuperIso_3_4::Functown::SI_BRBKstarmumu_CONV);
+
+    // Now do the b2ll likelihood
+    b2ll_likelihood.resolveDependency(&SI_FlavBit_fill);
+    b2ll_likelihood.resolveDependency(&b2ll_measurements);
+    b2ll_likelihood.resolveDependency(&Debug);
+    b2ll_likelihood.resolveDependency(&Debug_LL);
+
+    // b2ll_measurements depends on:
+    // - FlavBit_fill
+    // - Bsmumu_untag
+    // - Bdmumu
+    // - Debug_Cap
+    // - Debug_Cap_LL
+
+    b2ll_measurements.resolveDependency(&SI_FlavBit_fill);
+    b2ll_measurements.resolveDependency(&Debug);
+    b2ll_measurements.resolveDependency(&Debug_LL);
+    b2ll_measurements.resolveDependency(&SI_Bsmumu_untag);
+    b2ll_measurements.resolveDependency(&SI_Bdmumu);
+
+    // Resolve dependencies  of SI_Bsmumu_untag
+    // These are:
+    //  - FlavBit_fill
+    //  - Debug_Cap
+    //  - Debug_Cap_LL
+    // Plus BE reqs:
+    //  - Bsmumu_untag
+    //  - CW_calculator
+    //  - C_calculator_base1
+    //  - Cprime_calculator
+    //  - CQ_calculator
+
+    SI_Bsmumu_untag.resolveDependency(&SI_FlavBit_fill);
+    SI_Bsmumu_untag.resolveDependency(&Debug);
+    SI_Bsmumu_untag.resolveDependency(&Debug_LL);
+    SI_Bsmumu_untag.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Bsmumu_untag);
+    SI_Bsmumu_untag.resolveBackendReq(&Backends::SuperIso_3_4::Functown::CW_calculator);
+    SI_Bsmumu_untag.resolveBackendReq(&Backends::SuperIso_3_4::Functown::C_calculator_base1);
+    SI_Bsmumu_untag.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Cprime_calculator);
+    SI_Bsmumu_untag.resolveBackendReq(&Backends::SuperIso_3_4::Functown::CQ_calculator);
+    
+    // Resolve dependencies of SI_Bdmumu
+    // These are:
+    //  - FlavBit_fill
+    //  - Debug_Cap
+    //  - Debug_Cap_LL
+    // Plus BE reqs:
+    // - Bdmumu
+    // - CW_calculator
+    // - C_calculator_base1
+    // - CQ_calculator
+    
+    SI_Bdmumu.resolveDependency(&SI_FlavBit_fill);
+    SI_Bdmumu.resolveDependency(&Debug);
+    SI_Bdmumu.resolveDependency(&Debug_LL);
+    SI_Bdmumu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Bdmumu);
+    SI_Bdmumu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::CW_calculator);
+    SI_Bdmumu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::C_calculator_base1);
+    SI_Bdmumu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::CQ_calculator);
+
+    // Finally do the SL_LL
+    // This depends on:
+    // - FlavBit_fill
+    // - SL_M
+    // - Debug_Cap
+    // - Debug_Cap_LL
+
+    SL_likelihood.resolveDependency(&SI_FlavBit_fill);
+    SL_likelihood.resolveDependency(&SL_measurements);
+    SL_likelihood.resolveDependency(&Debug);
+    SL_likelihood.resolveDependency(&Debug_LL);
+
+    // Resolve dependencies of SL_measurements
+    // which are:
+    // - FlavBit_fill
+    // - Btaunu
+    // - BDtaunu
+    // - BDtaunu_BDenu
+    // - Kmunu_pimunu
+    // - Dstaunu
+    // - Dsmunu
+    // - Dmunu
+    // - Debug_Cap
+    // - Debug_Cap_LL
+
+    SL_measurements.resolveDependency(&SI_FlavBit_fill);
+    SL_measurements.resolveDependency(&Debug);
+    SL_measurements.resolveDependency(&Debug_LL);
+    SL_measurements.resolveDependency(&SI_Btaunu);
+    SL_measurements.resolveDependency(&SI_BDtaunu);
+    SL_measurements.resolveDependency(&SI_BDtaunu_BDenu);
+    SL_measurements.resolveDependency(&SI_Kmunu_pimunu);
+    SL_measurements.resolveDependency(&SI_Dstaunu);
+    SL_measurements.resolveDependency(&SI_Dsmunu);
+    SL_measurements.resolveDependency(&SI_Dmunu);
+
+    // Finally resolve all of the individual dependencies and backend reqs
+    // These are:
+    // - FlavBit_fill
+    // - Debug_Cap
+    // - Debug_Cap_LL
+    // BE Req: BDtaunu, etc
+
+    SI_Btaunu.resolveDependency(&SI_FlavBit_fill);
+    SI_Btaunu.resolveDependency(&Debug);
+    SI_Btaunu.resolveDependency(&Debug_LL);
+    SI_Btaunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Btaunu);
+    SI_BDtaunu.resolveDependency(&SI_FlavBit_fill);
+    SI_BDtaunu.resolveDependency(&Debug);
+    SI_BDtaunu.resolveDependency(&Debug_LL);
+    SI_BDtaunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::BDtaunu);
+    SI_BDtaunu_BDenu.resolveDependency(&SI_FlavBit_fill);
+    SI_BDtaunu_BDenu.resolveDependency(&Debug);
+    SI_BDtaunu_BDenu.resolveDependency(&Debug_LL);
+    SI_BDtaunu_BDenu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::BDtaunu_BDenu);
+    SI_Kmunu_pimunu.resolveDependency(&SI_FlavBit_fill);
+    SI_Kmunu_pimunu.resolveDependency(&Debug);
+    SI_Kmunu_pimunu.resolveDependency(&Debug_LL);
+    SI_Kmunu_pimunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Kmunu_pimunu);
+    SI_Dstaunu.resolveDependency(&SI_FlavBit_fill);
+    SI_Dstaunu.resolveDependency(&Debug);
+    SI_Dstaunu.resolveDependency(&Debug_LL);
+    SI_Dstaunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Dstaunu);
+    SI_Dsmunu.resolveDependency(&SI_FlavBit_fill);
+    SI_Dsmunu.resolveDependency(&Debug);
+    SI_Dsmunu.resolveDependency(&Debug_LL);
+    SI_Dsmunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Dsmunu);
+    SI_Dmunu.resolveDependency(&SI_FlavBit_fill);
+    SI_Dmunu.resolveDependency(&Debug);
+    SI_Dmunu.resolveDependency(&Debug_LL);
+    SI_Dmunu.resolveBackendReq(&Backends::SuperIso_3_4::Functown::Dmunu);
     
     // Double-check which backend requirements have been filled with what
     std::cout << std::endl << "My function SI_FlavBit_fill  has had its backend requirement on Init_param filled by:" << std::endl;
@@ -163,6 +294,21 @@ int main()
     std::cout << std::endl << "My function SI_FlavBit_fill  has had its backend requirement on slha_adjust filled by:" << std::endl;
     std::cout << FlavBit::Pipes::SI_FlavBit_fill::BEreq::slha_adjust.origin() << "::";
     std::cout << FlavBit::Pipes::SI_FlavBit_fill::BEreq::slha_adjust.name() << std::endl;
+
+    // Double-check which backend requirements have been filled with what
+    std::cout << std::endl << "My function SI_Bdmumu  has had its backend requirement on Bdmumu filled by:" << std::endl;
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::Bdmumu.origin() << "::";
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::Bdmumu.name() << std::endl;
+    std::cout << std::endl << "My function SI_Bdmumu  has had its backend requirement on CW_calculator filled by:" << std::endl;
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::CW_calculator.origin() << "::";
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::CW_calculator.name() << std::endl;
+    std::cout << std::endl << "My function SI_Bdmumu  has had its backend requirement on C_calculator_base1 filled by:" << std::endl;
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::C_calculator_base1.origin() << "::";
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::C_calculator_base1.name() << std::endl;
+      std::cout << std::endl << "My function SI_Bdmumu  has had its backend requirement on CQ_calculator filled by:" << std::endl;
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::CQ_calculator.origin() << "::";
+    std::cout << FlavBit::Pipes::SI_Bdmumu::BEreq::CQ_calculator.name() << std::endl;
+    
     
     // Double-check which dependencies have been filled with whatever (not every combination is shown)
     std::cout << std::endl << "My function b2sll_likelihood has had its dependency on FlavBit_fill filled by:" << endl;
@@ -235,11 +381,32 @@ int main()
       SI_BRBKstarmumu_60_80.reset_and_calculate();
       SI_BRBKstarmumu_15_17.reset_and_calculate();
       SI_BRBKstarmumu_17_19.reset_and_calculate();
+      b2sll_measurements.reset_and_calculate();
       b2sll_likelihood.reset_and_calculate();
      
       double loglike = b2sll_likelihood(0);
       std::cout << "b2sll log likelihood is " << loglike << std::endl;
 
+      // Calculate the b2ll likelihood
+      SI_Bsmumu_untag.reset_and_calculate();
+      SI_Bdmumu.reset_and_calculate();
+      b2ll_measurements.reset_and_calculate();
+      b2ll_likelihood.reset_and_calculate();
+      loglike = b2ll_likelihood(0);
+      std::cout << "b2ll log likelihood is " << loglike << std::endl;
+
+      // Calculate the SL likelihood
+      SI_Btaunu.reset_and_calculate();
+      SI_BDtaunu.reset_and_calculate();
+      SI_BDtaunu_BDenu.reset_and_calculate();
+      SI_Kmunu_pimunu.reset_and_calculate();
+      SI_Dstaunu.reset_and_calculate();
+      SI_Dsmunu.reset_and_calculate();
+      SI_Dmunu.reset_and_calculate();
+      SL_measurements.reset_and_calculate();
+      SL_likelihood.reset_and_calculate();
+      loglike = SL_likelihood(0);
+      std::cout << "SL log likelihood is " << loglike << std::endl;
       
     }
   }
