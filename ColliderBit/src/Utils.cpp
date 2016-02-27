42642d022177edf043353910eab03f8bf1faa75b
Abram Krislock <a.m.b.krislock@fys.uio.no>
2016-02-27 14:34:37 +0100

    Removed a lingering "inline" keyword in ColliderBit/src/Utils.cpp

--- ColliderBit/src/Utils.cpp
+++ ColliderBit/src/Utils.cpp
@@ -32,7 +32,7 @@ namespace Gambit {
 
 
         /// Utility function for filtering a supplied particle vector by sampling wrt a binned 2D efficiency map in |eta| and pT
-    inline void filtereff_etapt(std::vector<HEPUtils::Particle*>& particles, const HEPUtils::BinnedFn2D<double>& eff_etapt) {
+    void filtereff_etapt(std::vector<HEPUtils::Particle*>& particles, const HEPUtils::BinnedFn2D<double>& eff_etapt) {
       vector<HEPUtils::Particle*> todel;
       for (HEPUtils::Particle* p : particles) {
         if (!random_bool(eff_etapt, p->abseta(), p->pT())) todel.push_back(p);
