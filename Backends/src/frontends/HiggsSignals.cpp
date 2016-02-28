8ffe57b6c774ba2151db2b1f839fc0f01d4bad13
Pat Scott <p.scott@imperial.ac.uk>
2016-02-28 23:06:18 +0100

    Fixed HiggsSignals initialisation bug. Forced backend initialisation function to do scan-level initialisation in serial. MPI processes are now forbidden to leave the initialisation function before all process have initialised HiggsSignals.
    	modified:   Backends/src/frontends/HiggsSignals.cpp

--- Backends/src/frontends/HiggsSignals.cpp
+++ Backends/src/frontends/HiggsSignals.cpp
@@ -18,28 +18,58 @@
 
 #include "gambit/Backends/frontend_macros.hpp"
 #include "gambit/Backends/frontends/HiggsSignals.hpp"
-
+#include "gambit/Utils/mpiwrapper.hpp"
 
 BE_INI_FUNCTION
 {
 
-  // Scan-level initialisation
   static bool scan_level = true;
-  if(scan_level){
+
+  // Scan-level initialisation
+  if(scan_level)
+  {
+    
     int nHneut = 3; // number of neutral higgses
     int nHplus = 1; // number of charged higgses
-    
-    // initialize HiggsSignals with the latest results
-    initialize_HiggsSignals_latestresults(nHneut,nHplus);
+    int pdf = 2;    // which pdf style to use for Higgs lineshape (2=Gaussian)
 
-    // choose which pdf style to use for Higgs lineshape
-    int pdf = 2; // Gaussian
-    setup_pdf(pdf);
+    int rank = 0;
+    int totprocs = 1;
+
+    #ifdef WITH_MPI
+    if(GMPI::Is_initialized())
+    {
+      GMPI::Comm comm;
+      rank = comm.Get_rank();
+      totprocs = comm.Get_size();
+    }
+    #endif
+
+    // Initialize HiggsSignals with the latest results and set up the lineshape pdf.
+    // Do this one-by-one for each MPI process, as HS is insane (it writes files
+    // here then reads them back in later).
+    for (int i = 0; i < totprocs; i++)
+    {
+      
+      if (i == rank)
+      {
+        initialize_HiggsSignals_latestresults(nHneut,nHplus);   
+        setup_pdf(pdf);
+      }
+
+      #ifdef WITH_MPI
+      if (totprocs > 1)
+      {
+        GMPI::Comm comm;
+        comm.Barrier();
+      }
+      #endif
+
+    }
+
   }
-  scan_level = false;
 
-  // clean-up
-  // finish_HiggsSignals();
+  scan_level = false;
 
 }
 END_BE_INI_FUNCTION
