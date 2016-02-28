3ebe92c1654d5c4cd0b306bf299c824258cbceed
Pat Scott <p.scott@imperial.ac.uk>
2016-02-28 22:53:52 +0000

    Added MPI initialisation function daisychain fix for HB and DS as well. Updated nulike 1.0.2 md5sum. 	modified:   Backends/src/frontends/DarkSUSY.cpp 	modified:   Backends/src/frontends/HiggsBounds.cpp 	modified:   cmake/backends.cmake

--- Backends/src/frontends/HiggsBounds.cpp
+++ Backends/src/frontends/HiggsBounds.cpp
@@ -18,29 +18,59 @@
 
 #include "gambit/Backends/frontend_macros.hpp"
 #include "gambit/Backends/frontends/HiggsBounds.hpp"
-
+#include "gambit/Utils/mpiwrapper.hpp"
 
 BE_INI_FUNCTION
 {
 
- // Scan-level initialisation
   static bool scan_level = true;
-  if(scan_level){
-    // initialize LEP chisq tables
-    initialize_HiggsBounds_chisqtables();
-  
+
+  // Scan-level initialisation
+  if(scan_level)
+  {
+
     int nHneut = 3; // number of neutral higgses
     int nHplus = 1; // number of charged higgses
     int ANA = 1;    // indicates LEP-only analysis
-    
-    // initialize HiggsBounds to LEP only
-    initialize_HiggsBounds_int(nHneut,nHplus,ANA);
+
+    int rank = 0;
+    int totprocs = 1;
+
+    #ifdef WITH_MPI
+    if(GMPI::Is_initialized())
+    {
+      GMPI::Comm comm;
+      rank = comm.Get_rank();
+      totprocs = comm.Get_size();
+    }
+    #endif
+
+    // Initialize HiggsBounds. Do this one-by-one for each MPI process, as HB
+    // is insane (it writes files here then reads them back in later).
+    for (int i = 0; i < totprocs; i++)
+    {
+
+      if (i == rank)
+      {
+        // initialize LEP chisq tables
+        initialize_HiggsBounds_chisqtables();
+        // initialize HiggsBounds to LEP only
+        initialize_HiggsBounds_int(nHneut,nHplus,ANA);
+      }
+
+      #ifdef WITH_MPI
+      if (totprocs > 1)
+      {
+        GMPI::Comm comm;
+        comm.Barrier();
+      }
+      #endif
+
+    }
+
   }
+  
   scan_level = false;
   
-  // clean-up
-  // finish_HiggsBounds_chisqtables();
-  // finish_HiggsBounds();
-
 }
 END_BE_INI_FUNCTION
