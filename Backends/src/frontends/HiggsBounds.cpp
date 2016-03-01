e5a1e2a4b760de48df44ddc1404f394a844562d7
Pat Scott <p.scott@imperial.ac.uk>
2016-03-01 21:47:40 +0000

    Tidied up a couple of fairly superficial things wrt file locking. 	modified:   Backends/src/frontends/DarkSUSY.cpp 	modified:   Backends/src/frontends/DarkSUSY_5_1_1.cpp 	modified:   Backends/src/frontends/HiggsBounds.cpp 	modified:   Backends/src/frontends/HiggsSignals.cpp 	modified:   Utils/include/gambit/Utils/file_lock.hpp 	modified:   Utils/src/file_lock.cpp 	modified:   Utils/src/util_functions.cpp

--- Backends/src/frontends/HiggsBounds.cpp
+++ Backends/src/frontends/HiggsBounds.cpp
@@ -36,7 +36,7 @@ BE_INI_FUNCTION
     int ANA = 1;    // indicates LEP-only analysis
     // Initialize HiggsBounds. Do this one-by-one for each MPI process with
     // locks, as HB writes files here then reads them back in later (crazy). 
-    Utils::FileLock mylock("HiggsBounds_" STRINGIFY(SAFE_VERSION) "_init_lock");
+    Utils::FileLock mylock("HiggsBounds_" STRINGIFY(SAFE_VERSION) "_init");
     mylock.get_lock();
     // --braces just for dramatic effect--
     { 
