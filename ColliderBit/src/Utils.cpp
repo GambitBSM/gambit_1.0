a30c499ed9e390f2dccdba9ca90892d2923a3c63
Andy Buckley <andy.buckley@cern.ch>
2016-03-04 17:00:14 +0000

    Also reconvert other efficiency filtering functions back to using lambda expressions, now wrapping the remove_if in a call to the container's erase function

--- ColliderBit/src/Utils.cpp
+++ ColliderBit/src/Utils.cpp
@@ -12,43 +12,39 @@ namespace Gambit {
 
 
     void filtereff(std::vector<HEPUtils::Particle*>& particles, double eff) {
-      vector<HEPUtils::Particle*> todel;
-      for (HEPUtils::Particle* p : particles) {
-        if (!random_bool(eff)) todel.push_back(p);
-      }
-      for (HEPUtils::Particle* p : todel) {
-        delete p;
-        particles.erase(find(particles.begin(), particles.end(), p));
-      }
-
-      /// @todo This is better... but segfaults?
-      // std::remove_if(particles.begin(), particles.end(),
-      //                [&](const HEPUtils::Particle* p) {
-      //                  const bool kill = !random_bool(eff);
-      //                  if (kill) delete p;
-      //                  return kill;
-      //                } );
+      particles.erase(std::remove_if(particles.begin(), particles.end(),
+                                     [&](const HEPUtils::Particle* p) {
+                                       const bool kill = !random_bool(eff);
+                                       if (kill) delete p;
+                                       return kill; } ),
+                      particles.end());
+      // vector<HEPUtils::Particle*> todel;
+      // for (HEPUtils::Particle* p : particles) {
+      //   if (!random_bool(eff)) todel.push_back(p);
+      // }
+      // for (HEPUtils::Particle* p : todel) {
+      //   delete p;
+      //   particles.erase(find(particles.begin(), particles.end(), p));
+      // }
     }
 
 
         /// Utility function for filtering a supplied particle vector by sampling wrt a binned 2D efficiency map in |eta| and pT
     void filtereff_etapt(std::vector<HEPUtils::Particle*>& particles, const HEPUtils::BinnedFn2D<double>& eff_etapt) {
-      vector<HEPUtils::Particle*> todel;
-      for (HEPUtils::Particle* p : particles) {
-        if (!random_bool(eff_etapt, p->abseta(), p->pT())) todel.push_back(p);
-      }
-      for (HEPUtils::Particle* p : todel) {
-        delete p;
-        particles.erase(find(particles.begin(), particles.end(), p));
-      }
-
-      /// @todo This is better... but segfaults?
-      // std::remove_if(particles.begin(), particles.end(),
-      //                [&](const HEPUtils::Particle* p) {
-      //                  const bool kill = !random_bool(eff_etapt, p->abseta(), p->pT());
-      //                  if (kill) delete p;
-      //                  return kill;
-      //                } );
+      particles.erase(std::remove_if(particles.begin(), particles.end(),
+                                     [&](const HEPUtils::Particle* p) {
+                                       const bool kill = !random_bool(eff_etapt, p->abseta(), p->pT());
+                                       if (kill) delete p;
+                                       return kill; } ),
+                      particles.end());
+      // vector<HEPUtils::Particle*> todel;
+      // for (HEPUtils::Particle* p : particles) {
+      //   if (!random_bool(eff_etapt, p->abseta(), p->pT())) todel.push_back(p);
+      // }
+      // for (HEPUtils::Particle* p : todel) {
+      //   delete p;
+      //   particles.erase(find(particles.begin(), particles.end(), p));
+      // }
     }
 
 
