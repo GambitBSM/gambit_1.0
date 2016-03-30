8d12b0f0899b679acff9535f28aa4e27f6f43165
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2016-03-30 02:24:36 +0200

    BOSS: Updated base header files.

--- Backends/include/gambit/Backends/wrapperbase.hpp
+++ Backends/include/gambit/Backends/wrapperbase.hpp
@@ -1,132 +1,23 @@
 #ifndef __WRAPPERBASE__
 #define __WRAPPERBASE__
 
-#include <iostream>
 
 
+class AbstractBase;
 
-template <typename T>
 class WrapperBase
 {
-    public:
-        T* myBEptr;
-        T* const & BEptr;
-        bool skip_delete;
-
-        // Constructor
-        WrapperBase(T* BEptr_in) : myBEptr(BEptr_in), BEptr(myBEptr), skip_delete(false)
-        {
-            // BEptr->wrapper__BOSS(this);
-        }
-
-        WrapperBase(T* const & BEptr_in, bool) : BEptr(BEptr_in), skip_delete(false)
-        {
-            // BEptr->wrapper__BOSS(this);
-        }
-
-
-        // Copy constructor: 
-        WrapperBase(const WrapperBase<T>& in) : myBEptr(in.BEptr->pointerCopy__BOSS()), BEptr(myBEptr), skip_delete(false)
-        {
-            // BEptr->wrapper__BOSS(this);
-        }
-
-        // Assignment operator
-        WrapperBase& operator=(const WrapperBase<T>& in)
-        {
-            if (this != &in) 
-            { 
-                BEptr->pointerAssign__BOSS(in.BEptr); 
-            }
-            return *this;        
-        }
-
-        // Destructor
-        ~WrapperBase()
-        {
-            if (not skip_delete)
-            {
-                if (BEptr->can_delete_me())
-                {
-                    BEptr->can_delete_wrapper(false);
-                    delete BEptr;
-                }
-            }
-        }
-
-
     protected:
+        AbstractBase* BEptr;
+        bool delete_BEptr;
+    public:
+        bool can_delete_BEptr() { return delete_BEptr; }
+        void set_delete_BEptr(bool del_wrp_in) { delete_BEptr = del_wrp_in; }
 
-        // Function used to return pointer-to-wrapper
-        template<typename U, typename V>
-        U* pointer_returner(V* ptr)
-        {
-            if (ptr->is_wrapped())
-            {
-                return (ptr->wrapper__BOSS());
-            }
+    public:
+        WrapperBase(AbstractBase* BEptr_in) : BEptr(BEptr_in), delete_BEptr(true) {}
 
-            else
-            {
-                U* wptr = new U(ptr);
-                ptr->can_delete_wrapper(true);
-                return wptr;
-            }
-        }
-
-
-        // Function used to return reference-to-wrapper
-        template<typename U, typename V>
-        U& reference_returner(V* ptr)
-        {
-            if (ptr->is_wrapped())
-            {
-                return *(ptr->wrapper__BOSS());
-            }
-
-            else
-            {
-                U* wptr = new U(ptr);
-                ptr->can_delete_wrapper(true);
-                return *wptr;
-            }
-        }
-
-        // Const version of the above function
-        template<typename U, typename V>
-        U& reference_returner(V* ptr) const
-        {
-            if (ptr->is_wrapped())
-            {
-                return *(ptr->wrapper__BOSS());
-            }
-
-            else
-            {
-                U* wptr = new U(ptr);
-                ptr->can_delete_wrapper(true);
-                return *wptr;
-            }
-        }
-
-
-        // Const-const version of the above function
-        template<typename U, typename V>
-        U& reference_returner(const V* ptr)
-        {
-            if (ptr->is_wrapped())
-            {
-                return *(ptr->wrapper__BOSS());
-            }
-
-            else
-            {
-                U* wptr = new U(ptr);
-                ptr->can_delete_wrapper(true);
-                return *wptr;
-            }
-        }
-
+        virtual ~WrapperBase() {}
 };
 
 
