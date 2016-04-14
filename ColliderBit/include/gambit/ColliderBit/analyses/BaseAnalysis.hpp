fc5940b05eb38328ff0f1390dbf3ac755b4126eb
Abram Krislock <a.m.b.krislock@fys.uio.no>
2016-04-14 14:32:09 +0200

    More doxygen work... I found here that SpecializablePythia's init_external function probably needs to be renamed for clarity.
     Changes to be committed:
    	modified:   ColliderBit/include/gambit/ColliderBit/analyses/BaseAnalysis.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/analyses/HEPUtilsAnalysisContainer.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/colliders/BaseCollider.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/colliders/SimplePythia.hpp
    	modified:   ColliderBit/include/gambit/ColliderBit/colliders/SpecializablePythia.hpp
    	modified:   ColliderBit/todo.txt

--- ColliderBit/include/gambit/ColliderBit/analyses/BaseAnalysis.hpp
+++ ColliderBit/include/gambit/ColliderBit/analyses/BaseAnalysis.hpp
@@ -15,18 +15,23 @@ namespace Gambit {
 
     /// A simple container for the result of one signal region from one analysis.
     struct SignalRegionData {
-      std::string analysis_name; ///< The name of the analysis common to all signal regions.
-      std::string sr_label; ///< A label for the particular signal region of the analysis.
-      double n_observed; ///< The number of events passing selection for this signal region,
-                         ///< as reported by the experiment.
-      double n_signal; ///< The number of simulated model events passing selection for this signal region.
-      double n_signal_at_lumi; ///< n_signal, scaled to the experimental luminosity.
-      double n_background; ///< The number of standard model events expected to pass the selection for this signal
-                           ///< region, as reported by the experiment.
-      double signal_sys; ///< The absolute systematic error of n_signal.
-      double background_sys; ///< The absolute systematic error of n_background.
+      /// @name Analysis and signal region specification:
+      //@{
+        std::string analysis_name; ///< The name of the analysis common to all signal regions.
+        std::string sr_label; ///< A label for the particular signal region of the analysis.
+      //@}
 
-      SignalRegionData() {}
+      /// @name Signal region data:
+      //@{
+        double n_observed; ///< The number of events passing selection for this signal region,
+                           ///< as reported by the experiment.
+        double n_signal; ///< The number of simulated model events passing selection for this signal region.
+        double n_signal_at_lumi; ///< n_signal, scaled to the experimental luminosity.
+        double n_background; ///< The number of standard model events expected to pass the selection for this signal
+                             ///< region, as reported by the experiment.
+        double signal_sys; ///< The absolute systematic error of n_signal.
+        double background_sys; ///< The absolute systematic error of n_background.
+      //@}
     };
 
 
@@ -34,15 +39,12 @@ namespace Gambit {
     template <typename EventT>
     class BaseAnalysis {
       private:
-      /// @name Member variables
-      //@{
         double _ntot, _xsec, _xsecerr, _luminosity;
         std::vector<SignalRegionData> _results;
         typedef EventT EventType;
-      //@}
 
       public:
-      /// @name Construction, Destruction, and Recycling
+      /// @name Construction, Destruction, and Recycling:
       //@{
         BaseAnalysis() : _ntot(0), _xsec(-1), _xsecerr(-1), _luminosity(-1) {  }
         virtual ~BaseAnalysis() { }
@@ -54,32 +56,32 @@ namespace Gambit {
       //@}
 
       public:
-      /// @name Event analysis, event number, and cross section functions
+      /// @name Event analysis, event number, and cross section functions:
       //@{
-        /// Analyze the event (accessed by reference)
+        /// Analyze the event (accessed by reference).
         void analyze(const EventT& e) { analyze(&e); }
-        /// @brief Analyze the event (accessed by pointer)
-        /// @note Needs to be called from Derived::analyze()
+        /// Analyze the event (accessed by pointer).
+        /// @note Needs to be called from Derived::analyze().
         virtual void analyze(const EventT*) { _ntot += 1; }
 
-        /// @brief Return the total number of events seen so far
+        /// Return the total number of events seen so far.
         double num_events() const { return _ntot; }
-        /// @brief Return the cross-section (in pb)
+        /// Return the cross-section (in pb).
         double xsec() const { return _xsec; }
-        /// @brief Return the cross-section error (in pb)
+        /// Return the cross-section error (in pb).
         double xsec_err() const { return _xsecerr; }
-        /// @brief Return the cross-section relative error
+        /// Return the cross-section relative error.
         double xsec_relerr() const { return xsec() > 0 ? xsec_err()/xsec() : -1; }
-        /// @brief Return the cross-section per event seen (in pb)
+        /// Return the cross-section per event seen (in pb).
         double xsec_per_event() const { return (xsec() > 0) ? xsec()/num_events() : -1; }
-        /// @brief Return the integrated luminosity (in inverse pb)
+        /// Return the integrated luminosity (in inverse pb).
         double luminosity() const { return _luminosity; }
-        /// @brief Set the cross-section and its error (in pb)
+        /// Set the cross-section and its error (in pb).
         void set_xsec(double xs, double xserr) { _xsec = xs; _xsecerr = xserr; }
-        /// @brief Set the integrated luminosity (in inverse pb)
+        /// Set the integrated luminosity (in inverse pb).
         void set_luminosity(double lumi) { _luminosity = lumi; }
 
-        /// Get the collection of SignalRegionData for likelihood computation
+        /// Get the collection of SignalRegionData for likelihood computation.
         std::vector<SignalRegionData> get_results() {
           if (_results.empty()) collect_results();
           return _results;
@@ -87,22 +89,22 @@ namespace Gambit {
       //@}
 
       protected:
-      /// @name Protected collection functions
+      /// @name Protected collection functions:
       //@{
-        /// Add the given result to the internal results list
+        /// Add the given result to the internal results list.
         void add_result(const SignalRegionData& res) { _results.push_back(res);}
-        /// Gather together the info for likelihood calculation
+        /// Gather together the info for likelihood calculation.
         virtual void collect_results() = 0;
       //@}
 
       public:
-      /// @name (Re-)Initialization functions
+      /// @name (Re-)Initialization functions:
       //@{
-        /// @brief General init for any analysis of this type.
+        /// General init for any analysis of this type.
         virtual void init(const std::vector<std::string>&) {}
-        /// @brief General init for any collider of this type - no settings version.
+        /// General init for any collider of this type - no settings version.
         virtual void init() { }
-        /// @brief Scale by number of input events and xsec
+        /// Scale by number of input events and xsec.
         virtual void scale(double factor=-1) {
           if (factor < 0)
             factor = _luminosity * _xsec / _ntot;
@@ -115,9 +117,9 @@ namespace Gambit {
       //@}
 
       public:
-      /// @name BaseAnalysis combination operations
+      /// @name BaseAnalysis combination operations:
       //@{
-        /// @brief An operator to do xsec-weighted combination of analysis runs
+        /// An operator to do xsec-weighted combination of analysis runs.
         virtual void add(BaseAnalysis* other) {
           if (_results.empty()) collect_results();
           std::vector<SignalRegionData> otherResults = other->get_results();
@@ -129,7 +131,7 @@ namespace Gambit {
           }
           _ntot += other->num_events();
         }
-        /// @brief Add cross-sections and errors for two different process types
+        /// Add cross-sections and errors for two different process types.
         void add_xsec(double xs, double xserr) {
           if (xs > 0) {
             if (xsec() <= 0) {
@@ -140,7 +142,7 @@ namespace Gambit {
             }
           }
         }
-        /// @brief Combine cross-sections and errors for the same process type, assuming uncorrelated errors
+        /// Combine cross-sections and errors for the same process type, assuming uncorrelated errors.
         void improve_xsec(double xs, double xserr) {
           if (xs > 0) {
             if (xsec() <= 0) {
--- ColliderBit/include/gambit/ColliderBit/colliders/SpecializablePythia.hpp
+++ ColliderBit/include/gambit/ColliderBit/colliders/SpecializablePythia.hpp
@@ -9,70 +9,61 @@
 #include "gambit/ColliderBit/colliders/BaseCollider.hpp"
 #include "SLHAea/slhaea.h"
 
-namespace Gambit
-{
+namespace Gambit {
+  namespace ColliderBit {
 
-  namespace ColliderBit
-  {
-
-    /// @brief A specializable, recyclable class interfacing ColliderBit and Pythia.
+    /// A specializable, recyclable class interfacing ColliderBit and Pythia.
     class SpecializablePythia : public BaseCollider<Pythia8::Event> {
-
       protected:
-
-        /// @name Pythia-specific variables
-        ///@{
         Pythia8::Pythia* _pythiaInstance;
         Pythia8::Pythia* _pythiaBase;
-        ///@}
-
-        /// @name Specialization function pointers
-        ///@{
         std::vector<std::string> _pythiaSettings;
         void (*_specialInit)(SpecializablePythia*);
-        ///@}
-
+      /// @name Getters:
+      //@{
       public:
-
+        /// Get the Pythia instance.
         const Pythia8::Pythia* pythia() const { return _pythiaInstance; }
+      //@}
 
-        /// @name Custom exceptions
-        ///@{
-
+      /// @name Custom exceptions:
+      ///@{
+      public:
+        /// An exception for when Pythia fails to initialize.
         class InitializationError : public std::exception {
           virtual const char* what() const throw() {
             return "Pythia could not initialize.";
           }
         };
-
+        /// An exception for when Pythia fails to generate events.
         class EventFailureError : public std::exception {
           virtual const char* what() const throw() {
             return "Pythia could not make the next event.";
           }
         };
+      ///@}
 
-        ///@}
-
-        /// @name Construction, Destruction, and Recycling
-        ///@{
+      /// @name Construction, Destruction, and Recycling:
+      ///@{
+      public:
         SpecializablePythia() : _pythiaInstance(nullptr), _pythiaBase(nullptr) {}        
         ~SpecializablePythia();
         void clear();
-        ///@}
+      ///@}
 
-        /// @name (Re-)Initialization functions
-        ///@{
-
-        /// @brief Add a command to the list of settings used by "init"
+      /// @name (Re-)Initialization functions
+      ///@{
+      public:
+        /// Add a command to the list of settings used by "init".
         void addToSettings(const std::string& command) { _pythiaSettings.push_back(command); }
-
-        /// @brief Create a useless Pythia instance just to print the banner
+        /// Create a useless Pythia instance just to print the banner.
         void banner(const std::string pythiaDocPath) { Pythia8::Pythia myPythia(pythiaDocPath); }
-
-        /// @brief Initialize with no settings (error): override version
+        /// Initialize with no settings (error): override version.
         void init() { std::cout<<"No settings given to Pythia!\n\n"; throw InitializationError(); }
 
-        /// @brief Initialize from some external settings: override version
+        /// Initialize from some external settings: override version.
+        /// @note A string denoting the path to Pythia's xmldoc directory is
+        /// @note assumed to be at the end of the settings vector:
         void init(const std::vector<std::string>& externalSettings) {
           std::string docPath = externalSettings.back();
           std::vector<std::string> settings(externalSettings);
@@ -80,47 +71,47 @@ namespace Gambit
           init(docPath, settings);
         }
 
-        /// @brief Initialize from some external settings
+        /// Initialize from some external settings.
+        /// @note This override is most commonly used in ColliderBit.
         void init(const std::string pythiaDocPath,
                   const std::vector<std::string>& externalSettings,
                   const SLHAea::Coll* slhaea=nullptr, std::ostream& os=std::cout);
-
-        /// @brief Special initialization for externally provided process generation.
+        /// Initialize from some external settings.
+        /// @todo rename. 
         void init_external(const std::string pythiaDocPath,
                            const std::vector<std::string>& externalSettings,
                            const SLHAea::Coll* slhaea=nullptr, std::ostream& os=std::cout);
 
+        /// Initialize from some external settings, assuming no given SLHAea instance.
         void init(const std::string pythiaDocPath,
                   const std::vector<std::string>& externalSettings, std::ostream& os) {
           init(pythiaDocPath, externalSettings, nullptr, os);
         }
-
+        /// Initialize from some external settings, assuming no given SLHAea instance.
+        /// @todo rename. 
         void init_external(const std::string pythiaDocPath,
                            const std::vector<std::string>& externalSettings, std::ostream& os) {
           init_external(pythiaDocPath, externalSettings, nullptr, os);
         }
       
-        /// @brief Specialize this Pythia interface to Gambit with a specialization function.
+        /// Specialize this Pythia interface to Gambit with a specialization function.
         void resetSpecialization(const std::string&);
+      ///@}
 
-        ///@}
-
-        /// @name Event generation and cross section functions
-        ///@{
-
-        /// @brief Event generation for any Pythia interface to Gambit.
+      /// @name Event generation and cross section functions
+      ///@{
+      public:
+        /// Event generation for any Pythia interface to Gambit.
         void nextEvent(EventType& event) const {
           // Try to make and populate an event
           if (!_pythiaInstance->next()) throw EventFailureError();
           event = _pythiaInstance->event;
         }
-
-        /// @brief Report the cross section (in pb) at the end of the subprocess.
+        /// Report the cross section (in pb) at the end of the subprocess.
         double xsec_pb() const { return _pythiaInstance->info.sigmaGen() * 1e9; }
-        /// @brief Report the cross section uncertainty (in pb) at the end of the subprocess.
+        /// Report the cross section uncertainty (in pb) at the end of the subprocess.
         double xsecErr_pb() const { return _pythiaInstance->info.sigmaErr() * 1e9; }
-
-        ///@}
+      ///@}
 
      };
 
