b6b33ce35bfd3ea36b8867e4884716a96302ab2f
Christoph Weniger <c.weniger@uva.nl>
2016-05-11 11:54:08 +0200

    Update getRateMisc treatment for Weff
     Changes to be committed:
    	modified:   src/RelicDensity.cpp

--- DarkBit/src/RelicDensity.cpp
+++ DarkBit/src/RelicDensity.cpp
@@ -360,7 +360,8 @@ namespace Gambit {
           Weff = Weff + 
             it->genRate->set("v", 2*peff/sqrt(mDM*mDM+peff*peff))*s/GeV2tocm3s1;
         }
-        // FIXME: Should we add genRateMisc to Weff?
+        // Add genRateMisc to Weff
+        Weff = Weff + annProc.genRateMisc->set("v", 2*peff/sqrt(mDM*mDM+peff*peff))*s/GeV2tocm3s1;
         if ( Weff->getNArgs() != 1 )
           DarkBit_error().raise(LOCAL_INFO, 
               "RD_eff_annrate_from_ProcessCatalog: Wrong number of arguments.\n"
