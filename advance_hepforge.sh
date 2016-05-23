7ed11a4c7ad837a5f3de6b252ce5e2c1ef68ba7c
Pat Scott <p.scott@imperial.ac.uk>
2016-05-23 22:47:36 +0200

    Added SUSY-HIT to advance hepforge script. 	modified:   advance_hepforge.sh

--- advance_hepforge.sh
+++ advance_hepforge.sh
@@ -10,3 +10,5 @@ wget http://www.hepforge.org/archive/gm2calc/gm2calc-1.0.0.tar.gz
 mv gm2calc-1.0.0.tar.gz ../Backends/downloaded/
 wget http://www.hepforge.org/archive/gm2calc/gm2calc-1.1.0.tar.gz
 mv gm2calc-1.1.0.tar.gz ../Backends/downloaded/
+wget http://www.itp.kit.edu/~maggie/SUSY-HIT/susyhit.tar.gz
+mv susyhit.tar.gz ../Backends/downloaded
