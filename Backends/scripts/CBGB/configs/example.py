76112ca55229949f8523f5e43f9c74b12a7212e5
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2016-05-26 00:35:36 +0200

    CBGB: Added a Fortran source file for the CBGB example. Modified example.py.

--- Backends/scripts/CBGB/configs/example.py
+++ Backends/scripts/CBGB/configs/example.py
@@ -8,26 +8,24 @@
 
 
 # List paths to input Fortran files:
-input_files = ['examples/minimal_test_p1.f90', 'examples/minimal_test_p2.f90']
+input_files = ['example/example.f90']
 
+# List the functions/subroutines to be loaded:
+load_functions = ['one', 'two', 'text', 'MA::text', 'non_existing_function']
+
+# List the common blocks to be loaded:
+load_common_blocks = ['dummy_block', 'non_existing_block'] 
+
+
+
+# Choose name mangling scheme: 'gfortran', 'ifort' or 'g77'
+name_mangling = 'gfortran'
 
 # Choose 'fixed' or 'free' format:
 format = 'free'  
 
-# List the functions/subroutines to be loaded:
-load_functions = ['Five', 'six', 'text', 'MA::text', 'Partial_sums']
-
-# List the common blocks to be loaded:
-load_common_blocks = ['WIDTHA_HDEC', 'WIDTHHL_HDEC', 'WIDTHHH_HDEC', 
-                      'WIDTHHC_HDEC', 'WISUSY_HDEC', 'WISFER_HDEC', 
-                      'HD_golddec'] 
-
-
 # Convert tabs to how many spaces?
 tabs_to_n_spaces = 6
-
-# Choose name mangling scheme: 'gfortran', 'ifort' or 'g77'
-name_mangling = 'gfortran'
 
 # Settings for constructing a GAMBIT capability corresponding to each function/subroutine:
 f_capability_prefix = ''
