94545376f3249ade97e0979d97109ab5847494a0
Pat Scott <p.scott@imperial.ac.uk>
2016-06-22 11:49:08 +0100

    Sorry... 	new file:   Utils/include/gambit/Utils/standalone_utils.hpp 	new file:   Utils/src/standalone_utils.cpp

---
+++ Utils/include/gambit/Utils/standalone_utils.hpp
@@ -0,0 +1,33 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Utilities needed to use a GAMBIT
+///  module as a standalone analysis code.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Pat Scott  
+///          (patscott@physics.mcgill.ca)
+///  \date 2014 Feb
+///
+///  *********************************************
+
+#ifndef __standalone_utils_hpp__
+#define __standalone_utils_hpp__
+
+#include "gambit/Utils/util_functions.hpp"
+
+namespace Gambit
+{
+
+  /// Logger setup standalone utility function
+  void initialise_standalone_logs(str);
+
+}
+
+
+#endif //__standalone_utils_hpp__
+
