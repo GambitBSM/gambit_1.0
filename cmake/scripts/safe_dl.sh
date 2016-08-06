1e4eb5f423fc82a7925a9d7faa2528c9b089b8da
Pat Scott <p.scott@imperial.ac.uk>
2016-08-06 10:14:28 +0100

    more attempts to get downloads working for torsten 	modified:   ../cmake/scripts/safe_dl.sh

--- cmake/scripts/safe_dl.sh
+++ cmake/scripts/safe_dl.sh
@@ -36,7 +36,7 @@ if command -v axel >/dev/null; then
     $2 -E echo "Axel failed! The link probably redirects to https. Falling back to wget/curl..." 
   fi
 fi
-if [ ${axel_worked} = "0" ]; then
+if [ "${axel_worked}" = "0" ]; then
   if command -v wget >/dev/null; then
     wget $3 -O $1/${filename}
   elif command -v curl >/dev/null; then
@@ -48,7 +48,7 @@ if [ ${axel_worked} = "0" ]; then
 fi
 # Check the MD5 sum
 md5=$($2 -E md5sum $1/${filename} | sed 's#\s.*##g')
-if [ ${md5} != $4 ]; then
+if [ "${md5}" != "$4" ]; then
   $2 -E cmake_echo_color --red --bold  "ERROR: MD5 sum of downloaded file $1/${filename} does not match"
   $2 -E cmake_echo_color --red --bold  "Expected: $4"
   $2 -E cmake_echo_color --red --bold  "Found:    ${md5}"
