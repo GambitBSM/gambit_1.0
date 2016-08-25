f8b289be5a2f8de4473e26d779a6df3823252ebb
James McKay <j.mckay14@imperial.ac.uk>
2016-08-25 17:21:33 -0400

    added a Lilith model parameters structure to hold Lilith input 	new file:   ../Backends/include/gambit/Backends/backend_types/Lilith.hpp

---
+++ Backends/include/gambit/Backends/backend_types/Lilith.hpp
@@ -0,0 +1,41 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Definitions of types
+///  for the HiggsBounds backend.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author James McKay
+///          (j.mckay14@imperial.ac.uk)
+///  \date 2016 Aug
+///
+///  *********************************************
+#include "gambit/Utils/util_types.hpp"
+
+#ifndef __Lilith_types_hpp__
+#define __Lilith_types_hpp__
+
+namespace Gambit
+{
+  
+  struct lilith_ModelParameters
+  {
+    
+      double mh;
+      double CU;
+      double CD;
+      double CV;
+      double CGa;
+      double Cg;
+      double BRinv;
+      double BRund;
+  };
+  
+  
+}
+
+#endif
