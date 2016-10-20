ad54d99ae62ae8d82eee388590f8c0cca53b95de
mchrzasz <mchrzasz@cern.ch>
2016-10-20 08:37:53 +0200

    Defined the model with WC

---
+++ Models/include/gambit/Models/models/WC.hpp
@@ -0,0 +1,24 @@
+//  GAMBIT: Global and Modular BSM Inference Tool
+//  *********************************************
+//
+//  Singlet DM
+//
+//  *********************************************
+//
+//  Authors
+//  =======
+//
+//  (add name and date if you modify)
+//
+//  Marcin Chrzaszcz & Martin White
+//  2016 October
+//  *********************************************
+
+#ifndef __WC_hpp__   
+#define __WC_hpp__   
+
+
+#define MODEL WC
+   START_MODEL
+   DEFINEPARS(CQ0b1, CQ0b2, CQ0b3, CQ1b1, CQ1b2, CQ1b3, CQpb1, CQpb2, CQpb3)
+#undef MODEL  
