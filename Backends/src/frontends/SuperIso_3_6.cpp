574bd0e943eaf12b38438c8ad8155b493705805c
mchrzasz <mchrzasz@cern.ch>
2016-10-22 03:20:35 +0200

    removed warning

--- Backends/src/frontends/SuperIso_3_6.cpp
+++ Backends/src/frontends/SuperIso_3_6.cpp
@@ -48,7 +48,7 @@ BE_NAMESPACE
     CQ_calculator(2,byVal(CQ0b),byVal(CQ1b),byVal(mu_W),byVal(mu_b),param);
     Cprime_calculator(2,byVal(Cpb),byVal(CQpb),byVal(mu_W),byVal(mu_b),param);
     /*double BR =*/  BRBKstarll(2,0,byVal(q2_min), byVal(q2_max), byVal(obs),byVal(C0b),byVal(C1b),byVal(C2b),byVal(CQ0b),byVal(CQ1b),byVal(Cpb),byVal(CQpb),param,byVal(mu_b));
-    
+
     // filling results
     Flav_KstarMuMu_obs results;
     results.q2_min=Q2_min;
@@ -84,34 +84,34 @@ BE_NAMESPACE
    const double q2_max=Q2_max;
 
 
-   // the WC will be done via Delta C modification      
-   double Re_DeltaC7=param->Re_DeltaC7;                 
-   double Im_DeltaC7=param->Im_DeltaC7;                 
-   double Re_DeltaC9=param->Re_DeltaC9;                 
-   double Im_DeltaC9=param->Im_DeltaC9;                 
-   double Re_DeltaC10=param->Re_DeltaC10;               
-   double Im_DeltaC10=param->Im_DeltaC10;               
-   double Re_DeltaCQ1=param->Re_DeltaCQ1;               
-   double Im_DeltaCQ1=param->Im_DeltaCQ1;               
-   double Re_DeltaCQ2=param->Re_DeltaCQ2;               
-   double Im_DeltaCQ2=param->Im_DeltaCQ2;               
+   // the WC will be done via Delta C modification
+   double Re_DeltaC7=param->Re_DeltaC7;
+   //   double Im_DeltaC7=param->Im_DeltaC7;
+   double Re_DeltaC9=param->Re_DeltaC9;
+   //double Im_DeltaC9=param->Im_DeltaC9;
+   double Re_DeltaC10=param->Re_DeltaC10;
+   //double Im_DeltaC10=param->Im_DeltaC10;
+   double Re_DeltaCQ1=param->Re_DeltaCQ1;
+   double Im_DeltaCQ1=param->Im_DeltaCQ1;
+   double Re_DeltaCQ2=param->Re_DeltaCQ2;
+   double Im_DeltaCQ2=param->Im_DeltaCQ2;
 
    CW_calculator(2,byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),param);
    C_calculator_base1(byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),byVal(C0b),byVal(C1b),byVal(C2b),byVal(mu_b),param);
    CQ_calculator(2,byVal(CQ0b),byVal(CQ1b),byVal(mu_W),byVal(mu_b),param);
    Cprime_calculator(2,byVal(Cpb),byVal(CQpb),byVal(mu_W),byVal(mu_b),param);
 
-     // the prime WC I don't care about :P   
-     //now hacking the WC:    
-     C0b[7]+=Re_DeltaC7;      
-     C0b[9]+=Re_DeltaC9;      
-     C0b[10]+=Re_DeltaC10;    
+     // the prime WC I don't care about :P
+     //now hacking the WC:
+     C0b[7]+=Re_DeltaC7;
+     C0b[9]+=Re_DeltaC9;
+     C0b[10]+=Re_DeltaC10;
      CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1);
      CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2);
 
 
      /*double BR =*/  BRBKstarll(2,0,byVal(q2_min), byVal(q2_max), byVal(obs),byVal(C0b),byVal(C1b),byVal(C2b),byVal(CQ0b),byVal(CQ1b),byVal(Cpb),byVal(CQpb),param,byVal(mu_b));
-   
+
    // filling results
    Flav_KstarMuMu_obs results;
    results.q2_min=Q2_min;
@@ -162,21 +162,21 @@ BE_NAMESPACE
 	double mu_b=param->mass_b_1S/2.;
 	double C0w[11],C1w[11],C2w[11],C0b[11],C1b[11],C2b[11],Cpb[11];
 	std::complex<double> CQpb[3];
-	
-	// the WC will be done via Delta C modification 
+
+	// the WC will be done via Delta C modification
 	double Re_DeltaC7=param->Re_DeltaC7;
-	double Im_DeltaC7=param->Im_DeltaC7;
+	//	double Im_DeltaC7=param->Im_DeltaC7;
 	double Re_DeltaC9=param->Re_DeltaC9;
-	double Im_DeltaC9=param->Im_DeltaC9;
+	//double Im_DeltaC9=param->Im_DeltaC9;
 	double Re_DeltaC10=param->Re_DeltaC10;
-	double Im_DeltaC10=param->Im_DeltaC10;
-	double Re_DeltaCQ1=param->Re_DeltaCQ1;
-	double Im_DeltaCQ1=param->Im_DeltaCQ1;
-	double Re_DeltaCQ2=param->Re_DeltaCQ2;
-	double Im_DeltaCQ2=param->Im_DeltaCQ2;
+	//double Im_DeltaC10=param->Im_DeltaC10;
+	//double Re_DeltaCQ1=param->Re_DeltaCQ1;
+	//double Im_DeltaCQ1=param->Im_DeltaCQ1;
+	//double Re_DeltaCQ2=param->Re_DeltaCQ2;
+	//double Im_DeltaCQ2=param->Im_DeltaCQ2;
 
 	//superIso doesn't handle imaginary part of WC
- 
+
 	// now this has to be got from paramenters, need Nazila to answere the email
 	cout<<"Checking WC at W scale, C7= "<<C0w[7]+C1w[7]+C2w[7]<<" ,C9= "<<C0w[9]+C1w[9]+C2w[9]<<" ,C10= "<<C0w[10]+C1w[10]+C2w[10]<<endl;
 	CW_calculator(2,byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),param); // since param.SM=1 we calcule WC at SM
@@ -192,12 +192,12 @@ BE_NAMESPACE
 	//CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1);
 	//CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2);
 
-	
+
 	//result = bsgamma(byVal(C0b),byVal(C1b),byVal(C2b),byVal(Cpb),byVal(mu_b),byVal(mu_W),param);
 	result = bsgamma_Ecut(byVal(C0b),byVal(C1b),byVal(C2b),byVal(Cpb),byVal(mu_b),byVal(mu_W), E_t, param);
       }
     return result;
-    
+
   }
   //###################################################################################
   double Bsll_untag_CONV(struct parameters *param, int flav)
@@ -242,35 +242,35 @@ BE_NAMESPACE
 	double C0b[11],C1b[11],C2b[11],C0w[11],C1w[11],C2w[11],Cpb[11];
 	std::complex<double> CQ0b[3],CQ1b[3],CQpb[3];
 
-	// the WC will be done via Delta C modification      
-	double Re_DeltaC7=param->Re_DeltaC7;                 
-	double Im_DeltaC7=param->Im_DeltaC7;                 
-	double Re_DeltaC9=param->Re_DeltaC9;                 
-	double Im_DeltaC9=param->Im_DeltaC9;                 
-	double Re_DeltaC10=param->Re_DeltaC10;               
-	double Im_DeltaC10=param->Im_DeltaC10;               
-	double Re_DeltaCQ1=param->Re_DeltaCQ1;               
-	double Im_DeltaCQ1=param->Im_DeltaCQ1;               
-	double Re_DeltaCQ2=param->Re_DeltaCQ2;               
-	double Im_DeltaCQ2=param->Im_DeltaCQ2;               
+	// the WC will be done via Delta C modification
+	double Re_DeltaC7=param->Re_DeltaC7;
+	//double Im_DeltaC7=param->Im_DeltaC7;
+	double Re_DeltaC9=param->Re_DeltaC9;
+	//double Im_DeltaC9=param->Im_DeltaC9;
+	double Re_DeltaC10=param->Re_DeltaC10;
+	//double Im_DeltaC10=param->Im_DeltaC10;
+	double Re_DeltaCQ1=param->Re_DeltaCQ1;
+	double Im_DeltaCQ1=param->Im_DeltaCQ1;
+	double Re_DeltaCQ2=param->Re_DeltaCQ2;
+	double Im_DeltaCQ2=param->Im_DeltaCQ2;
 
 
 	// now this has to be got from paramenters, need Nazila to answere the email
-	
+
 	CW_calculator(flav,byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),param);
 	C_calculator_base1(byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),byVal(C0b),byVal(C1b),byVal(C2b),byVal(mu_b),param);
 	CQ_calculator(flav,byVal(CQ0b),byVal(CQ1b),byVal(mu_W),byVal(mu_b),param);
 	Cprime_calculator(flav,byVal(Cpb),byVal(CQpb),byVal(mu_W),byVal(mu_b),param);
 
-	// the prime WC I don't care about :P 
-	//now hacking the WC:                 
-	C0b[7]+=Re_DeltaC7;                   
-	C0b[9]+=Re_DeltaC9;                   
-	C0b[10]+=Re_DeltaC10;                 
-	CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1); 
-	CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2); 
-	
-	
+	// the prime WC I don't care about :P
+	//now hacking the WC:
+	C0b[7]+=Re_DeltaC7;
+	C0b[9]+=Re_DeltaC9;
+	C0b[10]+=Re_DeltaC10;
+	CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1);
+	CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2);
+
+
 	result = Bsll_untag(flav,byVal(C0b),byVal(C1b),byVal(C2b),byVal(CQ0b),byVal(CQ1b),byVal(Cpb),byVal(CQpb),param ,byVal(mu_b));
       }
 
@@ -326,34 +326,34 @@ BE_NAMESPACE
 	double C0b[11],C1b[11],C2b[11],C0w[11],C1w[11],C2w[11];
 	std::complex<double> CQ0b[3],CQ1b[3];
 
-	// the WC will be done via Delta C modification  
-	double Re_DeltaC7=param->Re_DeltaC7;             
-	double Im_DeltaC7=param->Im_DeltaC7;             
-	double Re_DeltaC9=param->Re_DeltaC9;             
-	double Im_DeltaC9=param->Im_DeltaC9;             
-	double Re_DeltaC10=param->Re_DeltaC10;           
-	double Im_DeltaC10=param->Im_DeltaC10;           
-	double Re_DeltaCQ1=param->Re_DeltaCQ1;           
-	double Im_DeltaCQ1=param->Im_DeltaCQ1;           
-	double Re_DeltaCQ2=param->Re_DeltaCQ2;           
-	double Im_DeltaCQ2=param->Im_DeltaCQ2;           
+	// the WC will be done via Delta C modification
+	double Re_DeltaC7=param->Re_DeltaC7;
+	//double Im_DeltaC7=param->Im_DeltaC7;
+	double Re_DeltaC9=param->Re_DeltaC9;
+	//double Im_DeltaC9=param->Im_DeltaC9;
+	double Re_DeltaC10=param->Re_DeltaC10;
+	//double Im_DeltaC10=param->Im_DeltaC10;
+	double Re_DeltaCQ1=param->Re_DeltaCQ1;
+	double Im_DeltaCQ1=param->Im_DeltaCQ1;
+	double Re_DeltaCQ2=param->Re_DeltaCQ2;
+	double Im_DeltaCQ2=param->Im_DeltaCQ2;
 
-	
+
 	// now this has to be got from paramenters, need Nazila to answere the email
-	
+
 	CW_calculator(2,byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),param);
 	C_calculator_base1(byVal(C0w),byVal(C1w),byVal(C2w),byVal(mu_W),byVal(C0b),byVal(C1b),byVal(C2b),byVal(mu_b),param);
 	CQ_calculator(2,byVal(CQ0b),byVal(CQ1b),byVal(mu_W),byVal(mu_b),param);
-	
-	// the prime WC I don't care about :P 
-	//now hacking the WC:                 
-	C0b[7]+=Re_DeltaC7;                   
-	C0b[9]+=Re_DeltaC9;                   
-	C0b[10]+=Re_DeltaC10;                 
-	CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1); 
-	CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2); 
 
-	
+	// the prime WC I don't care about :P
+	//now hacking the WC:
+	C0b[7]+=Re_DeltaC7;
+	C0b[9]+=Re_DeltaC9;
+	C0b[10]+=Re_DeltaC10;
+	CQ0b[1]+=std::complex<double>(Re_DeltaCQ1, Im_DeltaCQ1);
+	CQ0b[2]+=std::complex<double>(Re_DeltaCQ2, Im_DeltaCQ2);
+
+
 	result = Bdll(byVal(flav),(C0b),byVal(C1b),byVal(C2b),byVal(CQ0b),byVal(CQ1b),param,byVal(mu_b));
       }
 
