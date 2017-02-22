5b79eead5968e6e99ec9b506f98d878b86782f2c
Martin <martin.white@coepp.org.au>
2017-02-22 11:54:47 +1030

    MJW adds CMS 0 lepton 13 TeV analysis to the analysis list.

--- ColliderBit/src/ColliderBit.cpp
+++ ColliderBit/src/ColliderBit.cpp
@@ -148,7 +148,7 @@ namespace Gambit
       GET_COLLIDER_RUNOPTION(nEvents, int);
 
       // Nicely ask the entire loop to be quiet
-      std::cout.rdbuf(0);
+      //std::cout.rdbuf(0);
 
       // For every collider requested in the yaml file:
       for (iter = pythiaNames.cbegin(); iter != pythiaNames.cend(); ++iter)
