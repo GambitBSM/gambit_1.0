96081551793b1a042ff9af5504a023a48b639e73
Jonathan Cornell <cornellj@physics.mcgill.ca>
2017-04-18 19:00:30 -0400

    Resolved MicrOmegas allowed models issue.

--- Backends/include/gambit/Backends/frontends/MicrOmegas_SingletDM_3_6_9_2.hpp
+++ Backends/include/gambit/Backends/frontends/MicrOmegas_SingletDM_3_6_9_2.hpp
@@ -19,27 +19,23 @@
 
 LOAD_LIBRARY
 
-BE_ALLOW_MODELS(SingletDM, Halo_gNFW, Halo_gNFW_rho0, Halo_gNFW_rhos, Halo_Einasto, Halo_Einasto_rho0, Halo_Einasto_rhos)
-// FIXME the presence of the various halo models is just a workaround to allow initializing MO without SingletDM
-// model)
-
-BE_FUNCTION(assignVal, int, (char*,double),"assignVal","assignVal")
-BE_FUNCTION(vSigma, double, (double, double, int), "vSigma","vSigma")
-BE_FUNCTION(darkOmega, double, (double*, int, double), "darkOmega", "oh2")
-BE_FUNCTION(sortOddParticles, int, (char*), "sortOddParticles","mass_spectrum")
-BE_FUNCTION(cleanDecayTable, void, (), "cleanDecayTable", "cleanDecayTable")
-BE_FUNCTION(nucleonAmplitudes, int, (double(*)(double,double,double,double), double*, double*, double*, double*), "nucleonAmplitudes", "nucleonAmplitudes" )
-BE_FUNCTION(FeScLoop, double, (double, double, double, double), "FeScLoop", "FeScLoop")
-BE_FUNCTION(calcScalarQuarkFF, void, (double, double, double, double), "calcScalarQuarkFF", "calcScalarQuarkFF")
+BE_FUNCTION(assignVal, int, (char*,double),"assignVal","assignVal", (SingletDM))
+BE_FUNCTION(vSigma, double, (double, double, int), "vSigma","vSigma", (SingletDM))
+BE_FUNCTION(darkOmega, double, (double*, int, double), "darkOmega", "oh2", (SingletDM))
+BE_FUNCTION(sortOddParticles, int, (char*), "sortOddParticles","mass_spectrum", (SingletDM))
+BE_FUNCTION(cleanDecayTable, void, (), "cleanDecayTable", "cleanDecayTable", (SingletDM))
+BE_FUNCTION(nucleonAmplitudes, int, (double(*)(double,double,double,double), double*, double*, double*, double*), "nucleonAmplitudes", "nucleonAmplitudes", (SingletDM) )
+BE_FUNCTION(FeScLoop, double, (double, double, double, double), "FeScLoop", "FeScLoop", (SingletDM))
+BE_FUNCTION(calcScalarQuarkFF, void, (double, double, double, double), "calcScalarQuarkFF", "calcScalarQuarkFF", (SingletDM))
 
 BE_FUNCTION(mInterp, int, (double,int,int,double*) , "mInterp", "mInterp")
 BE_FUNCTION(zInterp, double, (double,double*) , "zInterp", "zInterp")
 BE_FUNCTION(readSpectra, int, (), "readSpectra", "readSpectra")
 
-BE_VARIABLE(mocommon_, MicrOmegas::MOcommonSTR, "mocommon_", "MOcommon")
-BE_VARIABLE(ForceUG, int, "ForceUG", "ForceUG")
-BE_VARIABLE(VZdecay, int, "VZdecay", "VZdecay")
-BE_VARIABLE(VWdecay, int, "VWdecay", "VWdecay")
+BE_VARIABLE(mocommon_, MicrOmegas::MOcommonSTR, "mocommon_", "MOcommon", (SingletDM))
+BE_VARIABLE(ForceUG, int, "ForceUG", "ForceUG", (SingletDM))
+BE_VARIABLE(VZdecay, int, "VZdecay", "VZdecay", (SingletDM))
+BE_VARIABLE(VWdecay, int, "VWdecay", "VWdecay", (SingletDM))
 
 BE_CONV_FUNCTION(dNdE, double, (double,double,int,int), "dNdE")
 
