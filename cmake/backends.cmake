a8216c7a6cd4939f985fa0f74fcc3c56e4960fee
Pat Scott <p.scott@imperial.ac.uk>
2017-05-12 12:01:16 +0100

    Updated SUSY-HIT 1.5 URL to new public link just added by Maggie 	modified:   cmake/backends.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -348,7 +348,7 @@ endif()
 set(name "susyhit")
 set(ver "1.5")
 set(lib "libsusyhit")
-set(dl "http://astro.ic.ac.uk/sites/default/files/susyhit-${ver}.tar_.gz_.txt")
+set(dl "https://www.itp.kit.edu/~maggie/SUSY-HIT/version${ver}_${name}.tar.gz")
 set(md5 "493c7ba3a07e192918d3412875fb386a")
 set(dir "${PROJECT_SOURCE_DIR}/Backends/installed/${name}/${ver}")
 set(patch "${PROJECT_SOURCE_DIR}/Backends/patches/${name}/${ver}/patch_${name}_${ver}.dif")
@@ -363,7 +363,6 @@ check_ditch_status(${name} ${ver})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
     DOWNLOAD_COMMAND ${DL_BACKEND} ${dl} ${md5} ${dir} ${name} ${ver}
-             COMMAND ${CMAKE_COMMAND} -E rename ${backend_download}/susyhit-${ver}.tar_.gz_.txt ${backend_download}/susyhit-${ver}.tar.gz
     SOURCE_DIR ${dir}
     BUILD_IN_SOURCE 1
     PATCH_COMMAND patch -p1 < ${patch}
